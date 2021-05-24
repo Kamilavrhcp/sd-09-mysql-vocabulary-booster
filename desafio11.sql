ef120790359962468d9bd5360f05f4e18e3e7269
Diogenes <diogenesfelipe.s@outlook.com>
2021-05-24 20:26:33 -0300

    fix/desafio11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,8 @@
 SELECT
   C1.`ContactName` AS `Nome`,
   C1.`Country` AS `País`,
-  (COUNT(C1.`Country`) - 1) AS `Número de compatriotas`
+  COUNT(C1.`Country`) AS `Número de compatriotas`
 FROM w3schools.customers AS C1
-INNER JOIN w3schools.customers AS C2 ON C2.Country = C1.Country
+INNER JOIN w3schools.customers AS C2 ON C2.Country = C1.Country AND C2.CustomerID <> C1.CustomerID
 GROUP BY C1.`ContactName`, C1.`Country`
-HAVING (COUNT(C1.`Country`) - 1) != 0
 ORDER BY C1.`ContactName` ASC;

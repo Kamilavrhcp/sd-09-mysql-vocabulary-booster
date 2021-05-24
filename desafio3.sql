4a890e221ecd39a074947e2459477a415bc9f768
danielfasanaro <danielfasanaro@gmail.com>
2021-05-24 17:01:34 -0300

    feat: 3rd Requirement

---
+++ desafio3.sql
@@ -0,0 +1,5 @@
+SELECT  jobs.JOB_TITLE AS 'Cargo',
+        jobs.MAX_SALARY - jobs.MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
+FROM  hr.jobs
+ORDER BY  jobs.MAX_SALARY - jobs.MIN_SALARY,
+          jobs.JOB_TITLE;

84e3f467452ee6acdb936b0480d59167b6e8c31c
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-24 19:04:17 -0300

    req 14 corrigido

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,5 @@
-((select Country as 'País' from w3schools.suppliers)
+(select Country as 'País' from w3schools.suppliers)
 union
-(select Country as 'País'from w3schools.customers))
+(select Country as 'País'from w3schools.customers)
 order by `País`
 limit 5;

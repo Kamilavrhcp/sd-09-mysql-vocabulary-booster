fc4e89fcc96f8fe978a8e86876e293323d83d9d8
Leticia Lima <letylima96@gmail.com>
2021-05-24 15:55:35 -0300

    passa o if pra segunda linha

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,5 @@
-SELECT C.COUNTRY_NAME AS 'País', IF (REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+SELECT C.COUNTRY_NAME AS 'País', 
+	IF (REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries AS C
 INNER JOIN hr.regions AS R
 ON C.REGION_ID = R.REGION_ID

9950ee71701ba7e2adefd1a4843aaaf1cefced03
LucioOSilva <lucioosilva00@gmail.com>
2021-05-24 19:50:10 -0300

    lint adjusts

--- desafio11.sql
+++ desafio11.sql
@@ -1,14 +1,14 @@
 SELECT
-	d.ContactName AS Nome,
+	  d.ContactName AS Nome,
     d.Country AS País,
     quantityTable.`Número de compatriotas`
 FROM w3schools.customers AS d
 INNER JOIN
-	(SELECT
-		c.Country AS País,
-		(COUNT(*) -1) AS `Número de compatriotas`
-	FROM
-		w3schools.customers AS c
-	GROUP BY Country) AS quantityTable
+    (SELECT
+		    c.Country AS País,
+        (COUNT(*) -1) AS `Número de compatriotas`
+	  FROM
+		    w3schools.customers AS c
+	  GROUP BY Country) AS quantityTable
 ON d.Country = quantityTable.País
 ORDER BY Nome;

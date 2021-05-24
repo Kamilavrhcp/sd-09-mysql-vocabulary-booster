e0c8e148b97806a6c187e7abbd5c04faef30c0f6
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-05-24 20:13:06 -0300

    Requisitos 3 e 4

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,12 @@
-SELECT job_title AS 'Cargo', 
-	CASE 
-		WHEN max_salary > 30000 THEN 'Altíssimo'
-      WHEN max_salary <= 30000 AND max_salary > 20000 THEN 'Alto'
-      WHEN max_salary <= 20000 AND max_salary > 10000 THEN 'Médio'
-      WHEN max_salary <= 10000 AND max_salary >= 5000 THEN 'Baixo'
-	END AS 'Nível'
- FROM hr.jobs 
- ORDER BY job_title ASC;
+SELECT job_title AS 'Cargo',
+  CASE
+    WHEN max_salary > 30000 THEN 'Altíssimo'
+    WHEN max_salary <= 30000
+    AND max_salary > 20000 THEN 'Alto'
+    WHEN max_salary <= 20000
+    AND max_salary > 10000 THEN 'Médio'
+    WHEN max_salary <= 10000
+    AND max_salary >= 5000 THEN 'Baixo'
+  END AS 'Nível'
+FROM hr.jobs
+ORDER BY job_title ASC;

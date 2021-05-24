0a4f5a96efa39f8d17d504da96f0b20f6645a163
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-24 17:41:45 -0300

    desafio 10 corrigido

---
+++ desafio11.sql
@@ -0,0 +1,5 @@
+select c1.ContactName as 'Nome', c1.Country as 'País',
+(select count(c2.country) - 1 from w3schools.customers as c2 group by Country having c1.Country = c2.Country)
+ as 'Número de compatriotas'
+from w3schools.customers as c1
+order by ContactName;

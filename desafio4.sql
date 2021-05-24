0d373e12eced04af06b01433cd4f6b2541f2dfd8
MarcioFujita <hanker78@gmail.com>
2021-05-24 17:27:22 -0300

    Requisito 4 review v2

--- desafio4.sql
+++ desafio4.sql
@@ -8,5 +8,5 @@ END AS `Senioridade`
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j
 ON j.JOB_ID = e.JOB_ID
-GROUP BY `Cargo`
+GROUP BY `Cargo`, `Média Salarial`
 ORDER BY `Média Salarial` ASC, j.JOB_TITLE ASC;

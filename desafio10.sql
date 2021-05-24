8a44af61fa0af92b208a5c1120f8ffac2549957c
tiago-yoneda <xyonedax@gmail.com>
2021-05-24 16:43:34 -0300

    desafio 10 - completed

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+-- 10 - Exibe todos os produtos que já foram pedidos, que possuem uma média de quantidade nos pedidos registrados acima de 20.00
+SELECT
+    p.ProductName AS Produto,
+    MIN(Quantity) AS Mínima,
+    MAX(Quantity) as Máxima,
+    ROUND(AVG(Quantity), 2) AS Média
+FROM
+    products AS p
+        INNER JOIN
+    order_details AS o ON p.ProductID = o.ProductID
+GROUP BY Produto
+HAVING `Média` > 20
+ORDER BY Média, Produto;

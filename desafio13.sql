b0ca3752ddeec423675ab9eda7d4500f6b46a885
MarcioFujita <hanker78@gmail.com>
2021-05-24 20:04:17 -0300

    Requisitos 12 ao 16 -14

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT ProductName AS Produto, Price AS Preço
+FROM w3schools.products as p
+WHERE EXISTS (SELECT * FROM w3schools.order_details AS d
+        WHERE d.ProductID = p.ProductID
+        HAVING d.Quantity > 80)
+ORDER BY Produto ASC;

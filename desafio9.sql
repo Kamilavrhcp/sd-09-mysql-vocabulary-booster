c7da6b7cc306bdb58b7e107c663cc92aab2622be
Leticia Lima <letylima96@gmail.com>
2021-05-24 20:32:10 -0300

    tenta ajustar novamente o desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -2,10 +2,10 @@ SELECT
     CONCAT(E.FirstName, ' ', E.LastName) AS 'Nome completo',
     OD.Quantity AS 'Total de pedidos'
 FROM
-    w3schools.orders AS O
+    w3schools.employees AS E
+        INNER JOIN
+    w3schools.orders AS O ON O.EmployeeID = E.EmployeeID
         INNER JOIN
     w3schools.order_details AS OD ON OD.OrderID = O.OrderID
-        INNER JOIN
-    w3schools.employees AS E ON O.EmployeeID = E.EmployeeID
 GROUP BY `Nome completo`
 ORDER BY `Total de pedidos`;

7c127bdbe33ec7fdd40098f9152d8eab5b172eb0
Pablo Fernandes <pabloprates@gmail.com>
2021-05-24 18:40:58 -0300

    Five

--- desafio5.sql
+++ desafio5.sql
@@ -3,5 +3,4 @@ SELECT j.job_title AS 'Cargo',
 REPLACE(FORMAT(j.min_salary / 12, 2), ',', '') AS `Média mínima mensal`,
 REPLACE(FORMAT(j.max_salary / 12, 2), ',', '') AS `Média máxima mensal`
 FROM hr.jobs AS J
-GROUP BY j.job_title
 ORDER BY (j.max_salary - j.min_salary) ASC, j.job_title ASC;

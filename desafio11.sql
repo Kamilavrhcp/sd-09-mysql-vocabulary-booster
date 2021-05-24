4fc8918e87ccd3414c22f5fee11d98fb00492441
BBrolesi <brunohsbrolesi@gmail.com>
2021-05-24 19:41:57 -0300

    feat: complete requirement 11

---
+++ desafio11.sql
@@ -0,0 +1,15 @@
+SELECT 
+    customers.ContactName AS Nome,
+    customers.Country AS `País`,
+    (SELECT 
+            COUNT(*)
+        FROM
+            w3schools.customers
+        WHERE
+            w3schools.customers.Country = `País`
+                AND w3schools.customers.ContactName <> Nome) AS `Número de compatriotas`
+FROM
+    w3schools.customers AS customers
+HAVING 
+	`Número de compatriotas` <> '0'
+ORDER BY Nome;

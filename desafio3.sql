9a8a58c18ae033f64ec0916fd404522daaa6c0a7
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-05-24 20:10:39 -0300

    Requisitos 3 e 4

--- desafio3.sql
+++ desafio3.sql
@@ -1,4 +1,3 @@
-SELECT job_title as 'Cargo',
-	(max_salary - min_salary) AS 'Diferença entre salários máximo e mínimo'
+SELECT job_title as 'Cargo', (max_salary - min_salary) AS 'Diferença entre salários máximo e mínimo'
 FROM hr.jobs
 ORDER BY 2, job_title ASC;

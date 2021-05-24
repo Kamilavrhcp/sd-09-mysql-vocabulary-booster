07a1cc2955b7922b2958b719ad229b6f432e6b9e
Vagner Pereira <sousa.vagner82@gmail.com>
2021-05-24 17:03:11 -0300

    code climate

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,2 @@
-SELECT 
-	C.COUNTRY_NAME País, 
-	IF(R.REGION_NAME = 'Europe', 'incluido', 'não incluido')
-FROM 
-	hr.countries C INNER JOIN hr.regions R 
-    ON C.REGION_ID = R.REGION_ID;
-    
+SELECT C.COUNTRY_NAME País, IF(R.REGION_NAME = 'Europe', 'incluido', 'não incluido')
+FROM hr.countries C INNER JOIN hr.regions R ON C.REGION_ID = R.REGION_ID;

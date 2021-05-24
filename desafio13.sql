04694472bc48d1c12c131b040501baa040c622cb
tiago-yoneda <xyonedax@gmail.com>
2021-05-24 17:20:23 -0300

    desafio 12 e 13 - completed

---
+++ desafio13.sql
@@ -0,0 +1,14 @@
+-- 13 - Exibe todos produtos que já tiveram um pedido associado requerindo uma quantidade desse produto maior que 80
+SELECT
+    ProductName AS Produto, Price AS Preço
+FROM
+    w3schools.products AS p
+WHERE
+    EXISTS( SELECT
+            *
+        FROM
+            order_details AS d
+        WHERE
+            d.ProductID = p.ProductID
+        HAVING d.Quantity > 80)
+ORDER BY Produto;

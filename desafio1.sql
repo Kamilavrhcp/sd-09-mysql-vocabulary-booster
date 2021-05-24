b55a61f22fed7e83a4d24da4fa44092250e014c1
Pedro <ped.alm2709@gmail.com>
2021-05-24 16:34:37 -0300

    req 1 de novo

--- desafio1.sql
+++ desafio1.sql
@@ -1,8 +1,8 @@
 SELECT 
     COUNTRY_NAME AS 'País',
-    IF(c.REGION_ID = 1,
+    IF(r.REGION_NAME= 'Europe',
         'incluído',
-        'não incluído') AS 'Status de Inclusão'
+        'não incluído') AS 'Status Inclusão'
 FROM
     hr.countries AS c
         INNER JOIN

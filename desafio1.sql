5cdaddc8a07f7505eaa0cc46e3a5d129ebc6f712
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-24 14:38:31 -0300

    desafio 2

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-select COUNTRY_NAME as 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão' from countries;
+select COUNTRY_NAME as 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão' from hr.countries;

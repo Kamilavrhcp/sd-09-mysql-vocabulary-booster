46f456a67baed2604ed607e3579250cb9c251954
Leticia Lima <letylima96@gmail.com>
2021-05-24 17:20:52 -0300

    retira tab da segunda linha desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT C.COUNTRY_NAME AS 'País', 
-	IF (REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+IF (REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries AS C
 INNER JOIN hr.regions AS R
 ON C.REGION_ID = R.REGION_ID

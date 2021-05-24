46263d8ed486e0bcfbe3f1a03c3c7cc41de2c15a
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-05-24 14:38:55 -0300

    fix cc

--- desafio8.sql
+++ desafio8.sql
@@ -7,9 +7,9 @@ w3schools.customers,
 w3schools.shippers,
 w3schools.orders
 WHERE
-	w3schools.customers.CustomerID = w3schools.orders.CustomerID
-    AND
-    w3schools.shippers.ShipperID = w3schools.orders.ShipperID
-    AND
-    w3schools.orders.ShipperID IN (1 , 2)
+w3schools.customers.CustomerID = w3schools.orders.CustomerID
+AND
+w3schools.shippers.ShipperID = w3schools.orders.ShipperID
+AND
+w3schools.orders.ShipperID IN (1 , 2)
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

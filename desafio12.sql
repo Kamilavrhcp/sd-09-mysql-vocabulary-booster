fbf36af1733295c78448448060ba6da412927729
Diogenes <diogenesfelipe.s@outlook.com>
2021-05-24 17:57:33 -0300

    feat/desafio12

---
+++ desafio12.sql
@@ -0,0 +1,11 @@
+SELECT
+  CONCAT(EMP1.`FIRST_NAME`, ' ', EMP1.`LAST_NAME`) AS `Nome Completo Funcionário 1`,
+  EMP1.`SALARY` AS `Salário funcionário 1`,
+  EMP1.`PHONE_NUMBER` AS `Telefone funcionário 1`,
+  CONCAT(EMP2.`FIRST_NAME`, ' ', EMP2.`LAST_NAME`) AS `Nome Completo Funcionário 2`,
+  EMP2.`SALARY` AS `Salário funcionário 2`,
+  EMP2.`PHONE_NUMBER` AS `Telefone funcionário 2`
+FROM hr.employees AS EMP1
+INNER JOIN hr.employees AS EMP2 ON EMP2.`JOB_ID` = EMP1.`JOB_ID`
+AND CONCAT(EMP2.`FIRST_NAME`, ' ', EMP2.`LAST_NAME`) != CONCAT(EMP1.`FIRST_NAME`, ' ', EMP1.`LAST_NAME`)
+ORDER BY `Nome Completo Funcionário 1` ASC, `Nome Completo Funcionário 2` ASC;

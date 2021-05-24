b086f91e5f002faed413807459f2d95961c5ae9f
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-24 20:50:22 -0300

    desafio 15 corrigido

--- desafio15.sql
+++ desafio15.sql
@@ -1,10 +1,9 @@
 delimiter $$
 create procedure buscar_media_por_cargo(
-in nome_cargo varchar(300),
-out media_salarial_cargo double
+in nome_cargo varchar(300)
 )
 begin
-select round(avg(em.SALARY),2) into media_salarial_cargo
+select round(avg(em.SALARY),2) as 'Média salarial'
 from hr.employees as em
 inner join hr.jobs as jo on jo.JOB_ID = em.JOB_ID
 where JOB_TITLE = nome_cargo;

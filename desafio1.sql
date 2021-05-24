0bdd1ae717e8f986ed3422b6c172ec40f69cd31e
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-05-24 15:25:09 -0300

    Desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,8 @@
-SELECT COUNTRY_NAME AS 'País', IF(REGION_ID= 1, 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM hr.countries
-ORDER BY País ASC; 
+SELECT 
+    COUNTRY_NAME AS 'País',
+    IF(REGION_ID = 1,
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
+FROM
+    hr.countries
+ORDER BY País ASC

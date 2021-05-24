80b366aa3dce94e735f2ecd40465f6e85aa96047
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-24 18:18:57 -0300

    desafio 12

---
+++ desafio12.sql
@@ -0,0 +1,9 @@
+select concat(em1.FIRST_NAME, ' ', em1.LAST_NAME) as 'Nome completo funcionário 1',
+em1.SALARY as 'Salário funcionário 1',
+em1.PHONE_NUMBER as 'Telefone funcionário 1',
+concat(em2.FIRST_NAME, ' ', em2.LAST_NAME) as 'Nome completo funcionário 2',
+em2.SALARY as 'Salário funcionário 2',
+em2.PHONE_NUMBER as 'Telefone funcionário 1'
+from hr.employees as em1, hr.employees as em2
+where em1.JOB_ID = em2.JOB_ID and em1.EMPLOYEE_ID != em2.EMPLOYEE_ID
+order by `Nome completo funcionário 1`, `Nome completo funcionário 2`;

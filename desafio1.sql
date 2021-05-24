92c1d536a6d096dc345e8aca5b137d9cbb773c89
Raquel Picanço <picanco.raquel@gmail.com>
2021-05-24 14:53:08 -0400

    Solve challenge 1

---
+++ desafio1.sql
@@ -0,0 +1,7 @@
+SELECT
+  ct.COUNTRY_NAME as 'País',
+  IF(rg.REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão'
+FROM
+  hr.countries ct
+  INNER JOIN hr.regions rg ON ct.REGION_ID = rg.REGION_ID
+ORDER BY ct.COUNTRY_NAME;

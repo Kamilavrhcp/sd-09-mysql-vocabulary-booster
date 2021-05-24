a7f3b26b09b5332fba1762b7aed7ffc3125c5bfe
tiago-yoneda <xyonedax@gmail.com>
2021-05-24 19:22:10 -0300

    desafio 17 - completed

--- desafio15.sql
+++ desafio15.sql
@@ -5,10 +5,10 @@ USE hr;
 DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(30))
-BEGIN 
-	SELECT ROUND(AVG(SALARY),2) AS `Média salarial` FROM hr.employees AS e
-	INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
-	WHERE JOB_TITLE = cargo;
+BEGIN
+    SELECT ROUND(AVG(SALARY),2) AS `Média salarial` FROM hr.employees AS e
+    INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
+    WHERE JOB_TITLE = cargo;
 END $$
 
 DELIMITER ;
--- desafio16.sql
+++ desafio16.sql
@@ -1,20 +1,20 @@
--- 16 - Crie uma função chamada buscar_quantidade_de_empregos_por_funcionario no banco de dados hr que, ao receber o email de uma pessoa funcionária, 
+-- 16 - Crie uma função chamada buscar_quantidade_de_empregos_por_funcionario no banco de dados hr que, ao receber o email de uma pessoa funcionária,
 -- retorne a quantidade de empregos presentes em seu histórico
 USE hr; -- Pode tirar que não tem problema para rodar no avaliador.
 
-DELIMITER $$ 
+DELIMITER $$
 
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (email VARCHAR(50))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE quantidade INT;
-	SELECT COUNT(*) AS `total_empregos`FROM hr.job_history
+    DECLARE quantidade INT;
+    SELECT COUNT(*) AS `total_empregos`FROM hr.job_history
     WHERE EMPLOYEE_ID IN (
-		SELECT EMPLOYEE_ID FROM hr.employees AS e 
+        SELECT EMPLOYEE_ID FROM hr.employees AS e
         WHERE e.EMAIL = email
-	)
+    )
     INTO quantidade;
-    RETURN quantidade;    
+    RETURN quantidade;
 END $$
 
-DELIMITER ; 
+DELIMITER ;

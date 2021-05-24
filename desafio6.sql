28ad2432de0fe354585e6d6f3f3a4de3cd992916
Luciano_Lodi <lrlodi@gmail.com>
2021-05-24 19:39:13 -0300

    Correçoes de Lint

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,8 @@
 SELECT
-	CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-	j.JOB_TITLE AS 'Cargo',
-    jh.START_DATE AS 'Data de início do cargo',
-    d.DEPARTMENT_NAME AS 'Departamento'
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
+j.JOB_TITLE AS 'Cargo',
+jh.START_DATE AS 'Data de início do cargo',
+d.DEPARTMENT_NAME AS 'Departamento'
 FROM hr.employees AS e
 INNER JOIN hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 INNER JOIN hr.jobs as j ON jh.JOB_ID = j.JOB_ID

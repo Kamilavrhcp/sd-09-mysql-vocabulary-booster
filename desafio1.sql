778cde55ad36723179c005fb748b8ceb3b4a4a91
tiago-yoneda <xyonedax@gmail.com>
2021-05-24 14:24:56 -0300

    desafio 1 - completed

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,11 @@
--- começamos aqui
+-- 1 - Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
+SELECT
+    COUNTRY_NAME AS 'País',
+    IF(r.REGION_NAME = 'Europe',
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
+FROM
+    hr.countries AS c
+        INNER JOIN
+    hr.regions AS r ON r.REGION_ID = c.REGION_ID
+ORDER BY COUNTRY_NAME;

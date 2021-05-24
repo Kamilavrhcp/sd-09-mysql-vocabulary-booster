28a54ea839e86a971e0746ac99f85646f036a801
Raquel Picanço <picanco.raquel@gmail.com>
2021-05-24 16:01:27 -0400

    Solve challenge 5

---
+++ desafio5.sql
@@ -0,0 +1,9 @@
+SELECT
+  JOB_TITLE as 'Cargo',
+  (MAX_SALARY - MIN_SALARY) as 'Variação Salarial',
+  ROUND(MIN_SALARY / 12, 2) as 'Média mínima mensal',
+  ROUND(MAX_SALARY / 12, 2) as 'Média máxima mensal'
+FROM
+  hr.jobs
+GROUP BY `Cargo`
+ORDER BY `Variação Salarial` ASC, `Cargo` ASC;

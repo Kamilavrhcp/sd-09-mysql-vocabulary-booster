986d786dabb841ef8b5fcb9de0f887b335244a4c
nyxnil <nsmnilsonn@gmail.com>
2021-05-24 18:46:17 -0300

    requisitos 1 ao 7 e correcoes

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+select ucase(concat(e.FIRST_NAME, ' ', e.LAST_NAME)) as 'Nome completo',
+h.START_DATE as 'Data de início do cargo',
+e.SALARY as 'Salário'
+from hr.job_history as h
+inner join hr.employees as e on h.EMPLOYEE_ID = E.EMPLOYEE_ID
+inner join hr.jobs as j on h.JOB_ID = j.JOB_ID
+where month(h.START_DATE) between 1 and 3
+order by concat(e.FIRST_NAME, ' ', e.LAST_NAME), j.JOB_TITLE;

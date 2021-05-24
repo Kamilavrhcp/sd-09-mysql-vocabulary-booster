1f65eaf26f5ef3637ea6c382d0cc9ccd534b103f
Marcelo Campos <marcelo.campos.economista@gmail.com>
2021-05-24 18:54:16 -0300

    Fix codeclimate issues

--- desafio16.sql
+++ desafio16.sql
@@ -4,13 +4,13 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(200))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE quantityOfRegisteredJobs INT;
-	SELECT COUNT(*) 
-	FROM employees AS E
-	INNER JOIN job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
-	WHERE E.EMAIL = email
-    INTO quantityOfRegisteredJobs;
-    RETURN quantityOfRegisteredJobs;
+DECLARE quantityOfRegisteredJobs INT;
+SELECT COUNT(*) 
+FROM employees AS E
+INNER JOIN job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+WHERE E.EMAIL = email
+INTO quantityOfRegisteredJobs;
+RETURN quantityOfRegisteredJobs;
 END $$
 
 DELIMITER ;
--- desafio17.sql
+++ desafio17.sql
@@ -3,8 +3,8 @@ USE w3schools;
 DELIMITER $$
 
 CREATE TRIGGER trigger_insert_order
-	BEFORE INSERT ON orders
-    FOR EACH ROW
+BEFORE INSERT ON orders
+FOR EACH ROW
 BEGIN
 	SET NEW.OrderDate = NOW();
 END $$

bbb4e755b3631b9543ed9e5f7e44c13674dec082
Pablo Fernandes <pabloprates@gmail.com>
2021-05-24 18:11:39 -0300

    Challenge five

--- desafio5.sql
+++ desafio5.sql
@@ -1,7 +1,7 @@
 SELECT j.job_title AS 'Cargo',
 (MAX(e.salary) - MIN(e.salary)) AS `Variação Salarial`,
-REPLACE(FORMAT(MIN(e.salary), 2), ',', '') AS `Média mínima mensal`,
-REPLACE(FORMAT(MAX(e.salary), 2), ',', '') AS `Média máxima mensal`
+REPLACE(FORMAT(MIN(e.salary) / 12, 2), ',', '') AS `Média mínima mensal`,
+REPLACE(FORMAT(MAX(e.salary) / 12, 2), ',', '') AS `Média máxima mensal`
 FROM hr.jobs AS j, hr.employees AS e
 WHERE e.job_id = j.job_id
 GROUP BY j.job_title

420fa0eb5448ef6636c7b6a11d32efd5c1bf1855
edison-ps <edison@osite.com.br>
2021-05-24 17:03:50 -0300

    Desafio 01 V1.02

--- desafio1.sql
+++ desafio1.sql
@@ -1,10 +1,10 @@
 SELECT COUNTRY_NAME AS País, 
-	IF (
-			(
-			SELECT REGION_NAME
-			FROM hr.regions
-			WHERE REGION_ID = C.REGION_ID
-			)  = 'Europe', 'incluído', 'não incluído'
-		) AS 'Status Inclusão'
+IF (
+(
+SELECT REGION_NAME
+FROM hr.regions
+WHERE REGION_ID = C.REGION_ID
+)  = 'Europe', 'incluído', 'não incluído'
+) AS 'Status Inclusão'
 FROM hr.countries AS C
 ORDER BY COUNTRY_NAME;

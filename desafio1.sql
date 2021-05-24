36fa100d2360a629dfbd003d5a2f873c4a505cd7
Vagner Pereira <sousa.vagner82@gmail.com>
2021-05-24 18:23:15 -0300

    desafio 3 e 4

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,3 @@
-SELECT C.COUNTRY_NAME País, IF(R.REGION_NAME = 'Europe', 'incluído', 'não incluído')
-FROM hr.countries C INNER JOIN hr.regions R ON C.REGION_ID = R.REGION_ID;
+SELECT C.COUNTRY_NAME País, IF(R.REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries C INNER JOIN hr.regions R ON C.REGION_ID = R.REGION_ID
+ORDER BY C.COUNTRY_NAME;

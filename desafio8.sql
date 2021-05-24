9b2407f441ea39aa6e4460dae2e7ef9baeb09292
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-05-24 18:21:10 -0300

    Realizado desafio 08

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT 
+    customers.ContactName 'Nome de contato',
+    shipperName AS 'Empresa que fez o envio',
+    orders.orderDate AS 'Data do pedido'
+FROM w3schools.orders AS orders
+INNER JOIN w3schools.customers AS customers ON orders.CustomerID = customers.CustomerID
+INNER JOIN w3schools.shippers AS shippers ON orders.ShipperID = shippers.ShipperID
+WHERE orders.ShipperID IN (1, 2)
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
+
+
+
+

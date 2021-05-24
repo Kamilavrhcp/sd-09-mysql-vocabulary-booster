6d978a7c1b32d7e4a3e15f1ff6f1faaf7b5cdaaf
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-24 15:02:11 -0300

    desafio 5 corrigidos

--- desafio2.sql
+++ desafio2.sql
@@ -1,7 +1,7 @@
 select JOB_TITLE as 'Cargo',
 case
-	when MAX_SALARY between 5000 and 10000 THEN 'Baixo'
-	when MAX_SALARY between 10001 and 20000 THEN 'Médio'
-	when MAX_SALARY between 20001 and 30000 THEN 'Alto'
-	when MAX_SALARY > 30000 THEN 'Altíssimo'
+when MAX_SALARY between 5000 and 10000 THEN 'Baixo'
+when MAX_SALARY between 10001 and 20000 THEN 'Médio'
+when MAX_SALARY between 20001 and 30000 THEN 'Alto'
+when MAX_SALARY > 30000 THEN 'Altíssimo'
 end as 'Nível' from hr.jobs order by JOB_TITLE;
--- desafio4.sql
+++ desafio4.sql
@@ -1,9 +1,9 @@
 select jo.JOB_TITLE as 'Cargo', round(avg(em.SALARY),2) as 'Média salarial',
 case
-	when round(avg(em.SALARY),2) between 2000 and 5800 then 'Júnior'
-	when round(avg(em.SALARY),2) between 5801 and 7500 then 'Pleno'
-	when round(avg(em.SALARY),2) between 7501 and 10000 then 'Sênior'
-	when round(avg(em.SALARY),2) > 10500 then 'CEO'
+when round(avg(em.SALARY),2) between 2000 and 5800 then 'Júnior'
+when round(avg(em.SALARY),2) between 5801 and 7500 then 'Pleno'
+when round(avg(em.SALARY),2) between 7501 and 10000 then 'Sênior'
+when round(avg(em.SALARY),2) > 10500 then 'CEO'
 end as 'Senioridade'
 from hr.jobs as jo inner join hr.employees as em on jo.JOB_ID = em.JOB_ID
 group by jo.JOB_TITLE

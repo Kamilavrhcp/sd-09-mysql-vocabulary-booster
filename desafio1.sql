272d51b52342baaa5156caa570eef5932a06208f
Leticia Lima <letylima96@gmail.com>
2021-05-24 18:47:15 -0300

    ajusta identacao dos desafios

--- desafio1.sql
+++ desafio1.sql
@@ -1,6 +1,10 @@
-SELECT C.COUNTRY_NAME AS 'País', 
-IF (REGION_NAME = 'Europe', 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM hr.countries AS C
-INNER JOIN hr.regions AS R
-ON C.REGION_ID = R.REGION_ID
+SELECT 
+    C.COUNTRY_NAME AS 'País',
+    IF(REGION_NAME = 'Europe',
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
+FROM
+    hr.countries AS C
+        INNER JOIN
+    hr.regions AS R ON C.REGION_ID = R.REGION_ID
 ORDER BY COUNTRY_NAME;
--- desafio6.sql
+++ desafio6.sql
@@ -1,11 +1,14 @@
-SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
-J.JOB_TITLE AS 'Cargo',
-JH.START_DATE AS 'Data de início do cargo',
-D.DEPARTMENT_NAME AS 'Departamento'
-
-FROM hr.job_history AS JH
-INNER JOIN hr.employees AS E ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
-INNER JOIN hr.jobs AS J ON J.JOB_ID = E.JOB_ID
-INNER JOIN hr.departments AS D ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
-
+SELECT 
+    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+    J.JOB_TITLE AS 'Cargo',
+    JH.START_DATE AS 'Data de início do cargo',
+    D.DEPARTMENT_NAME AS 'Departamento'
+FROM
+    hr.job_history AS JH
+        INNER JOIN
+    hr.employees AS E ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+        INNER JOIN
+    hr.jobs AS J ON J.JOB_ID = E.JOB_ID
+        INNER JOIN
+    hr.departments AS D ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, JOB_TITLE ASC;

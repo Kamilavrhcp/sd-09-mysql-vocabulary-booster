e6d2be6988da47d0cbf9e2e223887ab29b73475c
Pablo Fernandes <pabloprates@gmail.com>
2021-05-24 14:57:55 -0300

    Challenge One

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,6 @@
 SELECT 
-    *
+    c.country_name AS 'País', IF( c.region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM
-    hr.countries;
+    hr.countries AS c, hr.regions r
+WHERE
+	c.region_id = r.region_id;

773a241b6721ad8c260d543ec181400d86923855
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-05-24 18:16:43 -0300

    Lint gente boa...TESTE

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 SELECT JOB_TITLE AS 'Cargo',
 CASE
-    WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
-    WHEN MAX_SALARY > 20000 THEN 'Alto'
-    WHEN MAX_SALARY > 10000 THEN 'Médio'
-    ELSE 'Baixo'
-    END AS 'Nível'
+  WHEN MAX_SALARY > 30000 THEN 'Altíssimo'
+  WHEN MAX_SALARY > 20000 THEN 'Alto'
+  WHEN MAX_SALARY > 10000 THEN 'Médio'
+  ELSE 'Baixo'
+  END AS 'Nível'
 FROM hr.jobs
 ORDER BY JOB_TITLE;

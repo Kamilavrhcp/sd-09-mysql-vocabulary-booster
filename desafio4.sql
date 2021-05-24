0cf6388bd8223f7d498ed4c68fc0d85f61616f8c
Diogenes <diogenesfelipe.s@outlook.com>
2021-05-24 15:42:04 -0300

    feat/desafio5 and fix/desafio4 - correction of the name column

--- desafio4.sql
+++ desafio4.sql
@@ -1,4 +1,4 @@
-SELECT j.`JOB_TITLE` AS `Cargo`, ROUND(AVG(e.`SALARY`), 2) AS `Média`,
+SELECT j.`JOB_TITLE` AS `Cargo`, ROUND(AVG(e.`SALARY`), 2) AS `Média salarial`,
 CASE
   WHEN AVG(e.`SALARY`) BETWEEN 2000 AND 5800 THEN 'Júnior'
   WHEN AVG(e.`SALARY`) BETWEEN 5801 AND 7500 THEN 'Pleno'
@@ -8,4 +8,4 @@ END AS 'Senioridade'
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
 GROUP BY e.JOB_ID
-ORDER BY `Média` ASC, `Cargo` ASC;
+ORDER BY `Média salarial` ASC, `Cargo` ASC;

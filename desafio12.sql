dbad6c5d8775778bb91e20de3fd72062bb3e793b
LucioOSilva <lucioosilva00@gmail.com>
2021-05-24 19:44:40 -0300

    added req12 and req13

---
+++ desafio12.sql
@@ -0,0 +1,12 @@
+SELECT
+	CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo funcionário 1`,
+    e.SALARY AS `Salário funcionário 1`,
+    e.PHONE_NUMBER AS `Telefone funcionário 1`,
+    CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS `Nome completo funcionário 2`,
+    e2.SALARY AS `Salário funcionário 2`,
+    e2.PHONE_NUMBER AS `Telefone funcionário 2`
+FROM hr.employees as e
+INNER JOIN hr.employees as e2
+ON e.JOB_ID = e2.JOB_ID
+WHERE e.EMPLOYEE_ID <> e2.EMPLOYEE_ID
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;

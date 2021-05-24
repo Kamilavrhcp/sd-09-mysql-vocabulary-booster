05744493dd48d07d83fe2a7db09877b68890d89a
Diogenes <diogenesfelipe.s@outlook.com>
2021-05-24 17:57:18 -0300

    fix/desafio11 - group by

--- desafio11.sql
+++ desafio11.sql
@@ -4,5 +4,5 @@ SELECT
   (COUNT(C1.`Country`) - 1) AS `Número de compatriotas`
 FROM w3schools.customers AS C1
 INNER JOIN w3schools.customers AS C2 ON C2.Country = C1.Country
-GROUP BY C1.`ContactName`
+GROUP BY C1.`ContactName`, C1.`Country`
 ORDER BY C1.`ContactName` ASC;

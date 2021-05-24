f309eb58fb6e2b080726d83a9ba745ce5122dc4a
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-05-24 17:37:33 -0300

    reqs 4 and 5

---
+++ desafio5.sql
@@ -0,0 +1,15 @@
+SELECT * FROM hr.jobs;
+SELECT * FROM hr.employees;
+SELECT * FROM hr.job_history;
+
+SELECT 
+    J.JOB_TITLE AS Cargo,
+    (J.MAX_SALARY - J.MIN_SALARY) AS `Variação Salarial`,
+    ROUND(J.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
+    ROUND(J.MAX_SALARY / 12, 2) AS 'Média máxima mensal'
+FROM
+    hr.employees AS E
+        INNER JOIN
+    hr.jobs AS J ON E.JOB_ID = J.JOB_ID
+GROUP BY Cargo
+ORDER BY `Variação Salarial` ASC , Cargo ASC;

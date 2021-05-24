e8773039457c8bba3741dce797b861cfc7fbd1ad
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-05-24 17:02:52 -0300

    Requirement 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,8 @@
-select * from hr.countries;
+USE hr;
+SELECT 
+    COUNTRY_NAME AS 'País',
+    IF(REGION_ID = 1,
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
+FROM
+    countries;

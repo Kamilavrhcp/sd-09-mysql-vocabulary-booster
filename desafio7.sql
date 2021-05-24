6daf78f4442cfae67e02119515d7a8d216532cd2
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-24 15:31:55 -0300

    desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,6 @@
 select ucase(concat(em.FIRST_NAME, ' ', em.LAST_NAME)) as 'Nome completo',
-em.HIRE_DATE as 'Data de início',
-month(em.HIRE_DATE),
+jh.START_DATE as 'Data de início',
 em.SALARY as 'Salário'
-from hr.employees as em
-where month(em.HIRE_DATE) between 1 and 3
-order by concat(em.FIRST_NAME, ' ', em.LAST_NAME) asc, em.HIRE_DATE asc;
+from hr.employees as em inner join hr.job_history as jh on jh.EMPLOYEE_ID  = em.EMPLOYEE_ID
+where month(jh.START_DATE) between 1 and 3
+order by concat(em.FIRST_NAME, ' ', em.LAST_NAME) asc, jh.START_DATE asc;

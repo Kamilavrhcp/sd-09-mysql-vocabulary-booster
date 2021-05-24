52ee8131a5d9a3fe7047776edd725fd28a40a417
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-05-24 17:41:06 -0300

    task 1,2 and 3 withou USE

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,1 @@
-USE hr;
-SELECT COUNTRY_NAME as 'País', IF(REGION_ID = 1, "incluído", "não incluído")as `Status Inclusão` FROM countries;
+SELECT COUNTRY_NAME as 'País', IF(REGION_ID = 1, "incluído", "não incluído")as `Status Inclusão` FROM hr.countries;
--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,2 @@
-USE hr;
-SELECT JOB_TITLE as 'Cargo', (MAX_SALARY - MIN_SALARY) as 'Diferença entre salários máximo e mínimo' FROM jobs
-ORDER BY CAST('Diferença entre salários máximo e mínimo' as unsigned);
+SELECT JOB_TITLE as 'Cargo', (MAX_SALARY - MIN_SALARY) as 'Diferença entre salários máximo e mínimo' FROM hr.jobs
+ORDER BY CAST(`Diferença entre salários máximo e mínimo` as unsigned) ASC;

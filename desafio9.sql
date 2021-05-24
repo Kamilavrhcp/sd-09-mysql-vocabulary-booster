e677abcf178f5bb0a16792aab3637d3393cb1469
Pablo Fernandes <pabloprates@gmail.com>
2021-05-24 20:10:09 -0300

    Challenge Nine

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+SELECT
+UCASE(CONCAT(e.FirstName, ' ', e.LastName)) AS `Nome completo`,
+COUNT(*) AS `Total de pedidos`
+FROM w3schools.employees AS e, w3schools.orders AS o
+WHERE
+e.EmployeeID = o.EmployeeID
+GROUP BY
+e.EmployeeID
+ORDER BY
+`Total de pedidos` ASC;

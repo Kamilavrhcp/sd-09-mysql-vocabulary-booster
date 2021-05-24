b227e5ac7454544afae0f44defd2e6cbe616053d
Pablo Fernandes <pabloprates@gmail.com>
2021-05-24 15:08:01 -0300

    Challenge One - CC complains

--- desafio1.sql
+++ desafio1.sql
@@ -1,7 +1,4 @@
-SELECT 
-    c.country_name AS 'País', IF( c.region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
-FROM
-    hr.countries AS c, hr.regions r
-WHERE
-	c.region_id = r.region_id
+SELECT c.country_name AS 'País', IF( c.region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM hr.countries AS c, hr.regions r
+WHERE c.region_id = r.region_id
 ORDER BY c.country_name ASC;

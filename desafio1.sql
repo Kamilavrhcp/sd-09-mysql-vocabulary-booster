2df451bb9d3140b3e015a77d683b8667c5fd540b
Vagner Pereira <sousa.vagner82@gmail.com>
2021-05-24 17:45:44 -0300

    correcao

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,2 @@
-SELECT C.COUNTRY_NAME País, IF(R.REGION_NAME = 'Europe', 'incluido', 'não incluido')
+SELECT C.COUNTRY_NAME País, IF(R.REGION_NAME = 'Europe', 'incluído', 'não incluído')
 FROM hr.countries C INNER JOIN hr.regions R ON C.REGION_ID = R.REGION_ID;

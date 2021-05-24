59f043fc65b4edd3feaf5036ab356328e9ff3f52
Rafael Pufal <rafael.pufal23@gmail.com>
2021-05-24 15:17:15 -0300

    desafio 1 e 6 corrigidos

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-select COUNTRY_NAME as 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão' from hr.countries;
+select COUNTRY_NAME as 'País', IF(REGION_ID = 1, 'incluído', 'não incluído') as 'Status Inclusão' from hr.countries order by COUNTRY_NAME;
--- desafio6.sql
+++ desafio6.sql
@@ -4,4 +4,4 @@ em.HIRE_DATE as 'Data de início do cargo',
 de.DEPARTMENT_NAME as 'Departamento'
 from hr.jobs as jo inner join hr.employees as em on jo.JOB_ID = em.JOB_ID
 inner join hr.departments as de on de.DEPARTMENT_ID = em.DEPARTMENT_ID
-order by 'Nome Completo' desc, 'Cargo' asc;
+order by concat(em.FIRST_NAME, ' ', em.LAST_NAME) desc, jo.JOB_TITLE asc;

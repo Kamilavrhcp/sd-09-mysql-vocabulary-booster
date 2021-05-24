abcf4f7e741afe9d60b35e688fc0b9386fb068df
Khyrki <khyrkimitsunobi@gmail.com>
2021-05-24 19:30:06 -0300

    CC

--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
 SELECT
-	C.ContactName AS 'Nome de contato',
-    S.ShipperName AS 'Empresa que fez o envio',
-    O.OrderDate
+C.ContactName AS 'Nome de contato',
+S.ShipperName AS 'Empresa que fez o envio',
+O.OrderDate
 FROM w3schools.orders AS O
 INNER JOIN w3schools.customers AS C ON C.CustomerID = O.CustomerID
 INNER JOIN w3schools.shippers AS S ON S.ShipperID = O.ShipperID

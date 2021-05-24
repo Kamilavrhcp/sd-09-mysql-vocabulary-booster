73adf9a73dc228d65b3dd0025865b4c47d62b600
Daniela Muller <danielamuller20@gmail.com>
2021-05-24 14:51:55 -0300

    desafio 1

---
+++ desafio1.sql
@@ -0,0 +1,10 @@
+SELECT 
+    C.COUNTRY_NAME AS País,
+    IF(R.REGION_ID = 1,
+        'incluído',
+        'não incluído') AS `Status Inclusão`
+FROM
+    hr.countries AS C
+        INNER JOIN
+    hr.regions AS R ON C.REGION_ID = R.REGION_ID
+ORDER BY C.COUNTRY_NAME;

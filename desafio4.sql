ce13b8faabf1bfed538a45a056a0a2c685003012
cleber-teixeira <cleberlt@gmail.com>
2021-05-24 20:58:26 -0300

    Requisito 4 - Refatoração. :white_check_mark:

--- desafio4.sql
+++ desafio4.sql
@@ -1,11 +1,11 @@
 SELECT j.JOB_TITLE AS Cargo,
-ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+ROUND(AVG(e.SALARY), 2) AS `Média salarial`,
   CASE
-    WHEN 'Média salarial' < 5801 THEN 'Júnior'
-    WHEN 'Média salarial' < 7501 THEN 'Pleno'
-    WHEN 'Média salarial' < 10501 THEN 'Sênior'
-    WHEN 'Média salarial' > 10500 THEN 'CEO'
-END AS 'Senioridade'
+    WHEN AVG(e.SALARY) < 5801 THEN 'Júnior'
+    WHEN AVG(e.SALARY) < 7501 THEN 'Pleno'
+    WHEN AVG(e.SALARY) < 10501 THEN 'Sênior'
+    WHEN AVG(e.SALARY) > 10500 THEN 'CEO'
+END AS Senioridade
 FROM hr.jobs AS j
 INNER JOIN hr.employees AS e
 ON j.JOB_ID = e.JOB_ID

70aa5f2c6e6d1c7be41ee94d37708d10e8f38882
Pablo Fernandes <pabloprates@gmail.com>
2021-05-24 19:26:26 -0300

    CC useless war

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
-SELECT job_title AS 'Cargo',
+SELECT j.job_title AS 'Cargo',
 (j.max_salary - j.min_salary) AS `Variação Salarial`,
 REPLACE(FORMAT(j.min_salary / 12, 2), ',', '') AS `Média mínima mensal`,
 REPLACE(FORMAT(j.max_salary / 12, 2), ',', '') AS `Média máxima mensal`
-FROM hr.jobs AS J
+FROM hr.jobs AS j
 ORDER BY (j.max_salary - j.min_salary) ASC, j.job_title ASC;

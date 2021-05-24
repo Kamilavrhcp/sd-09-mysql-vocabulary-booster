7d960c58c9945a99d6c5d48cb13134b5fc3a38cb
PedroMCHenrique <pedro.876.ph@gmail.com>
2021-05-24 20:12:27 -0300

    fixing issues

--- desafio13.sql
+++ desafio13.sql
@@ -2,7 +2,5 @@ SELECT prod.ProductName AS `Produto`,
 prod.Price AS `Preço`
 FROM w3schools.products AS prod
 JOIN w3schools.order_details AS det
-ON prod.ProductID = det.ProductID
-WHERE det.Quantity > 80
-GROUP BY prod.ProductName
+ON det.ProductID = prod.ProductID AND det.Quantity > 80
 ORDER BY prod.ProductName ASC;

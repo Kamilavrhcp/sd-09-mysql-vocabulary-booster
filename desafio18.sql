462a8c10731b4ecbc3f1262374b01cf189b4580a
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-05-25 17:28:43 -0300

    fix requirement 18 and add requirement 20

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT
 CONCAT(T1.FIRST_NAME, ' ', T1.LAST_NAME) AS 'Nome completo',
 DATE_FORMAT(T2.START_DATE, '%d/%m/%Y') AS 'Data de início',
-DATE_FORMAT(T2.END_DATE, '%d/%m/%Y') AS 'Data de recisão',
+DATE_FORMAT(T2.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
 ROUND(DATEDIFF(T2.END_DATE, T2.START_DATE) / 365, 2) AS 'Anos trabalhados'
 FROM hr.employees AS T1
 INNER JOIN hr.job_history AS T2

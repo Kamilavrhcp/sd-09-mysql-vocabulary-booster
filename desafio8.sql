556eb2129a67a2b0fc82293748835fe2d77c2bd6
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-05-25 14:44:17 -0300

    ajusting task8

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
-SELECT c.ContactName as `Nome do contato`, s.ShipperName as `Empresa que fez o envio`, o.OrderDate as `Data do pedido` FROM w3schools.customers as c
+SELECT c.ContactName as `Nome de contato`, s.ShipperName as `Empresa que fez o envio`, o.OrderDate as `Data do pedido` FROM w3schools.customers as c
 INNER JOIN orders AS o ON c.CustomerID = o.CustomerID
 INNER JOIN shippers as s ON o.ShipperID = s.ShipperID
 WHERE s.ShipperName = 'Speedy Express' OR s.ShipperName = 'United Package'
-ORDER BY `Nome do contato` ASC, `Empresa que fez o envio` ASC, `Data do Pedido` ASC;
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do Pedido` ASC;

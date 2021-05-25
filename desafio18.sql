203578ebe027eca4a4c94776fde952695d7a6119
Pablo Fernandes <pabloprates@gmail.com>
2021-05-25 16:20:54 -0300

    Challenge Eighteen

--- desafio18.sql
+++ desafio18.sql
@@ -2,12 +2,6 @@ SELECT
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
     DATE_FORMAT(jh.START_DATE, '%m/%d/%y') AS `Data de início`,
     DATE_FORMAT(jh.END_DATE, '%m/%d/%y') AS `Data de rescisão`,
-    FORMAT(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365,
-        2) AS `Anos trabalhados`
-FROM
-    hr.employees AS e,
-    hr.job_history AS jh
-WHERE
-    jh.JOB_ID = e.JOB_ID
-GROUP BY jh.JOB_ID
+FORMAT(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2) AS `Anos trabalhados`
+FROM hr.job_history AS jh INNER JOIN hr.employees AS e ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
 ORDER BY `Nome completo` ASC , `Anos trabalhados` ASC;

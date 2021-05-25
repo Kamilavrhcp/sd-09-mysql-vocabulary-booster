11830b60df8dd20f8d9740bf097c13b28dfcdc55
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-05-24 22:50:20 -0300

    Requisitos 5 e 6

---
+++ desafio6.sql
@@ -0,0 +1,11 @@
+SELECT CONCAT(first_name, ' ', last_name) as 'Nome completo',
+  job_title as 'Cargo',
+  start_date as 'Data de início do cargo',
+  department_name as 'Departamento'
+FROM hr.job_history AS hist
+  INNER JOIN hr.employees AS emp ON hist.employee_id = emp.employee_id
+  INNER JOIN hr.jobs AS j ON hist.job_id = j.job_id
+  INNER JOIN hr.departments AS dep ON hist.department_id = dep.department_id
+ORDER BY 1 DESC,
+  2 ASC;
+  

c460acc8f5a648fbadc5439c1f99fdb4f074fabc
Luciano_Lodi <lrlodi@gmail.com>
2021-05-25 11:05:25 -0300

    Correcoes de Lint

---
+++ desafio16.sql
@@ -0,0 +1,15 @@
+USE hr;
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(80))
+RETURNS NUMERIC READS SQL DATA
+BEGIN
+DECLARE numberOfJobs NUMERIC;
+SELECT EMPLOYEE_ID AS Jobs FROM job_history AS jh
+INNER JOIN employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE e.EMAIL = email
+INTO numberOfJobs;
+RETURN numberOfJobs;
+END $$
+DELIMITER ;
+
+SELECT buscar_quantidade_de_empregos_por_funcionario(SKING);

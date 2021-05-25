638a0a4a1e72dc0a9f30b844fcb9cc32924b62f9
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-05-25 13:40:55 -0300

    fix requirement 12

--- desafio12.sql
+++ desafio12.sql
@@ -4,7 +4,7 @@ T1.SALARY AS 'Salário funcionário 1',
 T1.PHONE_NUMBER AS 'Telefone funcionário 1',
 CONCAT(T2.FIRST_NAME, ' ', T2.LAST_NAME) AS 'Nome completo funcionário 2',
 T2.SALARY AS 'Salário funcionário 2',
-T2.PHONE_NUMBER AS 'Telefone funcionario 2'
+T2.PHONE_NUMBER AS 'Telefone funcionário 2'
 FROM hr.employees AS T1
 INNER JOIN hr.employees AS T2
 ON T1.JOB_ID = T2.JOB_ID AND T1.EMPLOYEE_ID <> T2.EMPLOYEE_ID

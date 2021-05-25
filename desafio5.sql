da4f08366c5bd0a873daaaae71cc1e6c45fff8a8
cleber-teixeira <cleberlt@gmail.com>
2021-05-24 21:55:34 -0300

    Requisito 5 - Refatoração. :white_check_mark:

--- desafio5.sql
+++ desafio5.sql
@@ -2,4 +2,5 @@ SELECT JOB_TITLE AS Cargo,
 MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
 ROUND((MIN_SALARY / 12), 2) AS `Média mínima mensal`,
 ROUND((MAX_SALARY / 12), 2) AS `Média máxima mensal`
-FROM hr.jobs GROUP BY Cargo ORDER BY 2, 1;
+FROM hr.jobs
+ORDER BY 2, 1;

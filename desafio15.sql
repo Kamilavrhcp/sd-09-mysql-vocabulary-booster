2126bd1f3f5541676622cbed222aafcd1082212f
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-05-25 08:33:33 -0300

    requisito 15 feito

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
+BEGIN
+SELECT TRUNCATE(AVG(SALARY), 2) AS 'Média salarial' FROM hr.employees AS t1
+INNER JOIN hr.jobs AS t2
+ON t1.job_id = t2.job_id
+WHERE t2.job_title = cargo;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');

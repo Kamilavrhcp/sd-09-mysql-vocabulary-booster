abf2e4cdf3cd2988ae9d777423183e2908c4b7c5
Renato Feitosa <pythaqua@gmail.com>
2021-05-25 16:17:56 -0300

    style: Change 16th challenge

--- desafio16.sql
+++ desafio16.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(employee_email VARCHAR(10))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE total_jobs INT;
+DECLARE total_jobs INT;
 SELECT 
     COUNT(*)
 FROM

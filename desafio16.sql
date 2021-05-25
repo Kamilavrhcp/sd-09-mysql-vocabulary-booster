b6d20e181d7ed76fd99ca0affd9f72d90e02fa85
Osvaldo Borges <netoo70@gmail.com>
2021-05-24 22:29:34 -0300

    Refatorando o requisito 16 para function

--- desafio16.sql
+++ desafio16.sql
@@ -1,11 +1,14 @@
 USE hr;
 DELIMITER $$
 
-CREATE PROCEDURE buscar_quantidade_de_empregos_por_funcionario(IN email VARCHAR(50))
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
 BEGIN
-SELECT COUNT(*) AS total_empregos FROM hr.job_history AS jh INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID AND e.EMAIL = email;
+DECLARE total_empregos INT;
+SELECT COUNT(*) FROM hr.job_history AS jh INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID AND e.EMAIL = email INTO total_empregos;
+RETURN total_empregos;
 END $$
 
 DELIMITER ;
 
-CALL buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR');
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR') AS total_empregos;

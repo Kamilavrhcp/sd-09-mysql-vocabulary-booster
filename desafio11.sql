2cbd211588c0be75b5ac5edc823ffbf3e2952188
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-05-25 10:21:57 -0300

    fix requirements 6 and 11

--- desafio11.sql
+++ desafio11.sql
@@ -3,4 +3,5 @@ T1.ContactName AS 'Nome',
 T1.Country AS 'País',
 (SELECT COUNT(*) FROM w3schools.customers WHERE T1.Country = Country) - 1 AS 'Número de compatriotas'
 FROM w3schools.customers AS T1
+WHERE (SELECT COUNT(*) FROM w3schools.customers WHERE T1.Country = Country) - 1 <> 0
 ORDER BY T1.ContactName ASC;

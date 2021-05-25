4b0f2d4a99bebd78d2e408f3a109abf5fd934146
lcgoandete <lcgoandete@hotmail.com>
2021-05-25 14:55:47 -0300

    fix desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -4,6 +4,6 @@ SELECT
     COUNT(customers.ContactName) AS `Número de compatriotas`
 FROM
     w3schools.customers
-        INNER JOIN w3schools.Customers AS t2 ON Customers.Country = t2.Country AND Customers.CustomerID <> t2.CustomerID
+        INNER JOIN w3schools.customers AS t2 ON customers.Country = t2.Country AND customers.CustomerID <> t2.CustomerID
 GROUP BY customers.ContactName , customers.Country
 ORDER BY customers.ContactName;

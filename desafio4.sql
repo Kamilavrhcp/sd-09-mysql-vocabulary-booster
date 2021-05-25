7bd3af05fa607ea5abc81f24b411d60fcd3ec127
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-05-25 07:24:12 -0300

    query 4, attempt 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,10 @@
-SELECT JOB_ID AS "Cargo", ROUND(avg(SALARY), 2) AS "Média salarial",
+SELECT jobs.JOB_TITLE AS "Cargo", ROUND(avg(employees.SALARY), 2) AS "Média salarial",
 CASE 
-WHEN ROUND(avg(SALARY), 2) < 5801 then 'Júnior'
-WHEN ROUND(avg(SALARY), 2) < 7501 then 'Pleno'
-WHEN ROUND(avg(SALARY), 2) < 10501 then 'Sênior'
+WHEN ROUND(avg(employees.SALARY), 2) < 5801 then 'Júnior'
+WHEN ROUND(avg(employees.SALARY), 2) < 7501 then 'Pleno'
+WHEN ROUND(avg(employees.SALARY), 2) < 10501 then 'Sênior'
 else "CEO"
 end as 'Senioridade'
-FROM hr.employees 
-GROUP BY JOB_ID
-order by `Média salarial` asc, Cargo;
+FROM  (hr.jobs, hr.employees) 
+GROUP BY JOB_TITLE
+order by 2, 1 asc;

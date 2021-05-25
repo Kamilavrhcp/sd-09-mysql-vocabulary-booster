44689658b4648bb7a60df78535c4e7ed60ce9f51
johnealves <johne_af@hotmail.com>
2021-05-25 12:07:23 -0300

    conserta desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,5 +1,5 @@
-SELECT c1.ContactName AS Nome, c1.Country AS País, COUNT(*) - 1 AS `Número de compatriotas`
+SELECT c1.ContactName AS Nome, c1.Country AS País, COUNT(*) AS `Número de compatriotas`
 FROM w3schools.customers AS c1, w3schools.customers AS c2
-WHERE c1.Country = c2.Country
+WHERE c1.Country = c2.Country AND c1.ContactName <> c2.ContactName
 GROUP BY c1.ContactName, c1.country
 ORDER BY c1.ContactName;

c0d457be9b2e8fa5bc2ad4f4704815891650e74c
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-05-25 13:13:28 -0300

    Order desafio13 query

--- desafio13.sql
+++ desafio13.sql
@@ -1,4 +1,5 @@
 SELECT p.ProductName, p.Price
 FROM w3schools.products AS p
 INNER JOIN w3schools.order_details AS od ON p.ProductID = od.ProductID
-WHERE od.Quantity > 80;
+WHERE od.Quantity > 80
+ORDER BY p.ProductName;

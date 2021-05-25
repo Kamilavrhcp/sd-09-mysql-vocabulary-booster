519ece8f2f7b278ab4664f82213649acb5bb5f3e
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-05-25 17:45:41 -0300

    Task 07 - Code Climate: TAB DELETED

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT
     UPPER(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
-	jh.START_DATE AS `Data de início`,
+  jh.START_DATE AS `Data de início`,
     e.SALARY AS `Salário`
 FROM
     hr.employees AS e

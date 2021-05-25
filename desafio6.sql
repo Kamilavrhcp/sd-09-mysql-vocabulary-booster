3b76d441c5f8d3935d92b900d9a9b3c4d52667e8
Genadí Reuben <68882475+genadireuben@users.noreply.github.com>
2021-05-24 22:43:50 -0300

    Update desafio6.sql

--- desafio6.sql
+++ desafio6.sql
@@ -2,7 +2,7 @@ SELECT CONCAT(empregados.FIRST_NAME, ' ',
   empregados.LAST_NAME) AS `Nome completo`, 
   cargos.JOB_TITLE AS `Cargo`, 
   `historico`.START_DATE AS `Data de início do cargo`,
- departamentos.DEPARTMENT_NAME AS `Departamento`
+  departamentos.DEPARTMENT_NAME AS `Departamento`
 FROM hr.job_history AS `historico`
 INNER JOIN hr.employees AS empregados ON `historico`.EMPLOYEE_ID = `empregados`.EMPLOYEE_ID 
 INNER JOIN hr.jobs AS cargos ON cargos.JOB_ID = `historico`.JOB_ID		

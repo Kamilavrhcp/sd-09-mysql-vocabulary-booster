063caf1226e5a9dad90e43f5ef16227f9705d787
cleber-teixeira <cleberlt@gmail.com>
2021-05-25 20:04:46 -0300

    Requisito 11 - Refatoração. :white_check_mark:

--- desafio11.sql
+++ desafio11.sql
@@ -1,7 +1,7 @@
-SELECT t1.ContactName AS Nome, t1.Country AS `País`,
-Count(t2.Country) - 1 AS `Número de compatriotas`
-FROM w3schools.customers AS t1,
-w3schools.customers AS t2
+SELECT DISTINCT t1.ContactName AS Nome, t1.Country AS `País`,
+COUNT(t2.Country) - 1  AS `Número de compatriotas`
+FROM w3schools.customers AS t1, w3schools.customers AS t2
 WHERE t1.Country = t2.Country
 GROUP BY t1.ContactName
+HAVING `Número de compatriotas` > 0
 ORDER BY 1;

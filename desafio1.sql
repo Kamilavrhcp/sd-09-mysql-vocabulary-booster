b8735b87b496622386cd61e1957fdcccc105c7a7
LeonardoJosedaSilveira <rpgleo.js@gmail.com>
2021-05-25 02:51:03 -0300

    second attemp

--- desafio1.sql
+++ desafio1.sql
@@ -1,3 +1,4 @@
-SELECT COUNTRY_NAME AS "País",
-IF (REGION_ID = 1, "incluído", "não incluído" ) AS "status Inclusão" 
-FROM hr.countries; 
+SELECT COUNTRY_NAME AS 'País' ,
+IF (REGION_ID = 1, 'incluído', 'não incluído' ) AS 'status Inclusão'
+FROM hr.countries
+order by País; 

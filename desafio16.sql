d69a54a1735cdb3dff1a6561a5f892580d0a4ce0
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-05-25 20:02:14 -0300

    fix challenge 16s indentation

--- desafio16.sql
+++ desafio16.sql
@@ -4,12 +4,12 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(200))
 RETURNS VARCHAR(200) READS SQL DATA
 BEGIN
-	DECLARE quantidade_de_empregos INT;
-    SELECT COUNT(h.JOB_ID) AS 'Quantidade de Empregos'FROM hr.job_history h
-    INNER JOIN hr.employees e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
-    WHERE e.EMAIL = email
-    INTO quantidade_de_empregos;
-    RETURN quantidade_de_empregos;
+DECLARE quantidade_de_empregos INT;
+SELECT COUNT(h.JOB_ID) AS 'Quantidade de Empregos'FROM hr.job_history h
+INNER JOIN hr.employees e ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE e.EMAIL = email
+INTO quantidade_de_empregos;
+RETURN quantidade_de_empregos;
 END $$
 
 DELIMITER ;

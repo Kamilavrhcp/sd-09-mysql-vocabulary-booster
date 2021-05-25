9e3f440283b18cffc1f790494640714352caea38
Pablo Fernandes <pabloprates@gmail.com>
2021-05-25 13:27:27 -0300

    Bonus Challenge Eighteen

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+DATE_FORMAT(jh.START_DATE, "%m/%d/%y") AS `Data de início`,
+DATE_FORMAT(jh.END_DATE, "%m/%d/%y") AS `Data de rescisão`,
+FORMAT(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365, 2) AS `Anos trabalhados`
+FROM hr.employees AS e, hr.job_history AS jh
+WHERE e.JOB_ID = jh.JOB_ID
+ORDER BY `Nome completo` ASC, `Anos trabalhados` ASC;

0582da26071b40d6d8e074c50de8e19e50c0acc2
Pablo Fernandes <pabloprates@gmail.com>
2021-05-25 11:47:18 -0300

    Challenge Sixteen

---
+++ desafio16.sql
@@ -0,0 +1,11 @@
+DELIMITER $$
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE total_jobs INT;
+SELECT COUNT(*)
+FROM hr.employees AS e, hr.job_history AS jh
+WHERE e.EMPLOYEE_ID = jh.EMPLOYEE_ID AND e.EMAIL = email INTO total_jobs;
+RETURN total_jobs;
+END $$
+DELIMITER ;

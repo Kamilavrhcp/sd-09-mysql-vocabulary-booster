c38ac2e9fd7f9ef4a3e693531857851a0b40af80
Pablo Fernandes <pabloprates@gmail.com>
2021-05-25 11:25:24 -0300

    Challenge Fifteen

--- desafio15.sql
+++ desafio15.sql
@@ -1,11 +1,9 @@
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN job_name VARCHAR(50))
 BEGIN
-SELECT REPLACE(FORMAT(AVG(e.salary), 2), ',', '')
+SELECT REPLACE(FORMAT(AVG(e.salary), 2), ',', '') AS 'Média salarial'
 FROM hr.employees AS e
 WHERE
 e.JOB_ID IN (SELECT j.JOB_ID FROM jobs AS j WHERE j.JOB_TITLE = job_name);
 END $$
 DELIMITER ;
-
-CALL buscar_media_por_cargo('Programmer');

b52cba0dd0ed0003f37a733fe9f44f16c6e80347
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-05-25 18:49:10 -0300

    fix stylelint req6

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,19 @@
 SELECT 
-	CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
     J.JOB_TITLE AS `Cargo`,
     JH.START_DATE AS `Data de início de cargo`,
     D.DEPARTMENT_NAME AS `Departamento`
-    FROM hr.employees as E
-    INNER JOIN hr.jobs AS J ON E.JOB_ID = J.JOB_ID
-    INNER JOIN hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
-    INNER JOIN hr.departments AS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
-    WHERE E.EMPLOYEE_ID IN (SELECT EMPLOYEE_ID FROM hr.job_history) ORDER BY `Nome completo` DESC, `Cargo` ASC;
+FROM
+    hr.employees AS E
+        INNER JOIN
+    hr.jobs AS J ON E.JOB_ID = J.JOB_ID
+        INNER JOIN
+    hr.job_history AS JH ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+        INNER JOIN
+    hr.departments AS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+WHERE
+    E.EMPLOYEE_ID IN (SELECT 
+            EMPLOYEE_ID
+        FROM
+            hr.job_history)
+ORDER BY `Nome completo` DESC , `Cargo` ASC;

795de83174c4e1d23e6a617c9badcf5c4f12f2d0
Pablo Fernandes <pabloprates@gmail.com>
2021-05-25 14:22:53 -0300

    Challenge Eleven

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,10 @@
 SELECT
-c.ContactName AS `Nome`, c2.Country AS `País`, (COUNT(*) - 1) AS `Número de compatriotas`
+c.ContactName AS `Nome`, c2.Country AS `País`, COUNT(*) AS `Número de compatriotas`
 FROM
 w3schools.customers AS c, w3schools.customers AS c2
 WHERE
 c.Country = c2.Country
+AND
+c.CustomerID <> c2.CustomerID
 GROUP BY c2.Country, c.ContactName
 ORDER BY c.ContactName ASC;

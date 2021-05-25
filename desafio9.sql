f453c0a9e2004ac4b4b65e20c82b3b847a9a56ae
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-05-25 19:08:27 -0300

    Lint

--- desafio9.sql
+++ desafio9.sql
@@ -1,5 +1,5 @@
 SELECT CONCAT(FirstName, ' ', LastName) AS `Nome completo`,
-	COUNT(*) AS `Total de pedidos`
+COUNT(*) AS `Total de pedidos`
 FROM w3schools.employees AS we
 INNER JOIN w3schools.orders AS wo ON we.EmployeeID = wo.EmployeeID
 GROUP BY `Nome completo`

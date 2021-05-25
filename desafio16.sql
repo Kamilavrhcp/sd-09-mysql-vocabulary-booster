15ee6a1323fbd9c12177f6be02a7ec8be291b651
Luciano_Lodi <lrlodi@gmail.com>
2021-05-25 19:43:42 -0300

    Requisitos 16 e 18

--- desafio16.sql
+++ desafio16.sql
@@ -1,7 +1,7 @@
 USE hr;
 DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(80))
-RETURNS NUMERIC READS SQL DATA
+RETURNS INT READS SQL DATA
 BEGIN
 DECLARE numberOfJobs INT;
 SELECT COUNT(jh.EMPLOYEE_ID) AS Jobs FROM job_history AS jh

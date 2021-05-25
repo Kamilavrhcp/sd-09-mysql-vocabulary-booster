9ae5b0f1085f7aa23a379e4b19af19e601dfc1a9
cleber-teixeira <cleberlt@gmail.com>
2021-05-25 16:39:02 -0300

    Requisito 11 - Exibir os clientes que possuem compatriotas e a quantidade. :white_check_mark:

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT t1.ContactName AS Nome, t1.Country AS `País`,
+Count(t2.Country) - 1 AS `Número de compatriotas`
+FROM w3schools.customers AS t1,
+w3schools.customers AS t2
+WHERE t1.Country = t2.Country
+GROUP BY t1.ContactName
+ORDER BY 1;

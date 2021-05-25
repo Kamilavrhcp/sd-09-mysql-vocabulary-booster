fb0e50f8ce441c70a71afd9b642015092ac37db5
Arthur Nunes <coletivosuburbano@gmail.com>
2021-05-24 23:56:43 -0300

    12

--- desafio12.sql
+++ desafio12.sql
@@ -1,18 +1,22 @@
 SELECT 
-    CONCAT(employee1.FIRST_NAME,
-            ' ',
-            employee1.LAST_NAME) AS 'Nome completo funcionário 1',
-    employee1.SALARY AS 'Salário funcionário 1',
-    employee1.PHONE_NUMBER AS 'Telefone funcionário 1',
-    CONCAT(employee2.FIRST_NAME,
-            ' ',
-            employee2.LAST_NAME) AS 'Nome completo funcionário 2',
-    employee2.SALARY AS 'Salário funcionário 2',
-    employee2.PHONE_NUMBER AS 'Telefone funcionário 2'
-FROM
-    hr.employees AS employee1,
-    hr.employees AS employee2
-WHERE
-    employee1.JOB_ID = employee2.JOB_ID
-        AND employee1.EMPLOYEE_ID != employee2.EMPLOYEE_ID
-ORDER BY 1 ASC , 2 ASC;
+  CONCAT(
+    employee1.`FIRST_NAME`, ' ', employee1.`LAST_NAME`
+  ) AS `Nome completo funcionário 1`, 
+  employee1.`SALARY` AS `Salário funcionário 1`, 
+  employee1.`PHONE_NUMBER` AS `Telefone funcionário 1`, 
+  CONCAT(
+    EMP2.`FIRST_NAME`, ' ', employee2.`LAST_NAME`
+  ) AS `Nome completo funcionário 2`, 
+  employee2.`SALARY` AS `Salário funcionário 2`, 
+  employee2.`PHONE_NUMBER` AS `Telefone funcionário 2` 
+FROM 
+  hr.employees AS employee1 
+  INNER JOIN hr.employees AS employee2 ON employee2.`JOB_ID` = employee1.`JOB_ID` 
+  AND CONCAT(
+    employee2.`FIRST_NAME`, ' ', employee2.`LAST_NAME`
+  ) != CONCAT(
+    employee1.`FIRST_NAME`, ' ', employee1.`LAST_NAME`
+  ) 
+ORDER BY 
+  `Nome completo funcionário 1` ASC, 
+  `Nome completo funcionário 2` ASC;

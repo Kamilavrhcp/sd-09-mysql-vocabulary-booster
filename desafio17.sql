54621443c803722a7b8297a3f40b4436c07e8396
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-05-25 10:02:13 -0300

    corrigindo issues do lint

--- desafio17.sql
+++ desafio17.sql
@@ -2,10 +2,10 @@ USE w3schools;
 DELIMITER $$
 
 CREATE TRIGGER update_order_date
-	BEFORE INSERT ON w3schools.orders
-	FOR EACH ROW
+BEFORE INSERT ON w3schools.orders
+FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = NOW();
+SET NEW.OrderDate = NOW();
 END $$
 
 DELIMITER ;

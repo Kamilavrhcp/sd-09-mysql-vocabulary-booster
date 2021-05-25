9d46a47d2d600b510e3d5a212e603cb0e9460042
Raquel Picanço <picanco.raquel@gmail.com>
2021-05-24 20:01:13 -0400

    Let CC wars begin

--- desafio11.sql
+++ desafio11.sql
@@ -5,13 +5,13 @@ SELECT
 FROM
   w3schools.customers c1
 LEFT JOIN (
-    SELECT
-	    c.Country as 'country',
-      COUNT(c.Country) as 'contagem'
-    FROM
-      w3schools.customers c
-    GROUP BY c.Country
-    HAVING `contagem` > 1
-  ) c_join ON c1.Country = c_join.Country
+  SELECT
+	  c.Country as 'country',
+    COUNT(c.Country) as 'contagem'
+  FROM
+    w3schools.customers c
+  GROUP BY c.Country
+  HAVING `contagem` > 1
+) c_join ON c1.Country = c_join.Country
 WHERE c_join.contagem IS NOT NULL
 ORDER BY `Nome`;

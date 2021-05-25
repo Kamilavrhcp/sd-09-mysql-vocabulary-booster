93dfbc8a9d9e90d13c17499d8b54cbcc3dc1b444
Raquel Picanço <picanco.raquel@gmail.com>
2021-05-25 06:39:58 -0400

    Solve challenge 12

--- desafio11.sql
+++ desafio11.sql
@@ -1,16 +1,16 @@
 SELECT
-    c1.ContactName AS 'Nome',
-    c1.Country AS 'País',
-    c_join.contagem - 1 AS 'Número de compatriotas'
+  c1.ContactName AS 'Nome',
+  c1.Country AS 'País',
+  c_join.contagem - 1 AS 'Número de compatriotas'
 FROM
-    w3schools.customers c1
-        LEFT JOIN
-    (SELECT
-        c.Country AS 'country', COUNT(c.Country) AS 'contagem'
-    FROM
-        w3schools.customers c
-    GROUP BY c.Country
-    HAVING contagem > 1) c_join ON c1.Country = c_join.Country
+  w3schools.customers c1
+    LEFT JOIN
+  (SELECT
+    c.Country AS 'country', COUNT(c.Country) AS 'contagem'
+  FROM
+    w3schools.customers c
+  GROUP BY c.Country
+  HAVING contagem > 1) c_join ON c1.Country = c_join.Country
 WHERE
-    c_join.contagem IS NOT NULL
+  c_join.contagem IS NOT NULL
 ORDER BY Nome;

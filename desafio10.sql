d0c4d25d942ac7661018240eb2a872ebda69c52c
Renato Feitosa <pythaqua@gmail.com>
2021-05-25 13:56:56 -0300

    Add 11th challenge

--- desafio10.sql
+++ desafio10.sql
@@ -1,11 +1,11 @@
 SELECT 
-    p.ProductName as 'Produto',
-    MIN(o.Quantity) as 'Mínima',
-    MAX(o.Quantity) as 'Máxima',
-    ROUND(AVG(o.Quantity), 2) as 'Média'
-FROM w3schools.order_details as o
+    p.ProductName AS 'Produto',
+    MIN(o.Quantity) AS 'Mínima',
+    MAX(o.Quantity) AS 'Máxima',
+    ROUND(AVG(o.Quantity), 2) AS 'Média'
+FROM w3schools.order_details AS o
     INNER JOIN 
-    w3schools.products as p ON p.ProductID = o.ProductID
+    w3schools.products AS p ON p.ProductID = o.ProductID
     GROUP BY `Produto`
     HAVING `Média` > 20
     ORDER BY `Média`, `Produto`;

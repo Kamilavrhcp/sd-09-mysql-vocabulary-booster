8b21db1a58eb8248d4412992dd53b44f9c0b61e4
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-05-25 13:25:54 -0300

    requirements 12 and 15

---
+++ desafio15.sql
@@ -0,0 +1,14 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
+BEGIN 
+SELECT ROUND(AVG(T1.SALARY), 2) AS 'Média Salarial'
+FROM hr.employees AS T1
+INNER JOIN hr.jobs AS T2
+ON T1.JOB_ID = T2.JOB_ID
+WHERE T2.JOB_TITLE = cargo;
+END $$
+
+DELIMITER ;

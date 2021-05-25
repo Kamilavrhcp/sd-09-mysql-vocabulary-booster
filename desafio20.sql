30c1ccadf1d8f34af10ac89f9b81fc23d7458921
Pablo Fernandes <pabloprates@gmail.com>
2021-05-25 17:18:50 -0300

    Challenge Twenty

---
+++ desafio20.sql
@@ -0,0 +1,20 @@
+DELIMITER $$
+CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(50))
+BEGIN
+SELECT
+CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
+d.DEPARTMENT_NAME AS `Departamento`,
+j.JOB_TITLE AS `Cargo`
+FROM
+hr.employees AS e, hr.departments d, hr.job_history AS jh, hr.jobs AS j
+WHERE
+jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+AND
+e.DEPARTMENT_ID = d.DEPARTMENT_ID
+AND
+jh.JOB_ID = j.JOB_ID
+AND
+e.EMAIL = email
+ORDER BY `Departamento` ASC, `Cargo` ASC;
+END$$
+DELIMITER ;

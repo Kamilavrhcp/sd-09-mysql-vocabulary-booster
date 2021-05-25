0f910c9f70713196daca30f0321f52513bc1045e
lcgoandete <lcgoandete@hotmail.com>
2021-05-25 14:40:56 -0300

    fix desafio 7, desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -2,7 +2,7 @@ SELECT
     products.ProductName AS Produto,
     MIN(order_details.Quantity) AS Mínima,
     MAX(order_details.Quantity) AS Máxima,
-    ROUND(AVG(order_details.Quantity), 2) Média
+    ROUND(AVG(order_details.Quantity), 2) AS Média
 FROM
     w3schools.order_details
         INNER JOIN w3schools.products ON products.ProductID = order_details.ProductID

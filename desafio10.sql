48428735a610f415844f4b475cb907a163391ec0
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-05-25 19:45:59 -0300

    Requisitos 9 e 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT 
+    ProductName AS 'Produto',
+    MIN(Quantity) AS 'Mínima',
+    MAX(Quantity) AS 'Máxima',
+    ROUND(AVG(Quantity), 2) AS 'Média'
+FROM
+    w3schools.order_details AS ordr
+        INNER JOIN
+    w3schools.products AS prod ON ordr.ProductID = prod.ProductID
+GROUP BY ProductName
+ORDER BY ROUND(AVG(Quantity), 2) , ProductName ASC;
--- desafio9.sql
+++ desafio9.sql
@@ -1,1 +1,9 @@
-SELECT
+SELECT 
+    CONCAT(FirstName, ' ', LastName) AS 'Nome completo',
+    COUNT(*) AS 'Total de pedidos'
+FROM
+    w3schools.orders AS ords
+        INNER JOIN
+    w3schools.employees AS emp ON ords.EmployeeID = emp.EmployeeID
+GROUP BY CONCAT(FirstName, ' ', LastName)
+ORDER BY COUNT(*) ASC;

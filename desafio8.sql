f38ca602213429e0c60f867ee0c1d5ce8c320032
edison-ps <edison@osite.com.br>
2021-05-25 19:56:17 -0300

    Desafio 08 V1.00

---
+++ desafio8.sql
@@ -0,0 +1,19 @@
+SELECT 
+(
+SELECT ContactName
+FROM w3schools.customers
+WHERE CustomerID = ord.CustomerID
+) AS 'Nome de contato',
+(
+SELECT ShipperName
+FROM w3schools.shippers AS ship
+WHERE ShipperID = ord.ShipperID
+) AS 'Empresa que fez o envio',
+OrderDate AS 'Data do pedido'
+FROM w3schools.orders AS ord
+WHERE ord.ShipperID IN (
+SELECT ShipperID
+FROM w3schools.shippers
+WHERE ShipperName = 'Speedy Express' OR ShipperName = 'United Package'
+)
+ORDER BY `Nome de contato`, `Empresa que fez o envio`;

bc0f253745d36dec97d2c66dbd11c0cde80a5fd5
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-05-24 22:51:30 -0300

    FIX: Task 02 and 03

--- desafio2.sql
+++ desafio2.sql
@@ -5,7 +5,7 @@ SELECT
         WHEN MAX_SALARY BETWEEN 10001 AND 20000 THEN 'Médio'
         WHEN MAX_SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
         ELSE  'Altíssimo'
-    END AS 'Nível'
+    END AS `Nível`
 FROM
     hr.jobs
-ORDER BY 'Cargo' ASC;
+ORDER BY `Cargo` ASC;
--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,6 @@
 SELECT
-    JOB_TITLE AS 'Cargo',
-    MAX_SALARY - MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
+    JOB_TITLE AS `Cargo`,
+    (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
 FROM
     hr.jobs
-ORDER BY 2 DESC , 1 ASC;
+ORDER BY 2 , 1 ASC;

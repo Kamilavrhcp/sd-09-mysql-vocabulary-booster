ac9f73340e05b311cfa9cf541c5092b45a7280b0
Pablo Fernandes <pabloprates@gmail.com>
2021-05-25 11:03:35 -0300

    Challenge Fifteen

---
+++ desafio15.sql
@@ -0,0 +1,9 @@
+DELIMITER $$
+CREATE PROCEDURE buscar_media_por_cargo(IN job_name VARCHAR(50), OUT salary_avg VARCHAR(10))
+BEGIN
+SELECT REPLACE(FORMAT(AVG(e.salary), 2), ',', '') INTO salary_avg
+FROM hr.employees AS e
+WHERE
+e.JOB_ID IN (SELECT j.JOB_ID FROM jobs AS j WHERE j.JOB_TITLE = job_name);
+END $$
+DELIMITER ;

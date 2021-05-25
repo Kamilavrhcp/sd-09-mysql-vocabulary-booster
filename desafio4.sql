f5c39ea3f69222f7d42478cdd8f8a5853028f4aa
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-05-25 14:32:49 -0300

    Task 04

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,15 @@
-SELECT
-    JOB_TITLE AS `Cargo`,
-    (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
+SELECT 
+    J.JOB_TITLE AS 'Cargo',
+    ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
+    CASE  
+		WHEN ROUND(AVG(e.SALARY), 2) between 2000 AND 5800 THEN 'Júnior'
+        WHEN ROUND(AVG(e.SALARY), 2) between 5801 AND 7500 THEN 'Pleno'
+        WHEN ROUND(AVG(e.SALARY), 2) between 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS `Senioridade`
 FROM
-    hr.jobs
-ORDER BY 2 , 1 ASC;
+    hr.jobs AS J
+        INNER JOIN
+    hr.employees AS e ON J.JOB_ID = e.JOB_ID
+GROUP BY J.JOB_TITLE
+ORDER BY `Média salarial` , `Cargo`;

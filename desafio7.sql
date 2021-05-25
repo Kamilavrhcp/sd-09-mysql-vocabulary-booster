3317a855fbc461f8531f10bd65679000294eb60e
cleber-teixeira <cleberlt@gmail.com>
2021-05-25 10:16:30 -0300

    Requisito 7 - Fazer um relatório com o salário dos empregados que iniciaram entre os meses de janeiro e março. :white_check_mark:

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT UCASE(CONCAT(e.FIRST_NAME, ' ',
+e.LAST_NAME)) AS `Nome completo`,
+DATE(h.START_DATE) AS `Data de início`,
+e.SALARY AS `Salário`
+FROM hr.job_history AS h
+INNER JOIN hr.employees AS e
+ON h.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(h.START_DATE) BETWEEN 01 AND 03
+ORDER BY 1, 2;

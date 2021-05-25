354e49ad1e5e9b881ad55a663287577d91070282
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-05-25 16:21:18 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT ContactName AS `Nome de contato`,
+    ws.ShipperName AS `Empresa que fez o envio`,
+    wo.OrderDate AS `Data do pedido`
+FROM w3schools.customers AS wc
+    INNER JOIN w3schools.orders AS wo ON wc.CustomerID = wo.CustomerID
+    INNER JOIN w3schools.shippers AS ws ON ws.ShipperID = wo.ShipperID
+    AND ws.ShipperName IN ('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

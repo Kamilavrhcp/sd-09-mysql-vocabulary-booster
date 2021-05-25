0d9ec6b3435ae3a69ff6d1a5b6f3ec66d8058aee
Renato Feitosa <pythaqua@gmail.com>
2021-05-24 22:17:07 -0300

    Change 1st challenge

--- desafio1.sql
+++ desafio1.sql
@@ -1,9 +1,8 @@
 SELECT 
     COUNTRY_NAME AS 'País',
-    CASE
-		WHEN REGION_ID = 1 THEN 'incluído'
-        ELSE 'não incluído'
-	END AS 'Status Inclusão'
+    IF(REGION_ID = 1,
+        'incluído',
+        'não incluído') AS 'Status Inclusão'
 FROM
     hr.countries
 ORDER BY COUNTRY_NAME;

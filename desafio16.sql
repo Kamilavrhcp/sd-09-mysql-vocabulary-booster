54d76b2d73f3ab85ea16ebc99dfe0aee0d398f51
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-05-25 09:52:21 -0300

    requisito 16 feito

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(usermail VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE result INT;
+SELECT COUNT(*) FROM hr.job_history AS t1
+INNER JOIN hr.employees AS t2
+ON t1.employee_id = t2.employee_id
+WHERE t2.email = usermail INTO result;
+RETURN result;
+END $$
+
+DELIMITER ;
+
+SELECT BUSCAR_QUANTIDADE_DE_EMPREGOS_POR_FUNCIONARIO('NKOCHHAR') INTO result;

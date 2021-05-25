0f5819eceb258fae2d3b03f16d574222d33a273d
marcusWittho <witthoeft.mcs@hotmail.com>
2021-05-25 16:56:32 -0300

    Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,17 @@
+SELECT 
+    ContactName AS 'Nome de contato',
+    ShipperName AS 'Empresa que fez o envio',
+    OrderDate AS 'Data do pedido'
+FROM
+    w3schools.customers AS c
+
+INNER JOIN
+w3schools.orders AS o ON c.CustomerID = o.CustomerID
+
+INNER JOIN
+w3schools.shippers AS s ON o.ShipperID = s.ShipperID
+
+WHERE s.ShipperID <> 3
+
+ORDER BY ContactName, ShipperName;
+    

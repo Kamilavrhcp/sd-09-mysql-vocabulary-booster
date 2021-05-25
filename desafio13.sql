670cd45ec611213d570d4456f7d22109b7b81d73
Felipe Cardoso <felipedclc@gmail.com>
2021-05-25 11:55:18 -0300

    tirando espacos para code climate

--- desafio13.sql
+++ desafio13.sql
@@ -1,11 +1,11 @@
 /* Exibe todos produtos que já tiveram um pedido associado requerindo uma quantidade desse produto maior que 80 */
 
-SELECT 
+SELECT
     ProductName AS `Produto`, Price AS `Preço`
 FROM
     w3schools.products
 WHERE
-    ProductID IN (SELECT 
+    ProductID IN (SELECT
             ProductID
         FROM
             w3schools.order_details

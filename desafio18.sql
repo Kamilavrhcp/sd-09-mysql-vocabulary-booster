45935e9919e73ae593be6827aa760b3d436f78f9
unknown <nicolasgodek@gmail.com>
2021-05-25 19:21:50 -0300

    fix requisito 18

--- desafio18.sql
+++ desafio18.sql
@@ -1,20 +1,12 @@
 SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-    CONCAT(DAY(jb.START_DATE),
-            '/',
-            MONTH(jb.START_DATE),
-            '/',
-            YEAR(jb.START_DATE)) AS 'Data de início',
-    CONCAT(DAY(jb.END_DATE),
-            '/',
-            MONTH(jb.END_DATE),
-            '/',
-            YEAR(jb.END_DATE)) AS 'Data de rescisão',
-    ROUND((DATEDIFF(jb.END_DATE, jb.START_DATE)) / 365,
+    DATE_FORMAT(jh.START_DATE, '%d/%m/%Y') AS 'Data de início',
+    DATE_FORMAT(jh.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+    ROUND((DATEDIFF(jh.END_DATE, jh.START_DATE)) / 365,
             2) AS 'Anos trabalhados'
 FROM
     hr.employees AS e
         INNER JOIN
-    job_history AS jb ON jb.EMPLOYEE_ID = e.EMPLOYEE_ID
-ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) , ROUND((DATEDIFF(jb.END_DATE, jb.START_DATE)) / 365,
+    hr.job_history AS jh ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+ORDER BY CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) , ROUND((DATEDIFF(jh.END_DATE, jh.START_DATE)) / 365,
         2);

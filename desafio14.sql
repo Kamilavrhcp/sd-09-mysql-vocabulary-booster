80cd0ed5b4dc8cf8378f3d32cd68b7a26f15a475
Felipe Cardoso <felipedclc@gmail.com>
2021-05-25 11:57:32 -0300

    code climate

--- desafio14.sql
+++ desafio14.sql
@@ -1,7 +1,12 @@
 /* Exiba os 05 primeiros países distintos dentre os consumidores e empresas fornecedoras, em ordem alfabética. */
 
-SELECT Country AS `País` FROM w3schools.customers
-UNION
-SELECT Country AS `País` FROM w3schools.suppliers
+SELECT 
+    Country AS `País`
+FROM
+    w3schools.customers 
+UNION SELECT 
+    Country AS `País`
+FROM
+    w3schools.suppliers
 ORDER BY `País` ASC
 LIMIT 5;

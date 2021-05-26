d0b0dd35271d94420ca0d233bd34f8de6112b226
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-05-25 22:25:22 -0300

    Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,7 @@
+SELECT wc.ContactName AS 'Nome',
+	wc.Country AS `País`,
+  COUNT(wc.ContactName) AS 'Número de compatriotas'
+FROM w3schools.customers AS wc, w3schools.customers AS wc2
+WHERE wc.Country = wc2.Country AND wc.ContactName <> wc2.ContactName
+GROUP BY `Nome`, `País`
+ORDER BY `Nome`;

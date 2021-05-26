b98a89f3997d1ce2f72668df7deaa814aa27deba
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-05-26 19:28:27 -0300

    Requisito 12 Ajuste

--- desafio12.sql
+++ desafio12.sql
@@ -1,14 +1,14 @@
 SELECT
-    CONCAT(hEmp.first_name, ' ', hEmp.last_name) AS Nome completo funcionário 1,
-    hEmp.SALARY AS Salário funcionário 1,
-    hEmp.PHONE_NUMBER AS Telefone funcionário 1,
-    CONCAT(hEmp2.first_name, ' ', hEmp2.last_name) AS Nome completo funcionário 2,
-    hEmp2.SALARY AS Salário funcionário 2,
-    hEmp2.PHONE_NUMBER AS Telefone funcionário 2
+    CONCAT(hEmp.first_name, ' ', hEmp.last_name) AS `Nome completo funcionário 1`,
+    hEmp.SALARY AS `Salário funcionário 1`,
+    hEmp.PHONE_NUMBER AS `Telefone funcionário 1`,
+    CONCAT(hEmp2.first_name, ' ', hEmp2.last_name) AS `Nome completo funcionário 2`,
+    hEmp2.SALARY AS `Salário funcionário 2`,
+    hEmp2.PHONE_NUMBER AS `Telefone funcionário 2`
 FROM
     hr.employees AS hEmp,
     hr.employees AS hEmp2
 WHERE
     hEmp.JOB_ID = hEmp2.JOB_ID
         AND hEmp.EMPLOYEE_ID <> hEmp2.EMPLOYEE_ID
-ORDER BY Nome completo funcionário 1 , Nome completo funcionário 2;
+ORDER BY `Nome completo funcionário 1` , `Nome completo funcionário 2`;

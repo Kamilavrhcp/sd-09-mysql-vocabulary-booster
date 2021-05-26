31567b854e4d655a3a007bcf6cb9e635b13fbe19
brenno-calado <brenno.calado@gmail.com>
2021-05-26 13:31:03 -0300

    desafio12 using self join

--- desafio12.sql
+++ desafio12.sql
@@ -1,17 +1,11 @@
 SELECT
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo funcionário 1',
     e.SALARY AS 'Salário funcionário 1',
-    e.PHONE_NUMBER AS 'Telefone funcionário 1'
-    -- CONCAT(empl.FIRST_NAME, ' ', empl.LAST_NAME) AS 'Nome completo funcionário 2',
---     empl.SALARY AS 'Salário funcionário 2',
---     empl.PHONE_NUMBER AS 'Telefone funcionário 2'
+    e.PHONE_NUMBER AS 'Telefone funcionário 1',
+    CONCAT(empl.FIRST_NAME, ' ', empl.LAST_NAME) AS 'Nome completo funcionário 2',
+    empl.SALARY AS 'Salário funcionário 2',
+    empl.PHONE_NUMBER AS 'Telefone funcionário 2'
     FROM hr.employees AS e,
     hr.employees AS empl
-    WHERE e.JOB_ID = empl.JOB_ID
-    GROUP BY `Nome completo funcionário 1`, e.SALARY, job, e.PHONE_NUMBER 
-    ORDER BY job, `Nome completo funcionário 1`;
-    
-SELECT COUNT(*) AS 'amount'
-FROM hr.employees
-GROUP BY employees.JOB_ID
-HAVING `amount` > 1;
+    WHERE e.JOB_ID = empl.JOB_ID AND e.EMPLOYEE_ID <> empl.EMPLOYEE_ID
+    ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;

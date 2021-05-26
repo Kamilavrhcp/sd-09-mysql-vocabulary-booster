206a086245022251dbaa788d67818332aabf6c0c
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-05-26 10:06:21 -0300

    Add challenge 18

--- desafio16.sql
+++ desafio16.sql
@@ -2,7 +2,7 @@ USE hr;
 DELIMITER $$
 
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(200))
-RETURNS VARCHAR(200) READS SQL DATA
+RETURNS INT READS SQL DATA
 BEGIN
 DECLARE quantidade_de_empregos INT;
 SELECT COUNT(h.JOB_ID) AS 'Quantidade de Empregos'FROM hr.job_history h

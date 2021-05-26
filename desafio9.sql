fd54cc9c95e25a24c8a4dd66da2e4edbd2697f41
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-05-26 13:43:24 -0300

    Desafio 9

--- desafio9.sql
+++ desafio9.sql
@@ -1,10 +1,9 @@
 SELECT 
     CONCAT(emp.FirstName, ' ', emp.LastName) AS 'Nome completo',
-    o.EmployeeID AS 'Total de pedidos'
+    COUNT(o.EmployeeID) AS 'Total de pedidos'
 FROM
     w3schools.employees AS emp
         INNER JOIN
     w3schools.orders AS o ON emp.EmployeeID = o.EmployeeID
 GROUP BY o.EmployeeID
-HAVING COUNT(o.EmployeeID)
-ORDER BY 2 DESC;
+ORDER BY 2 ASC;

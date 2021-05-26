832a5a43fe5dfc073e38ce8e589d80aeb150ff85
edison-ps <edison@osite.com.br>
2021-05-26 12:14:21 -0300

    Desafio 11 V1.02

--- desafio11.sql
+++ desafio11.sql
@@ -1,12 +1,12 @@
 SELECT 
-	cust.ContactName AS `Nome`, 
-    cust.Country AS 'País',
-    (
-		SELECT COUNT(*) - 1
-        FROM w3schools.customers
-        WHERE Country = cust.Country
-        GROUP BY Country
-    ) AS 'Número de compatriotas'
+cust.ContactName AS `Nome`, 
+cust.Country AS 'País',
+(
+SELECT COUNT(*) - 1
+FROM w3schools.customers
+WHERE Country = cust.Country
+GROUP BY Country
+) AS 'Número de compatriotas'
 FROM w3schools.customers AS cust
 HAVING `Número de compatriotas` > 0
 ORDER BY cust.ContactName;

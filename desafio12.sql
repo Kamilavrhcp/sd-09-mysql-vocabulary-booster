ac833ff2e4d41d2c9d194f3cc86f10702bbff9e8
Pedro Henrique <pedroenrique.400@gmail.com>
2021-05-26 16:45:47 -0300

    Faz até o req 12 and fix

--- desafio12.sql
+++ desafio12.sql
@@ -1,9 +1,7 @@
 SELECT 
-    e.JOB_ID,
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo funcionário 1`,
     e.SALARY AS 'Salário funcionário 1',
     e.PHONE_NUMBER AS 'Telefone funcionário 1',
-    e2.JOB_ID,
     CONCAT(e2.FIRST_NAME, ' ', e2.LAST_NAME) AS `Nome completo funcionário 2`,
     e2.SALARY AS 'Salário funcionário 2',
     e2.PHONE_NUMBER AS 'Telefone funcionário 2'

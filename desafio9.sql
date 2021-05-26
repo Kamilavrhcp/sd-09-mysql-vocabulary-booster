5567c27afbc32b2a56675e9f96c063da396a0d2b
edison-ps <edison@osite.com.br>
2021-05-25 21:39:03 -0300

    Desafio 09 V1.01

--- desafio9.sql
+++ desafio9.sql
@@ -1,10 +1,10 @@
 SELECT 
-	(
-		SELECT CONCAT(FirstName, ' ', LastName)
-        FROM w3schools.employees
-        WHERE EmployeeID = ord.EmployeeID
-    ) AS 'Nome completo',
-	COUNT(*) AS 'Total de pedidos'
- FROM w3schools.orders AS ord
- GROUP BY `Nome completo`
- ORDER BY `Total de pedidos`;
+(
+SELECT CONCAT(FirstName, ' ', LastName)
+FROM w3schools.employees
+WHERE EmployeeID = ord.EmployeeID
+) AS 'Nome completo',
+COUNT(*) AS 'Total de pedidos'
+FROM w3schools.orders AS ord
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;

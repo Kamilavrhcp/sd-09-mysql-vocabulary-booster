6b099fd90120b6741a4361421b0311bfc00a6f02
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-05-26 12:27:41 -0300

    Desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,10 @@
+SELECT 
+    CONCAT(emp.LastName, ' ', emp.FirstName) AS 'Nome completo',
+    o.EmployeeID AS 'Total de pedidos'
+FROM
+    w3schools.employees AS emp
+        INNER JOIN
+    w3schools.orders AS o ON emp.EmployeeID = o.EmployeeID
+GROUP BY o.EmployeeID
+HAVING COUNT(o.EmployeeID)
+ORDER BY 2 DESC;

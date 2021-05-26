23f5970ef894418724079c1f490463d5875f52ea
Antonio Schappo <antonioschappo@gmail.com>
2021-05-26 15:02:18 -0300

    fourth try challenge 5

--- desafio5.sql
+++ desafio5.sql
@@ -3,4 +3,4 @@ ROUND((MAX_SALARY - MIN_SALARY), 2) AS 'Variação Salarial',
 ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal',
 ROUND(MAX_SALARY/12, 2) AS 'Média máxima mensal'
 FROM hr.jobs
-ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
+ORDER BY ROUND((MAX_SALARY - MIN_SALARY), 2), JOB_TITLE;

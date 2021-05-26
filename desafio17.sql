94d6bf69b6932709fb1fdeefd2c6653f4e81274f
Luciano_Lodi <lrlodi@gmail.com>
2021-05-26 19:21:04 -0300

    Correcoes de CC

--- desafio17.sql
+++ desafio17.sql
@@ -2,10 +2,10 @@ USE w3schools;
 DELIMITER $$
 
 CREATE TRIGGER insert_new_order_date
-	BEFORE INSERT ON orders
-    FOR EACH ROW
+BEFORE INSERT ON orders
+FOR EACH ROW
 BEGIN
-	SET NEW.OrderDate = DATE(NOW());
+SET NEW.OrderDate = DATE(NOW());
 END $$
 
 DELIMITER ;

ff15a64d2b0b374751067e03885664006ad74635
brenno-calado <brenno.calado@gmail.com>
2021-05-26 13:33:23 -0300

    beautify 12

--- desafio12.sql
+++ desafio12.sql
@@ -1,11 +1,14 @@
-SELECT
+SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo funcionário 1',
     e.SALARY AS 'Salário funcionário 1',
     e.PHONE_NUMBER AS 'Telefone funcionário 1',
     CONCAT(empl.FIRST_NAME, ' ', empl.LAST_NAME) AS 'Nome completo funcionário 2',
     empl.SALARY AS 'Salário funcionário 2',
     empl.PHONE_NUMBER AS 'Telefone funcionário 2'
-    FROM hr.employees AS e,
+FROM
+    hr.employees AS e,
     hr.employees AS empl
-    WHERE e.JOB_ID = empl.JOB_ID AND e.EMPLOYEE_ID <> empl.EMPLOYEE_ID
-    ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;
+WHERE
+    e.JOB_ID = empl.JOB_ID
+        AND e.EMPLOYEE_ID <> empl.EMPLOYEE_ID
+ORDER BY `Nome completo funcionário 1` , `Nome completo funcionário 2`;

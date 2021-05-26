44c0cbddcc04e5520e7645864011f228c96c98ee
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-05-26 05:50:57 -0300

    desafio 6

---
+++ desafio6.sql
@@ -0,0 +1,14 @@
+SELECT 
+    CONCAT(e.first_name, ' ', e.last_name) AS 'Nome Completo',
+    j.job_title AS 'Cargo',
+    jh.start_date AS 'Data de início do cargo',
+    d.department_name AS 'Departamento'
+FROM
+    hr.job_history AS jh
+        INNER JOIN
+    hr.jobs AS j ON jh.job_id = j.job_id
+        INNER JOIN
+    hr.departments AS d ON jh.department_id = d.department_id
+        INNER JOIN
+    hr.employees AS e ON jh.employee_id = e.employee_id
+ORDER BY 1 DESC, 2;

3f0aef7edbb0f0530b5a2cafa2c3f99c28439256
Pedro Henrique <pedroenrique.400@gmail.com>
2021-05-25 23:38:17 -0300

    fiz code climate e req 8/2

--- desafio7.sql
+++ desafio7.sql
@@ -1,6 +1,6 @@
 SELECT 
-    CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
-    DATE(h.START_DATE) AS 'Data de início do cargo',
+    UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    DATE(h.START_DATE) AS 'Data de início',
     e.SALARY AS 'Salário'
 FROM
     hr.employees AS e
--- desafio8.sql
+++ desafio8.sql
@@ -1,7 +1,7 @@
 SELECT 
     c.ContactName AS 'Nome de contato',
     s.ShipperName AS 'Empresa que fez o envio',
-    o.OrderDate AS '"Data do pedido'
+    o.OrderDate AS 'Data do pedido'
 FROM
     w3schools.customers AS c,
     w3schools.orders AS o,

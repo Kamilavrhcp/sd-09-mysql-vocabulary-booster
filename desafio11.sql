80dce02d09529694541a9c7e54ba941f2c8c0e34
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-05-26 19:02:09 -0300

    fix req7 and 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,4 +1,4 @@
-SELECT 
+SELECT * FROM (SELECT 
     ContactName AS Nome,
     Country AS País,
     (SELECT 
@@ -8,5 +8,5 @@ SELECT
         WHERE
             C1.Country = C2.Country) AS `Número de compatriotas`
 FROM
-    w3schools.customers AS C1
+    w3schools.customers AS C1) AS C3 WHERE `Número de compatriotas` > 0
 ORDER BY Nome;
--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,6 @@
-SELECT * FROM hr.employees;
-
 SELECT 
     CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome Completo`,
-    DATE(START_DATE) AS `Data de início`,
+    DATE(`START_DATE`) AS `Data de início`,
     SALARY AS Salário
 FROM
     hr.employees AS E

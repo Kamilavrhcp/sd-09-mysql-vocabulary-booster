6efd730474136eba7f832bfb734a68c7115b62af
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-05-26 11:04:21 -0300

    solving task11

--- desafio11.sql
+++ desafio11.sql
@@ -6,8 +6,9 @@ SELECT
         FROM
             w3schools.customers AS support_table
         WHERE
-            support_table.country = main_table.country AND support_table.customerID != main_table.customerID)AS 'Número de compatriotas'
+            support_table.country = main_table.country AND support_table.customerID <> main_table.customerID)AS 'Número de compatriotas'
 FROM
-    w3schools.customers AS main_table
-GROUP BY CustomerName , Country, `Número de compatriotas`
+    w3schools.customers AS main_table	
+GROUP BY `Nome`, Country, `Número de compatriotas`
+HAVING `Número de compatriotas` <> 0
 ORDER BY Nome;

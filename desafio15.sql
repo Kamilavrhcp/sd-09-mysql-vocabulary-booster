cc40aef09e088f7db2d7d21d2dc729a76bfa93a8
Daniela Muller <danielamuller20@gmail.com>
2021-05-26 17:14:17 -0300

    desafio 16

---
+++ desafio15.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(
+    IN job_name VARCHAR(100)
+)
+BEGIN
+    SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+    FROM employees AS e
+    INNER JOIN jobs AS j
+    ON j.JOB_ID = e.JOB_ID 
+    WHERE JOB_TITLE = job_name;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');

0fae5de23dadb7290a103dd7e2094ca9b53e7dd2
nyxnil <nsmnilsonn@gmail.com>
2021-05-26 15:47:56 -0300

    requisitos 1 a 10 v2

--- desafio10.sql
+++ desafio10.sql
@@ -2,7 +2,7 @@ select p.ProductName as Produto,
 min(oD.Quantity) as 'Min',
 max(oD.Quantity) as 'Max',
 round(avg(oD.Quantity),2) as 'Média'
-from w3schools.order_details as Od
+from w3schools.order_details as oD
 inner join w3schools.products as p on oD.ProductID = p.ProductID
-group by Od.ProductID having round(avg(oD.Quantity),2) > 20.00
+group by oD.ProductID having round(avg(oD.Quantity),2) > 20.00
 order by round(avg(oD.Quantity),2), Produto;

d47dc462746d4897ec667a49674ed16b9cef4d19
Daniela Muller <danielamuller20@gmail.com>
2021-05-26 16:32:48 -0300

    lint

--- desafio17.sql
+++ desafio17.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER trigger_order_update
     BEFORE INSERT ON orders
     FOR EACH ROW
 BEGIN
-   SET NEW.OrderDate = CURRENT_DATE();
+	SET NEW.OrderDate = CURRENT_DATE();
 END $$
 
 DELIMITER ;

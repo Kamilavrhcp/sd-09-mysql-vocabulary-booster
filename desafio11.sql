5462329a79d869e53ea4c73e55d9698d0bda6bd9
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-05-25 22:27:09 -0300

    Requisito 11 lint

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,6 @@
 SELECT wc.ContactName AS 'Nome',
-	wc.Country AS `País`,
-  COUNT(wc.ContactName) AS 'Número de compatriotas'
+    wc.Country AS `País`,
+    COUNT(wc.ContactName) AS 'Número de compatriotas'
 FROM w3schools.customers AS wc, w3schools.customers AS wc2
 WHERE wc.Country = wc2.Country AND wc.ContactName <> wc2.ContactName
 GROUP BY `Nome`, `País`

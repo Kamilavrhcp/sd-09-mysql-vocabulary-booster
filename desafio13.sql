0bef35692c8498e12d3ca8ce4b2b1df38e811571
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-05-26 19:14:14 -0300

    Requisitos 12 e 13

--- desafio13.sql
+++ desafio13.sql
@@ -1,7 +1,9 @@
-SELECT ProductName AS 'Produto',
-Price AS 'Preço'
-FROM w3schools.products as prod
-INNER JOIN w3schools.order_details as ordr
-ON prod.ProductID = ordr.ProductID
-WHERE ordr.quantity > 80
+SELECT 
+    ProductName AS 'Produto', Price AS 'Preço'
+FROM
+    w3schools.products AS prod
+        INNER JOIN
+    w3schools.order_details AS ordr ON prod.ProductID = ordr.ProductID
+WHERE
+    ordr.quantity > 80
 ORDER BY ProductName;

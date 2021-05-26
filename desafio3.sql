6eece2b0783e29a815ed5f620af0cbe04da758b9
Celso Oliva Aparecido <celso.oliva.aparecido@gmail.com>
2021-05-26 05:54:46 -0300

    feat(desafio3.sql|hr): query difference between salaries

---
+++ desafio3.sql
@@ -0,0 +1,7 @@
+SELECT 
+    j.JOB_TITLE AS 'Cargo',
+    j.MAX_SALARY - j.MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
+FROM
+    hr.jobs AS j
+ORDER BY j.MAX_SALARY - j.MIN_SALARY ASC , j.JOB_TITLE ASC;
+

6049db8344b9c6e39eaa686d5f936cb20fb0fbf7
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-05-26 10:48:18 -0300

    task 15 ESLINT turn off tabs

--- desafio15.sql
+++ desafio15.sql
@@ -1,8 +1,8 @@
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN JOBTITLE VARCHAR(100))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2) as 'Média salarial' FROM hr.employees as e
-    INNER JOIN hr.jobs as j ON j.JOB_ID = e.JOB_ID
-    WHERE j.JOB_TITLE = JOBTITLE;
+SELECT ROUND(AVG(e.SALARY), 2) as 'Média salarial' FROM hr.employees as e
+INNER JOIN hr.jobs as j ON j.JOB_ID = e.JOB_ID
+WHERE j.JOB_TITLE = JOBTITLE;
 END $$
 DELIMITER ;

3dd1b39da959d03feef5df2fa6c996f9f16b7b10
giovani.diccini <giovani-diccini@pop-os.localdomain>
2021-05-25 21:27:11 -0300

    req10

---
+++ desafio10.sql
@@ -0,0 +1,12 @@
+SELECT 
+    ProductName AS Produto,
+    MIN(Quantity) AS Mínima,
+    MAX(Quantity) AS Máxima,
+    ROUND(AVG(Quantity), 2) AS Média
+FROM
+    w3schools.order_details AS OD
+        INNER JOIN
+    w3schools.products AS P ON P.ProductID = OD.ProductID
+GROUP BY OD.ProductID
+HAVING Média > 20.00
+ORDER BY Média ASC;

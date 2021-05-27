8224406d44c9696d44f7abbb566a0139f8a506df
Felipe Cardoso <felipedclc@gmail.com>
2021-05-27 15:33:27 -0300

    code climate

--- desafio17.sql
+++ desafio17.sql
@@ -1,10 +1,10 @@
 /* Crie uma TRIGGER que, a cada nova inserção realizada na tabela orders, insira automaticamente
  a data atual na coluna OrderDate */
- 
+
 USE w3schools;
 -- INSERT INTO orders (CustomerID, EmployeeID, OrderDate, ShipperID) VALUES (4, 2, NOW(), 2);
 DELIMITER $$
- 
+
 CREATE TRIGGER trigger_order_insert
 BEFORE INSERT ON orders
 FOR EACH ROW

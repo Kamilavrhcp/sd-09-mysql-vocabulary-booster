87c39913bb3c377f5607207f11c46055353fac02
Leonardo Jose da Silveira <rpgleo.js@gmail.com>
2021-05-27 19:22:01 -0300

    Update desafio6.sql
    use correte table

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,12 @@
 SELECT concat(employe.FIRST_NAME, " ", employe.LAST_NAME) AS "Nome completo",
 job.JOB_TITLE as "Cargo",
-employe.START_DATE as "Data de início do cargo",
+hist.START_DATE as "Data de início do cargo",
 depart.DEPARTMENT_NAME as "Departamento"
 from hr.employees as employe
 JOIN hr.jobs as job
 ON employe.JOB_ID = job.JOB_ID
 join hr.departments AS depart
 ON employe.DEPARTMENT_ID = depart.DEPARTMENT_ID
+join hr.job_history as hist
+on employe.EMPLOYEE_ID = hist.EMPLOYEE_ID
 order by `Nome completo` desc, `Cargo` asc;

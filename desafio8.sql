17260cc4e705393554e475c93099287e011f4fc5
Antonio Schappo <antonioschappo@gmail.com>
2021-05-27 15:27:25 -0300

    first try challenge 8

---
+++ desafio8.sql
@@ -0,0 +1,10 @@
+SELECT t1.CustomerName AS 'Nome de contato',
+t2.ShipperName AS 'Empresa que fez o envio',
+t3.OrderDate AS 'Data do pedido'
+FROM w3schools.orders AS t3
+INNER JOIN w3schools.customers AS t1
+ON t1.CustomerID = t3.CustomerID
+INNER JOIN w3schools.shippers AS t2
+ON t2.ShipperID = t3.ShipperID
+HAVING `Empresa que fez o envio` IN('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

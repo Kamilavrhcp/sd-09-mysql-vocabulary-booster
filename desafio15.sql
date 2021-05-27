86c01648addab2f2bba6d660b970d29efd02a639
williamDieter88 <williamzdieter@gmail.com>
2021-05-27 02:06:33 -0300

    Add req 15 - FIX CC

--- desafio15.sql
+++ desafio15.sql
@@ -3,11 +3,11 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(80))
 BEGIN
-	SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
-    FROM hr.employees AS employees
-    INNER JOIN hr.jobs AS jobs
-    ON jobs.JOB_ID = employees.JOB_ID
-    WHERE JOB_TITLE = cargo;
+SELECT ROUND(AVG(SALARY), 2) AS 'Média salarial'
+FROM hr.employees AS employees
+INNER JOIN hr.jobs AS jobs
+ON jobs.JOB_ID = employees.JOB_ID
+WHERE JOB_TITLE = cargo;
 END $$
 
 DELIMITER ;

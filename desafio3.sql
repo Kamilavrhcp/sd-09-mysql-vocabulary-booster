9ffac8b0d0ca4bcb4b7c2b5c1a0663c46563cc1a
Cainan <cainan6697@gmail.com>
2021-05-27 19:31:03 -0300

    Desafio 3

---
+++ desafio3.sql
@@ -0,0 +1,24 @@
+SELECT 
+    JOB_TITLE AS Cargo,
+    CASE
+        WHEN
+            MAX_SALARY >= 5000
+                AND MAX_SALARY <= 10000
+        THEN
+            MAX_SALARY - MIN_SALARY
+        WHEN
+            MAX_SALARY > 10001
+                AND MAX_SALARY <= 20000
+        THEN
+            MAX_SALARY - MIN_SALARY
+        WHEN
+            MAX_SALARY >= 20001
+                AND MAX_SALARY <= 30000
+        THEN
+            MAX_SALARY - MIN_SALARY
+        WHEN MAX_SALARY > 30001 THEN MAX_SALARY - MIN_SALARY
+        ELSE MAX_SALARY
+    END AS `Diferença entre salários máximo e mínimo`
+FROM
+    hr.jobs
+ORDER BY `Diferença entre salários máximo e mínimo` , JOB_TITLE;

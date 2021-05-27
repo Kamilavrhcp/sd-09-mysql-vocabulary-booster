d68fa28d70de82c0cb3860dfdcd51a4c701c1862
Leticia Lima <letylima96@gmail.com>
2021-05-27 20:30:46 -0300

    ajusta desafio 14

--- desafio14.sql
+++ desafio14.sql
@@ -1,6 +1,9 @@
-SELECT C.Country AS `País`
-FROM w3schools.customers AS C
-INNER JOIN w3schools.suppliers AS S ON S.Country != C.Country
+SELECT 
+    C.Country AS `País`
+FROM
+    w3schools.customers AS C
+        INNER JOIN
+    w3schools.suppliers AS S ON S.Country != C.Country
 GROUP BY `País`
 ORDER BY `País` ASC
 LIMIT 5;

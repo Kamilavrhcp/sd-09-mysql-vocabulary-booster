a665b790725322e4a7e91cf06ce972a1696f20d3
williamDieter88 <williamzdieter@gmail.com>
2021-05-26 22:45:10 -0300

    Add req 11 - Final

--- desafio11.sql
+++ desafio11.sql
@@ -1,6 +1,5 @@
 SELECT A.ContactName AS 'Nome', A.Country AS 'País', COUNT(A.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS A, w3schools.customers AS B
-WHERE A.Country = B.Country
+WHERE A.Country = B.Country AND A.CustomerID <> B.CustomerID
 GROUP BY 1, 2
-HAVING COUNT(A.Country) > 1
 ORDER BY 1;

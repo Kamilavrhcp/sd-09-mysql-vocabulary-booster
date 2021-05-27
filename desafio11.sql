98d01b25224e527e65a9b04dee052c68ddc1de97
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-05-27 01:44:05 -0300

    desafio 11

---
+++ desafio11.sql
@@ -0,0 +1,15 @@
+SELECT 
+    t1.ContactName AS 'Nome',
+    t1.Country AS 'País',
+    (SELECT
+            COUNT(*)
+        FROM
+            w3schools.customers AS t2
+        WHERE
+			t1.ContactName <> t2.ContactName
+                AND t1.Country = t2.Country) AS 'Número de compatriotas'
+FROM
+    w3schools.customers AS t1
+GROUP BY 1, 2, 3
+HAVING `Número de compatriotas` > 0
+ORDER BY 1;

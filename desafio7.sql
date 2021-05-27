3cdf8a1c1ea4d86668cfeadaf55f9402fb6ea631
Antonio Schappo <antonioschappo@gmail.com>
2021-05-27 14:33:43 -0300

    second try challenge 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,11 +1,11 @@
 SELECT UCASE(CONCAT(t2.FIRST_NAME, ' ', t2.LAST_NAME )) AS 'Nome completo',
 t3.JOB_TITLE AS Cargo,
-t1.START_DATE AS 'Data de início do cargo',
+t1.START_DATE AS 'Data de início',
 t2.Salary AS 'Salário'
 FROM hr.job_history AS t1
 INNER JOIN hr.employees As t2
 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
 INNER JOIN hr.jobs AS t3
 ON t1.JOB_ID = t3.JOB_ID
-HAVING MONTH(`Data de início do cargo`) IN (1, 2, 3)
-ORDER BY `Nome completo`, `Data de início do cargo`;
+HAVING MONTH(`Data de início`) IN (1, 2, 3)
+ORDER BY `Nome completo`, `Data de início`;

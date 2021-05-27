95606dd540942d98d437bdf73b758cde0f191c7a
jdaniel-ap <jdaniell.93@gmail.com>
2021-05-27 11:54:34 -0300

    req 1 fix

--- desafio1.sql
+++ desafio1.sql
@@ -2,7 +2,7 @@ SELECT
     COUNTRY_NAME AS 'País',
     IF(REGION_ID = 'Europa',
         'incluído',
-        'não incluído')
+        'não incluído') AS 'Status Inclusão'
 FROM
     hr.countries
 ORDER BY 'País';

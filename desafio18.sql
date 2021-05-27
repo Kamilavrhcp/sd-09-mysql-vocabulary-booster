dbc5fba59c554a18e9b4b784ca4d256ecc8486be
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-05-27 01:24:27 -0300

    Task 19 - fix: Full year

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,7 @@
 SELECT
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS `Nome completo`,
-    DATE_FORMAT(jb.START_DATE, '%d/%m/%y') AS `Data de início`,
-    DATE_FORMAT(jb.END_DATE, '%d/%m/%y') AS `Data de rescisão`,
+    DATE_FORMAT(jb.START_DATE, '%d/%m/%Y') AS `Data de início`,
+    DATE_FORMAT(jb.END_DATE, '%d/%m/%Y') AS `Data de rescisão`,
     ROUND(DATEDIFF(jb.END_DATE, jb.START_DATE) / 365, 2) AS `Anos trabalhados`
 FROM
     hr.employees AS e

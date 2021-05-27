35212264c3a556a2f8bbe463ac7d740ec70e4d20
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-05-27 01:16:08 -0300

    Task 19

---
+++ desafio19.sql
@@ -0,0 +1,19 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano( mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE returnFunction INT;
+    SELECT
+        COUNT(*)
+    FROM
+        hr.employees
+    WHERE
+        MONTH(HIRE_DATE) = mes
+            AND YEAR(HIRE_DATE) = ano
+	INTO returnFunction;
+    RETURN returnFunction;
+END $$
+
+DELIMITER ;

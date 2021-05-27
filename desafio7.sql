bf10bd7df7477973e1b1db58a204cf4db536d5ab
Thiago Granville <thiagogranville@gmail.com>
2021-05-27 13:30:32 -0300

    [DESAFIO 7][Exibe relatório que mostra o histórico de cargos das pessoas empregadas que iniciaram seus cargos em meses especificados]

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT 
+    UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS 'Nome completo',
+    h.START_DATE AS 'Data de início',
+    e.SALARY AS 'Salário'
+FROM
+    hr.job_history AS h
+        JOIN
+    hr.employees AS e ON e.EMPLOYEE_ID = h.EMPLOYEE_ID
+        AND MONTH(h.START_DATE) IN (01 , 02, 03)
+ORDER BY `Nome completo`, `Data de início`;

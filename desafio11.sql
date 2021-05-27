f62525d526d0541b755909839b052cda6d17be37
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-05-26 21:48:22 -0300

    Desafio 11

--- desafio11.sql
+++ desafio11.sql
@@ -6,6 +6,6 @@ FROM
     w3schools.customers
         INNER JOIN
     w3schools.customers AS customers2 ON customers2.Country = customers.Country
-       AND customers2.CustomerID <> customers.CustomerID
+        AND customers2.CustomerID <> customers.CustomerID
 GROUP BY 1, 2
 ORDER BY 1;

d9e02130fd997ae04f5674247cdbb0a7cf9f4581
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-05-26 22:01:29 -0300

    Requisito 14 LINT

--- desafio14.sql
+++ desafio14.sql
@@ -1,11 +1,10 @@
 SELECT DISTINCT
-	Country AS `País`
+    Country AS País
 FROM
-  w3schools.customers
-UNION ALL
-SELECT DISTINCT
-	Country AS `País`
+    w3schools.customers
+UNION ALL SELECT DISTINCT
+    Country AS País
 FROM
-  w3schools.suppliers
-ORDER BY `País`
+    w3schools.suppliers
+ORDER BY País
 LIMIT 5;

aee952638de6ed76c60ef32f622c2d69bcdd0067
Christopher <chris.tryber@gmail.com>
2021-05-27 13:49:17 -0300

    Requisitos 8 e 9

---
+++ desafio9.sql
@@ -0,0 +1,5 @@
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo', COUNT(o.ShipperID) AS 'Total de pedidos' FROM w3schools.orders AS o 
+INNER JOIN w3schools.employees AS e
+ON e.EmployeeID = o.EmployeeID
+GROUP BY CONCAT(e.FirstName, ' ', e.LastName)
+ORDER BY COUNT(o.ShipperID);

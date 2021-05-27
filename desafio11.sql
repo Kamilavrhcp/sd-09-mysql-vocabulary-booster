3eaf0590b0e8f0c1f666b2d1ac92a42bd396f578
williamDieter88 <williamzdieter@gmail.com>
2021-05-26 22:22:59 -0300

    Add req 11

---
+++ desafio11.sql
@@ -0,0 +1,4 @@
+SELECT A.ContactName AS 'Nome', A.Country AS 'País', COUNT(A.Country) AS 'Número de compatriotas'
+FROM w3schools.customers AS A, w3schools.customers AS B
+WHERE A.Country = B.Country
+GROUP BY 1, 2

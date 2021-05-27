8fd0245c5fd709f559d0a1982e9fcc3af62ec94c
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-05-27 15:50:50 -0300

    Desafio 1 concluido

---
+++ desafio1.sql
@@ -0,0 +1,11 @@
+SELECT 
+    C.COUNTRY_NAME AS 'País',
+    CASE
+        WHEN R.REGION_NAME = 'Europe' THEN 'incluído'
+        ELSE 'não incluído'
+    END AS 'Status Inclusão'
+FROM
+    hr.countries AS C
+        INNER JOIN
+    hr.regions AS R ON C.REGION_ID = R.REGION_ID
+ORDER BY C.COUNTRY_NAME;

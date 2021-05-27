d5dd7f2fdd2200e9ecdc858fbb494bfa0e9a07a3
Leonardo Jose da Silveira <rpgleo.js@gmail.com>
2021-05-27 18:31:50 -0300

    Update desafio6.sql
    edit collum name

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,10 @@
 SELECT concat(employe.FIRST_NAME, " ", employe.LAST_NAME) AS NOME,
 job.JOB_TITLE as Cargo,
 employe.HIRE_DATE as "Data de início do cargo",
-DEPART.DEPARTMENT_NAME as Departamento
+depart.DEPARTMENT_NAME as "Departamento"
 from hr.employees as employe
 JOIN hr.jobs as job
 ON employe.JOB_ID = job.JOB_ID
-join hr.departments AS DEPART
+join hr.departments AS depart
 ON employe.DEPARTMENT_ID = depart.DEPARTMENT_ID
 order by nome desc, Cargo asc;

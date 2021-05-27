469fe5fe54a1e90c7067a1933ca34e333e66b9e3
Leonardo Jose da Silveira <rpgleo.js@gmail.com>
2021-05-27 18:55:12 -0300

    Update desafio6.sql
    update

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 SELECT concat(employe.FIRST_NAME, " ", employe.LAST_NAME) AS "Nome completo",
-job.JOB_TITLE as Cargo,
+job.JOB_TITLE as "Cargo",
 employe.HIRE_DATE as "Data de início do cargo",
 depart.DEPARTMENT_NAME as "Departamento"
 from hr.employees as employe
@@ -7,4 +7,4 @@ JOIN hr.jobs as job
 ON employe.JOB_ID = job.JOB_ID
 join hr.departments AS depart
 ON employe.DEPARTMENT_ID = depart.DEPARTMENT_ID
-order by `Nome completo` desc, Cargo asc;
+order by `Nome completo` desc, `Cargo` asc;

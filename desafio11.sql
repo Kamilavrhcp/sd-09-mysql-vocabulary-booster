f415d222d69ec0c15209d251fdc3f2e9ae9af750
williamDieter88 <williamzdieter@gmail.com>
2021-05-26 22:35:19 -0300

    Add req 11 Add Having condition

--- desafio11.sql
+++ desafio11.sql
@@ -2,3 +2,5 @@ SELECT A.ContactName AS 'Nome', A.Country AS 'País', COUNT(A.Country) AS 'Núme
 FROM w3schools.customers AS A, w3schools.customers AS B
 WHERE A.Country = B.Country
 GROUP BY 1, 2
+HAVING COUNT(A.Country) > 1
+ORDER BY 1;

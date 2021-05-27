30302e636b4e6eb9ac06d91c5b5b192e8bd54e97
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-05-26 21:58:07 -0300

    Task 11 better

--- desafio11.sql
+++ desafio11.sql
@@ -1,11 +1,11 @@
 SELECT
     t1.ContactName AS `Nome`,
     t1.Country AS País,
-    COUNT(t2.Country) AS `Número de compatriotas`
+    (COUNT(t2.Country) - 1) AS `Número de compatriotas`
 FROM
     w3schools.customers AS t1
 JOIN
     w3schools.customers AS t2
-ON t2.Country = t1.Country AND t1.CustomerID <> t2.CustomerID
+ON t2.Country = t1.Country
 GROUP BY t1.ContactName, t1.Country
 ORDER BY t1.ContactName;

c8862da54a71b08a12dcce052249ba53310b6a13
Leonardo Jose da Silveira <rpgleo.js@gmail.com>
2021-05-28 00:42:52 -0300

    Update desafio6.sql
    change true font

--- desafio6.sql
+++ desafio6.sql
@@ -2,11 +2,11 @@ SELECT concat(employe.FIRST_NAME, " ", employe.LAST_NAME) AS "Nome completo",
 job.JOB_TITLE as "Cargo",
 hist.START_DATE as "Data de início do cargo",
 depart.DEPARTMENT_NAME as "Departamento"
-from hr.employees as employe
+from hr.job_history as hist
 JOIN hr.jobs as job
-ON employe.JOB_ID = job.JOB_ID
+ON hist.JOB_ID = job.JOB_ID
 join hr.departments AS depart
-ON employe.DEPARTMENT_ID = depart.DEPARTMENT_ID
-join hr.job_history as hist
+ON hist.DEPARTMENT_ID = depart.DEPARTMENT_ID
+join hr.employees as employe
 on employe.EMPLOYEE_ID = hist.EMPLOYEE_ID
 order by `Nome completo` desc, `Cargo` asc;

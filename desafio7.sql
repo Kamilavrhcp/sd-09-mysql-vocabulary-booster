560abcd28a8f13e0fb3a0d7f2abf62f9b9ca018a
ViniciusChagas <viniciushrcs@gmail.com>
2021-05-28 12:45:22 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT UCASE(CONCAT(emp.FIRST_NAME,' ', emp.LAST_NAME)) AS 'Nome completo',
+hist.START_DATE AS 'Data de início',
+SALARY AS 'Salário'
+
+FROM hr.employees AS emp
+INNER JOIN hr.job_history AS hist ON emp.EMPLOYEE_ID = hist.EMPLOYEE_ID
+WHERE MONTH(hist.START_DATE) IN(01,02,03)
+
+ORDER BY 'Nome completo', hist.START_DATE;

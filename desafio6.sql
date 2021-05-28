ae6e88bc350b43858e9be720e6c41692ec227328
Matheus Martino <matheus.martino97@gmail.com>
2021-05-28 00:01:54 -0300

    Teste.

--- desafio6.sql
+++ desafio6.sql
@@ -1,24 +1,24 @@
--- -- USANDO INNER JOIN
--- SELECT 
---     CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
---     J.JOB_TITLE AS Cargo,
---     START_DATE AS 'Data de início do cargo',
---     D.DEPARTMENT_NAME AS Departamento
--- FROM
---     hr.job_history AS JH
---         INNER JOIN
---     hr.employees AS E ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
---         INNER JOIN
---     hr.jobs AS J ON J.JOB_ID = JH.JOB_ID
---         INNER JOIN
---     hr.departments AS D ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
--- ORDER BY `Nome completo` DESC , Cargo ASC;
+-- USANDO INNER JOIN
+SELECT 
+    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome completo`,
+    J.JOB_TITLE AS Cargo,
+    START_DATE AS 'Data de início do cargo',
+    D.DEPARTMENT_NAME AS Departamento
+FROM
+    hr.job_history AS JH
+        INNER JOIN
+    hr.employees AS E ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+        INNER JOIN
+    hr.jobs AS J ON J.JOB_ID = JH.JOB_ID
+        INNER JOIN
+    hr.departments AS D ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC , Cargo ASC;
 
--- -- Usando subqueries
--- -- SELECT
--- -- 	(SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) FROM hr.employees WHERE EMPLOYEE_ID = JH.EMPLOYEE_ID) AS `Nome completo`,
--- --     (SELECT DISTINCT JOB_TITLE FROM hr.jobs WHERE JOB_ID = JH.JOB_ID) AS Cargo,
--- --     START_DATE AS `Data de início do cargo`,
--- --     (SELECT DEPARTMENT_NAME FROM hr.departments WHERE DEPARTMENT_ID = JH.DEPARTMENT_ID) AS Departamento
--- -- FROM hr.job_history AS JH
--- -- ORDER BY `Nome completo` DESC, Cargo ASC;
+-- Usando subqueries
+-- SELECT
+-- 	(SELECT CONCAT(FIRST_NAME, ' ', LAST_NAME) FROM hr.employees WHERE EMPLOYEE_ID = JH.EMPLOYEE_ID) AS `Nome completo`,
+--     (SELECT DISTINCT JOB_TITLE FROM hr.jobs WHERE JOB_ID = JH.JOB_ID) AS Cargo,
+--     START_DATE AS `Data de início do cargo`,
+--     (SELECT DEPARTMENT_NAME FROM hr.departments WHERE DEPARTMENT_ID = JH.DEPARTMENT_ID) AS Departamento
+-- FROM hr.job_history AS JH
+-- ORDER BY `Nome completo` DESC, Cargo ASC;
--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,9 @@
--- SELECT 
---     CONCAT(E.FirstName, ' ', E.LastName) AS `Nome completo`,
---     COUNT(*) AS `Total de pedidos`
--- FROM
---     w3schools.orders AS O
---         INNER JOIN
---     w3schools.employees AS E ON E.EmployeeID = O.EmployeeID
--- GROUP BY `Nome completo`
--- ORDER BY `Total de pedidos` ASC;
+SELECT 
+    CONCAT(E.FirstName, ' ', E.LastName) AS `Nome completo`,
+    COUNT(*) AS `Total de pedidos`
+FROM
+    w3schools.orders AS O
+        INNER JOIN
+    w3schools.employees AS E ON E.EmployeeID = O.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos` ASC;

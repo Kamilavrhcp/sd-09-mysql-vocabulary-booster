74b203eddb732ba91071b10c1963c1069cc31700
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-05-28 19:30:44 -0300

    Lint fix

--- desafio15.sql
+++ desafio15.sql
@@ -4,12 +4,12 @@ DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(60))
 BEGIN
     SELECT 
-		ROUND(AVG(e.SALARY), 2) AS 'Média salarial'    
-	FROM
-		hr.employees AS e
-		INNER JOIN hr.jobs AS j
-			ON e.JOB_ID = j.JOB_ID
-	WHERE j.JOB_TITLE LIKE CONCAT('%', cargo, '%');
+    ROUND(AVG(e.SALARY), 2) AS 'Média salarial'    
+  FROM
+    hr.employees AS e
+    INNER JOIN hr.jobs AS j
+      ON e.JOB_ID = j.JOB_ID
+  WHERE j.JOB_TITLE LIKE CONCAT('%', cargo, '%');
 END $$
 
 DELIMITER ;

1f0214c173ffe712358b8844ae127113e40159c6
marcusWittho <witthoeft.mcs@hotmail.com>
2021-05-28 15:10:24 -0300

    Requisito 14

---
+++ desafio14.sql
@@ -0,0 +1,16 @@
+SELECT DISTINCT
+	c.Country AS 'País'
+FROM
+    w3schools.customers AS c
+
+UNION
+
+SELECT DISTINCT
+	s.Country
+FROM
+	w3schools.suppliers AS s
+
+ORDER BY 'País'
+
+LIMIT 5;
+    

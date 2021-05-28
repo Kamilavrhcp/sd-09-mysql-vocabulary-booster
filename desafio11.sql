d36e8d3f940d25328c70ccfc66f52d0bc146e64a
Vagner Pereira <sousa.vagner82@gmail.com>
2021-05-28 12:10:30 -0300

    desafio 11 e 14

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,9 @@
 SELECT
 C.ContactName AS 'Nome',
 C.Country AS 'País',
-(SELECT (COUNT(Country)) - 1 FROM w3schools.customers
-WHERE C.Country = customers.Country) AS 'Número de compatriotas'
+(SELECT (COUNT(*) - 1) AS T FROM w3schools.customers
+WHERE C.Country = customers.Country) AS `Número de compatriotas`
 FROM w3schools.customers C
-GROUP BY C.ContactName
+GROUP BY C.ContactName, C.Country
+HAVING `Número de compatriotas` > 0
 ORDER BY C.ContactName;
--- desafio14.sql
+++ desafio14.sql
@@ -1,4 +1,5 @@
-SELECT Country FROM w3schools.suppliers
+SELECT DISTINCT Country AS `País` FROM w3schools.customers
 UNION ALL
-SELECT Country FROM w3schools.customers
-ORDER BY Country;
+SELECT DISTINCT Country AS `País` FROM w3schools.suppliers
+ORDER BY `País`
+LIMIT 5;

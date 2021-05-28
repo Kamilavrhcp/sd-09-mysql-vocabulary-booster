429a828415c614f37bfc81525c0a4fbc29a5e94d
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-05-28 16:17:51 -0300

    Requirement 19

---
+++ desafio19.sql
@@ -0,0 +1,16 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(month_param INT, year_param INT)
+RETURNS INT READS SQL DATA
+BEGIN
+    DECLARE number_of_hired INT;
+	SELECT COUNT(*) 
+	FROM hr.employees AS e
+	WHERE MONTH(e.HIRE_DATE) = month_param AND YEAR(e.HIRE_DATE) = year_param INTO number_of_hired;
+    RETURN number_of_hired;
+END $$
+
+DELIMITER ;
+
+SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987);

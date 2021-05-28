5a3fcf22dfe63a1db116c96bc1e877a1889c1073
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-05-28 16:18:55 -0300

    Requirement 19 corrections

--- desafio19.sql
+++ desafio19.sql
@@ -5,12 +5,12 @@ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(month_param 
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE number_of_hired INT;
-	SELECT COUNT(*) 
-	FROM hr.employees AS e
-	WHERE MONTH(e.HIRE_DATE) = month_param AND YEAR(e.HIRE_DATE) = year_param INTO number_of_hired;
+    SELECT COUNT(*) 
+    FROM hr.employees AS e
+    WHERE MONTH(e.HIRE_DATE) = month_param AND YEAR(e.HIRE_DATE) = year_param INTO number_of_hired;
     RETURN number_of_hired;
 END $$
 
 DELIMITER ;
 
-SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987);
+SELECT EXIBIR_QUANTIDADE_PESSOAS_CONTRATADAS_POR_MES_E_ANO(6, 1987);

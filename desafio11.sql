5568d2be73abbfbd7687c7ef387abf31bd20de55
marcusWittho <witthoeft.mcs@hotmail.com>
2021-05-28 12:37:49 -0300

    Requisito 12

---
+++ desafio11.sql
@@ -0,0 +1,12 @@
+SELECT 
+    c1.ContactName AS 'Nome',
+    c1.Country AS 'País',
+    COUNT(c2.ContactName) AS 'Número de compatriotas'
+FROM
+    w3schools.customers AS c1,
+    w3schools.customers AS c2
+
+WHERE
+	c1.CustomerID <> c2.CustomerID AND c1.Country = c2.Country
+
+ORDER BY c1.ContactName;

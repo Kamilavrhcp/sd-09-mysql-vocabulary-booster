1a6020be498c7ecfe3f0922c6fed8df4230d6b33
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-05-28 17:10:48 -0300

    Lint Fixes

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,7 @@
 SELECT 
     UCASE(CONCAT(e.FIRST_NAME, " ", e.LAST_NAME)) AS 'Nome completo',
     h.START_DATE AS 'Data de início',
-	e.SALARY AS 'Salário'
+    e.SALARY AS 'Salário'
 FROM
     hr.job_history AS h
         INNER JOIN

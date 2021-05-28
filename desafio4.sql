ae19a85b1d237a8c343708197f2adcbf1839d56d
Pedro <ped.alm2709@gmail.com>
2021-05-28 17:03:59 -0300

    correcao req 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,15 +1,15 @@
 SELECT 
-    j.JOB_TITLE AS 'Cargo',
+    j.job_title AS Cargo,
     ROUND(AVG(e.SALARY), 2) AS 'Média salarial',
     CASE
-        WHEN AVG(e.SALARY) BETWEEN 2000 AND 5000 THEN 'Jùnior'
+        WHEN AVG(e.SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
         WHEN AVG(e.SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
         WHEN AVG(e.SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
         WHEN AVG(e.SALARY) > 10500 THEN 'CEO'
-    END AS 'Senioridade'
+    END AS Senioridade
 FROM
-    hr.jobs AS j
+    hr.jobs j
         INNER JOIN
-    hr.employees AS e ON j.JOB_ID = e.JOB_ID
+    hr.employees e ON j.JOB_ID = e.JOB_ID
 GROUP BY Cargo
-ORDER BY `Média salarial` , Cargo ASC;
+ORDER BY 2 , 1 ASC;

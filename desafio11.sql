0639e14439a1233533987b9e029348da02cbb4c1
Micaela Alves <micaela.alves93@gmail.com>
2021-05-29 12:52:55 -0300

    Requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -2,10 +2,14 @@ SELECT
     t1.ContactName AS 'Nome',
     t1.Country AS 'País',
     (SELECT 
-            COUNT(ContactName)
+            COUNT(t2.ContactName)
         FROM
-            w3schools.customers) AS 'Número de compatriotas'
+            w3schools.customers AS t2
+        WHERE
+            t1.Country = t2.Country
+                AND t1.ContactName <> t2.ContactName) AS 'Número de compatriotas'
 FROM
     w3schools.customers AS t1
-WHERE
-    t1.Country = t2.Country;
+GROUP BY Nome , País
+HAVING `Número de compatriotas` > 0
+ORDER BY Nome;

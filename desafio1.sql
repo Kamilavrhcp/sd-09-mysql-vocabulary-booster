d97cf8b466b76701b2256fb500d7c54a63ecc065
negres <nataliaknegres@gmail.com>
2021-05-29 13:50:54 -0300

    Corrige requisito 1 /style

--- desafio1.sql
+++ desafio1.sql
@@ -1,4 +1,7 @@
-SELECT country_name País,
-	IF(region_id = 1, 'incluído', 'não incluído') 'Status Inclusão'
+SELECT
+	country_name País,
+	IF(region_id = 1,
+    'incluído',
+    'não incluído') AS 'Status inclusão'
 FROM hr.countries
 ORDER BY country_name;

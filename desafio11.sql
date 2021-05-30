bb1d02a624627151e7eec9ea473a0cead9627998
Pedro de Almeida <ped.alm2709@gmail.com>
2021-05-30 10:29:08 -0300

    correcao 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,14 @@
 SELECT 
-    c.ContactName AS `Nome`,
-    c.Country AS `País`,
+    c.ContactName AS 'Nome',
+    c.Country AS 'País',
     (SELECT 
             COUNT(*)
         FROM
             w3schools.customers AS co
         WHERE
             co.ContactName <> c.ContactName
-                AND co.Country = c.Country) AS `Número de compatriotas`
+                AND co.Country = c.Country) AS 'Número de compatriotas'
 FROM
     w3schools.customers AS c
-ORDER BY 1;
+GROUP BY 1 , 2 , 3
+ORDER BY 1 ASC;

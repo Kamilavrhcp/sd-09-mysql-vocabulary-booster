857faebb23200046d3d91ecdeb05ebd7a42b7284
Bob <wend3llsouza@gmail.com>
2021-05-30 15:34:20 -0300

    :beetle: FIX CC

--- desafio5.sql
+++ desafio5.sql
@@ -3,6 +3,3 @@ SELECT JOB_TITLE AS 'Cargo',
 ROUND((min_salary / 12), 2) AS 'Média mínima mensal',
 ROUND((max_salary / 12), 2) AS 'Média máxima mensal'
 FROM hr.jobs ORDER BY (max_salary - min_salary), JOB_TITLE;
-
-
-

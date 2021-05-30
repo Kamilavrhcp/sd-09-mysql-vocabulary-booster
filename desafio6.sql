fe06d926fba69c17cdb6e6faa1325b1430af597c
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-05-29 21:14:17 -0300

    Code Climate

--- desafio6.sql
+++ desafio6.sql
@@ -6,13 +6,13 @@ SELECT
     job_history.START_DATE AS "Data de início do cargo",
     departments.DEPARTMENT_NAME AS "Departamento"
 FROM
-    hr.job_history AS job_history
-        INNER JOIN
-    hr.employees AS employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
-        INNER JOIN
-    hr.jobs AS jobs ON jobs.JOB_ID = job_history.JOB_ID
-		INNER JOIN
-	hr.departments AS departments ON departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID
+hr.job_history AS job_history
+INNER JOIN
+hr.employees AS employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+INNER JOIN
+hr.jobs AS jobs ON jobs.JOB_ID = job_history.JOB_ID
+INNER JOIN
+hr.departments AS departments ON departments.DEPARTMENT_ID = job_history.DEPARTMENT_ID
 ORDER BY CONCAT(employees.FIRST_NAME,
         ' ',
         employees.LAST_NAME) DESC , jobs.JOB_TITLE;

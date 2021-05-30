fe8e12b4abf7db5db4110f9111185f41d49fa6d6
santiago <felipesantiagolds@gmail.com>
2021-05-30 20:12:46 -0300

    Requisitos feitos

--- desafio17.sql
+++ desafio17.sql
@@ -1,11 +1,11 @@
 USE w3schools;
+
 DELIMITER $$
+    CREATE TRIGGER adicionar_data_atual BEFORE
+INSERT
+    ON orders FOR EACH ROW BEGIN
+SET
+    NEW.OrderDate = CURRENT_DATE();
 
-CREATE TRIGGER adicionar_data_atual
-	BEFORE INSERT ON orders
-    FOR EACH ROW
-BEGIN
-    SET NEW.OrderDate = CURRENT_DATE();
 END $$
-
-DELIMITER ;
+DELIMITER;

fbde1d3c184a308430d7f62cdc695f92c79df767
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-05-29 21:22:13 -0300

    Requisito 7

---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+SELECT 
+    CONCAT(employees.FIRST_NAME,
+            ' ',
+            employees.LAST_NAME) AS 'Nome completo'
+FROM
+    hr.job_history AS job_history
+        INNER JOIN
+    hr.employees AS employees ON employees.EMPLOYEE_ID = job_history.EMPLOYEE_ID
+WHERE
+	MONTH(job_history.START_DATE) = 01 OR
+    MONTH(job_history.START_DATE) = 02 OR
+    MONTH(job_history.START_DATE) = 03
+ORDER BY CONCAT(employees.FIRST_NAME,
+        ' ',
+        employees.LAST_NAME);

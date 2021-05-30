5ecf0ab289f791a831c7cd1cfb8e775e28cc7502
Matheus Martino <matheus.martino97@gmail.com>
2021-05-30 18:23:20 -0300

    Arrumando erros de code climate.

--- desafio15.sql
+++ desafio15.sql
@@ -2,10 +2,10 @@ USE hr;
 DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo (IN cargo VARCHAR(45))
-	BEGIN
-		SELECT ROUND(AVG(SALARY), 2) AS `Média salarial` FROM hr.employees
-		WHERE JOB_ID = (SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = cargo)
-		GROUP BY JOB_ID;
-    END $$
+BEGIN
+	SELECT ROUND(AVG(SALARY), 2) AS `Média salarial` FROM hr.employees
+	WHERE JOB_ID = (SELECT JOB_ID FROM hr.jobs WHERE JOB_TITLE = cargo)
+	GROUP BY JOB_ID;
+END $$
     
 DELIMITER ;

c530af0699e09c9cd287b1e6e98180380db39a1a
Matheus Martino <matheus.martino97@gmail.com>
2021-05-30 19:37:23 -0300

    Desafio 19.

--- desafio19.sql
+++ desafio19.sql
@@ -4,9 +4,9 @@ DELIMITER $$
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano (input_month INT, input_year INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE quantity INT;
-	SELECT COUNT(*) FROM hr.employees
-	WHERE HIRE_DATE LIKE CONCAT(input_year, '-', IF (input_month < 10, CONCAT(0, input_month), input_month), '%') INTO quantity;
+DECLARE quantity INT;
+SELECT COUNT(*) FROM hr.employees
+WHERE HIRE_DATE LIKE CONCAT(input_year, '-', IF (input_month < 10, CONCAT(0, input_month), input_month), '%') INTO quantity;
 RETURN quantity;
 END $$
 

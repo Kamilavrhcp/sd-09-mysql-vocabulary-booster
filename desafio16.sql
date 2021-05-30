b5437bbc3e29d2f46b981fe2ab2d8280c5f14495
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-05-29 21:46:24 -0300

    Realizado desafio 16

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+USE hr;
+
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(25))
+RETURNS DOUBLE READS SQL DATA
+BEGIN
+	DECLARE jobs_history DOUBLE;
+	SELECT COUNT(employee_id) 
+		FROM hr.job_history 
+        WHERE employee_id IN (SELECT employee_id FROM hr.employees AS e WHERE e.email = email)
+        INTO jobs_history;
+    RETURN jobs_history;
+END $$
+
+DELIMITER ;
+

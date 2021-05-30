0e4fc41a31f82fb2732822157cc0295fb63f8e8c
jhon <jhonatan.arguello@gmail.com>
2021-05-29 21:47:33 -0300

    Ajustes no desafio 5.

--- desafio5.sql
+++ desafio5.sql
@@ -1,11 +1,11 @@
 SELECT 
     J.JOB_TITLE AS 'Cargo',
-    J.MAX_SALARY - J.MIN_SALARY AS 'Variação Salarial',
+    ROUND(J.MAX_SALARY - J.MIN_SALARY, 2) AS 'Variação Salarial',
     ROUND(J.MIN_SALARY / 12, 2) AS 'Média mínima mensal',
     ROUND(J.Max_SALARY / 12, 2) AS 'Média máxima mensal'
 FROM
-    hr.jobs AS J
-INNER JOIN
     hr.employees AS E
-GROUP BY `Cargo`
+INNER JOIN
+    hr.jobs AS J ON J.JOB_ID = E.JOB_ID
+GROUP BY E.JOB_ID
 ORDER BY `Variação Salarial` , J.JOB_TITLE;

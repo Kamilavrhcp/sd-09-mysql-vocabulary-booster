244692742a26f9f22945a7b8105f39f9851c4586
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-05-31 13:36:22 -0300

    codeclimate

--- desafio17.sql
+++ desafio17.sql
@@ -2,8 +2,8 @@ USE w3schools;
 DELIMITER $$ 
 
 CREATE TRIGGER InsertOrderDate 
-	BEFORE INSERT ON orders 
-	FOR EACH ROW 
+BEFORE INSERT ON orders 
+FOR EACH ROW 
 BEGIN 
     SET NEW.OrderDate = NOW();
 END $$

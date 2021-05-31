58c8145bb23d4ffdfff98f4cdfac49f93c9be884
Aurelio Scarante <aurelioscarante25@gmail.com>
2021-05-31 13:50:30 -0300

    :ambulance: Desafio 13 , tratando issue codeclimate

--- desafio13.sql
+++ desafio13.sql
@@ -1,6 +1,6 @@
 SELECT ProductName AS `Produto`, Price AS `Preço`
 FROM w3schools.products AS p
 WHERE EXISTS( SELECT * FROM w3schools.order_details AS d
-	WHERE d.ProductID = p.ProductID
-	HAVING d.Quantity > 80)
+WHERE d.ProductID = p.ProductID
+HAVING d.Quantity > 80)
 ORDER BY `Produto`  ASC;

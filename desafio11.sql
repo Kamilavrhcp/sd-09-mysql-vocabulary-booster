8e308991cea025214e814bfcff35ad202016548f
jhon <jhonatan.arguello@gmail.com>
2021-05-31 00:45:58 -0300

    Ajuste desafio 11 e finalizacao do 14.

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,5 @@
-SELECT 
-    T1.ContactName AS Nome,
-    T1.Country AS 'País',
-    COUNT(T1.Country)-1 AS 'Número de compatriotas'
+SELECT T1.ContactName AS 'Nome', T1.Country AS 'País', COUNT(T1.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS T1
-INNER JOIN w3schools.customers AS T2 ON T1.Country = T2.Country
-GROUP BY Nome
-ORDER BY Nome ASC;
+INNER JOIN w3schools.customers AS T2 ON T1.Country = T2.Country AND T1.CustomerID != T2.CustomerID
+GROUP BY T1.Country, T1.CustomerID
+ORDER BY T1.ContactName ASC;

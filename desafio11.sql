3bf7d65c1f20530050cfae459605365b54b14206
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-05-31 18:09:49 -0300

    desafio 11 fix

--- desafio11.sql
+++ desafio11.sql
@@ -1,13 +1,13 @@
 SELECT 
-    a.ContactName AS 'Nome',
-    a.Country AS 'País',
+    cus.ContactName AS 'Nome',
+    cus.Country AS 'País',
     (SELECT 
             COUNT(b.Country)
         FROM
             customers AS b
         WHERE
-            b.country = a.country
-                AND b.CustomerID <> a.CustomerID) AS 'Número de compatriotas'
+            b.country = cus.country
+                AND b.CustomerID <> cus.CustomerID) AS 'Número de compatriotas'
 FROM
-    customers AS a
+    customers AS cus
 ORDER BY `Nome`;

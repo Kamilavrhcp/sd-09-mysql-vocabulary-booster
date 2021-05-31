7a20ed671b6f426bbadc84567be73f68d637bed4
jhon <jhonatan.arguello@gmail.com>
2021-05-31 00:17:37 -0300

    Desafio 13.

---
+++ desafio13.sql
@@ -0,0 +1,6 @@
+SELECT P.ProductName AS Produto, P.Price AS Preço
+FROM w3schools.order_details AS OD
+INNER JOIN w3schools.products AS P ON OD.ProductID = P.ProductID
+WHERE OD.Quantity > 80
+GROUP BY Produto
+ORDER BY Produto

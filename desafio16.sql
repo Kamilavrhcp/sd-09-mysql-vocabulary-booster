53256feddd7e5d946c0b622fde007cda27ff7bd6
Thiago Granville <thiagogranville@gmail.com>
2021-05-31 03:17:06 -0300

    [DESAFIO 16][Cria função buscar_quantidade_de_empregos_por_funcionario]

---
+++ desafio16.sql
@@ -0,0 +1,20 @@
+USE hr
+
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email varchar(25))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE job_total INT;
+SELECT 
+    COUNT(j.EMPLOYEE_ID)
+FROM
+    job_history AS j
+        JOIN
+    hr.employees AS e ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE
+    e.EMAIL = email INTO job_total;
+RETURN job_total;
+END $$
+
+DELIMITER ;

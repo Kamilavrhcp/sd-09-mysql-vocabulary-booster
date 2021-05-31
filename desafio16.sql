edc8ab8dfeb22d96c2d8b2932e6a487694d7f122
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-05-31 13:27:36 -0300

    requisitos 11, 13, 16 e 17

---
+++ desafio16.sql
@@ -0,0 +1,18 @@
+USE hr;
+DELIMITER $$ 
+ 
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(EMAIL_EMPLOYEES VARCHAR(25))
+RETURNS INT READS SQL DATA
+BEGIN 
+    DECLARE amount_of_jobs INT;
+    SELECT 
+    COUNT(*)
+FROM
+    hr.employees AS E
+        INNER JOIN
+    hr.job_history AS JH ON JH.EMPLOYEE_ID = E.EMPLOYEE_ID
+WHERE EMAIL = EMAIL_EMPLOYEES INTO amount_of_jobs;
+    RETURN amount_of_jobs; 
+END $$ 
+ 
+DELIMITER ; 

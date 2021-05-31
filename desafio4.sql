62a7d05479d76896492f4126e093f878b1dc5eb9
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-05-31 13:37:55 -0300

    desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,13 @@
-SELECT JOB_ID as Cargo, ROUND(AVG(SALARY), 2) as `Média Salarial`,
-CASE
-	WHEN AVG(SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
-    WHEN AVG(SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
-    WHEN AVG(SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
-    ELSE 'CEO'
-	END as 'Senioridade'
-FROM employees
+SELECT 
+    JOB_ID AS Cargo,
+    ROUND(AVG(SALARY), 2) AS `Média Salarial`,
+    CASE
+        WHEN AVG(SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS 'Senioridade'
+FROM
+    employees
 GROUP BY JOB_ID
-ORDER BY `Média Salarial`, JOB_ID;
+ORDER BY `Média Salarial` , JOB_ID;

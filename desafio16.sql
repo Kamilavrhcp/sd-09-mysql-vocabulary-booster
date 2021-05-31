3b65d86cdaa74b852b3642369a9c9573d13f4ce6
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-05-31 13:32:30 -0300

    codeclimate

--- desafio16.sql
+++ desafio16.sql
@@ -1,6 +1,6 @@
 USE hr;
 DELIMITER $$ 
- 
+
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(EMAIL_EMPLOYEES VARCHAR(25))
 RETURNS INT READS SQL DATA
 BEGIN 
@@ -14,5 +14,5 @@ FROM
 WHERE EMAIL = EMAIL_EMPLOYEES INTO amount_of_jobs;
     RETURN amount_of_jobs; 
 END $$ 
- 
+
 DELIMITER ; 
--- desafio17.sql
+++ desafio17.sql
@@ -1,11 +1,11 @@
 USE w3schools;
 DELIMITER $$ 
- 
+
 CREATE TRIGGER InsertOrderDate 
 	BEFORE INSERT ON orders 
 	FOR EACH ROW 
 BEGIN 
     SET NEW.OrderDate = NOW();
 END $$
- 
+
 DELIMITER ; 

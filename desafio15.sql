1e5662a463fc3415c467423dd8be5f0dfb1c7244
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-05-31 11:48:14 -0300

    desafio 15 codeclimate

--- desafio15.sql
+++ desafio15.sql
@@ -3,17 +3,17 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(100))
 BEGIN
-	SELECT
-		t.Média
-	FROM
-		(SELECT DISTINCT
-			j.JOB_TITLE AS 'Cargo',
-				ROUND(AVG(e.SALARY), 2) AS 'Média'
-		FROM
-			jobs AS j
+  SELECT
+    t.Média
+  FROM
+    (SELECT DISTINCT
+      j.JOB_TITLE AS 'Cargo',
+        ROUND(AVG(e.SALARY), 2) AS 'Média'
+    FROM
+      jobs AS j
         INNER JOIN
-			employees AS e ON j.JOB_ID = e.JOB_ID
-		GROUP BY j.JOB_TITLE) AS t
+      employees AS e ON j.JOB_ID = e.JOB_ID
+    GROUP BY j.JOB_TITLE) AS t
 WHERE t.Cargo = cargo;
 END $$
 

4083cebef76fb8c81a8f530830c2fbd69ce4b28a
Lucas Silva <martssilvapro@gmail.com>
2021-06-01 13:42:31 -0300

    correcao codeclimate

--- desafio8.sql
+++ desafio8.sql
@@ -5,9 +5,9 @@ FROM w3schools.customers AS customers
 INNER JOIN w3schools.orders AS orders
 ON customers.CustomerID = orders.CustomerID
 INNER JOIN(
-	SELECT * FROM w3schools.shippers
-	WHERE ShipperName = 'Speedy Express'
-    OR ShipperName = 'United Package'
+SELECT * FROM w3schools.shippers
+WHERE ShipperName = 'Speedy Express'
+OR ShipperName = 'United Package'
 ) AS shippers
 ON orders.ShipperID = shippers.ShipperID
 ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido` ASC;

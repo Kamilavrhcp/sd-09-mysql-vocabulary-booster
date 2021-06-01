dabea30b3cdc2a9c0d2194b2c7b9b6d16974ace8
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-05-31 22:20:15 -0300

    desafio 15 fix tabs

--- desafio15.sql
+++ desafio15.sql
@@ -1,14 +1,14 @@
 DELIMITER $$
 CREATE PROCEDURE buscar_media_por_cargo(in cargo varchar(50))
 BEGIN
-	SELECT 
-		ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
-	FROM
-		hr.employees AS e
-			INNER JOIN
-		hr.jobs AS j ON j.JOB_ID = e.JOB_ID
-	WHERE
-		j.JOB_TITLE = cargo;
+SELECT 
+	ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+FROM
+	hr.employees AS e
+		INNER JOIN
+	hr.jobs AS j ON j.JOB_ID = e.JOB_ID
+WHERE
+	j.JOB_TITLE = cargo;
 END $$
 DELIMITER ;
 

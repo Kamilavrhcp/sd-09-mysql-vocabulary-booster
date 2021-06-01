0a230bb10e675f62106a68315d0bf780a84213f2
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-06-01 14:46:03 -0300

    desafio 18 fix date_format

--- desafio18.sql
+++ desafio18.sql
@@ -1,14 +1,14 @@
 SELECT 
     CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME) AS 'Nome completo',
     CONCAT(DAY(jh.START_DATE),
-            '-',
+            '/',
             MONTH(jh.START_DATE),
-            '-',
+            '/',
             YEAR(jh.START_DATE)) AS 'Data de início',
     CONCAT(DAY(jh.END_DATE),
-            '-',
+            '/',
             MONTH(jh.END_DATE),
-            '-',
+            '/',
             YEAR(jh.END_DATE)) AS 'Data de rescisão',
     ROUND(DATEDIFF(jh.END_DATE, jh.START_DATE) / 365,
             2) AS 'Anos trabalhados'
@@ -16,4 +16,4 @@ FROM
     job_history AS jh
         INNER JOIN
     employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
-ORDER BY `Nome completo` , `Anos trabalhados`;
+ORDER BY `Nome completo` , `Anos trabalhados` ASC;

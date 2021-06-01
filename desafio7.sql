77f8dca9ff343bb7b25d813095355a25b30e2c36
Lucas Silva <martssilvapro@gmail.com>
2021-06-01 14:04:31 -0300

    correcao query desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -1,5 +1,5 @@
 SELECT UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS `Nome completo`,
-DATE(jobHistory.START_DATE) AS `Data de início`, employees.SALARY AS Salary
+DATE(jobHistory.START_DATE) AS `Data de início`, employees.SALARY AS Salário
 FROM hr.employees AS employees
 INNER JOIN hr.job_history AS jobHistory
 ON employees.EMPLOYEE_ID = jobHistory.EMPLOYEE_ID

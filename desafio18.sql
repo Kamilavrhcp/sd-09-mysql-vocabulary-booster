e32df9048c386c158b900dff367198a97c2e453b
Hudson Carlos <hc88806@gmail.com>
2021-06-01 14:17:21 -0400

    challenge 18 done

---
+++ desafio18.sql
@@ -0,0 +1,7 @@
+SELECT CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS 'Nome completo',
+DATE_FORMAT(H.START_DATE, '%d/%m/%Y') AS 'Data de início',
+DATE_FORMAT(H.END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+ROUND((DATEDIFF(H.END_DATE, START_DATE))/365, 2) AS 'Anos trabalhados'
+FROM hr.employees AS E INNER JOIN hr.job_history AS H
+ON E.EMPLOYEE_ID = H.EMPLOYEE_ID
+ORDER BY 1, 4;

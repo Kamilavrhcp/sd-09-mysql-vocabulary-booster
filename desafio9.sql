c88c893e286d07f0634463e12a766dc4cb4b2ce1
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-05-31 21:25:48 -0300

    Code Climate

--- desafio9.sql
+++ desafio9.sql
@@ -1,9 +1,9 @@
 SELECT 
-	CONCAT(employees.FirstName, ' ', LastName) AS "Nome completo",
-    COUNT(*) AS 'Total de pedidos'
+CONCAT(employees.FirstName, ' ', LastName) AS "Nome completo",
+COUNT(*) AS 'Total de pedidos'
 FROM
-    w3schools.employees AS employees
-        INNER JOIN
-    w3schools.orders AS orders ON orders.EmployeeID = employees.EmployeeID
+w3schools.employees AS employees
+INNER JOIN
+w3schools.orders AS orders ON orders.EmployeeID = employees.EmployeeID
 GROUP BY employees.EmployeeID
 ORDER BY COUNT(*);

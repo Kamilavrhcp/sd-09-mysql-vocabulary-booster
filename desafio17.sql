a6fabe22907dfd19f789b571d44a28108cd96c5f
Lucas Silva <martssilvapro@gmail.com>
2021-06-02 10:08:46 -0300

    ajustes

--- desafio17.sql
+++ desafio17.sql
@@ -1,12 +1,9 @@
 USE w3schools;
-
 DELIMITER $$
 CREATE TRIGGER trigger_actual_date
-	BEFORE INSERT ON orders
-    FOR EACH ROW
+BEFORE INSERT ON orders
+FOR EACH ROW
 BEGIN 
-	SET NEW.OrderDate = NOW();
+SET NEW.OrderDate = NOW();
 END $$
 DELIMITER ;    
-
--- SELECT * FROM w3schools.orders;

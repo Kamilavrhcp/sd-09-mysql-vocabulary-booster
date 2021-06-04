b5a31c19fe55caadbb6932ca57794e081f8345ee
hannibal1207 <mathausannibal@gmail.com>
2021-06-04 18:58:40 -0300

    req 8,9,10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT p.ProductName AS 'Produto',
+MIN(o.Quantity) AS 'Mínima',
+MAX(o.Quantity) AS 'Máxima',
+ROUND(AVG(o.Quantity),2) AS 'Média'  
+FROM w3schools.products AS p
+JOIN w3schools.order_details AS o ON p.ProductID = o.ProductID
+GROUP BY o.ProductID
+HAVING AVG(o.Quantity) > 20
+ORDER BY AVG(o.Quantity), p.ProductName
--- desafio8.sql
+++ desafio8.sql
@@ -7,5 +7,5 @@ FROM
         JOIN
     w3schools.customers AS c ON o.CustomerID = c.CustomerID
         JOIN
-    w3schools.shippers AS s ON o.ShipperID = s.ShipperID
-ORDER BY c.ContactName , s.ShipperName
+    w3schools.shippers AS s ON o.ShipperID = s.ShipperID AND (o.ShipperID = 1 OR o.ShipperID = 2)
+ORDER BY c.ContactName , s.ShipperName, o.OrderDate

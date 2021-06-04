b08db94ec2a27626107559b0b60e666c162f4a07
Enio Nicael <enionicael@gmail.com>
2021-06-04 14:39:30 -0300

    Correcoes do code climate.

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,7 @@ USE hr;
 DELIMITER $$;
 CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(50))
 BEGIN
-	  SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+	SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
     FROM employees AS e
     INNER JOIN jobs AS j ON j.JOB_ID = e.JOB_ID
     WHERE j.JOB_TITLE = job;

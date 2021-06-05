517b178cf67d904273ebd6ace7caa4b72ae52f15
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-06-05 15:19:48 -0300

    correção requisito 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,6 +1,6 @@
 SELECT JOB_TITLE AS 'Cargo',
 ( MAX_SALARY - MIN_SALARY ) AS 'Variação Salarial',
-ROUND(MAX_SALARY/12, 2) AS 'Média máxima',
-ROUND(MIN_SALARY/12, 2) AS 'Média mínima'
+ROUND(MAX_SALARY/12, 2) AS 'Média máxima mensal',
+ROUND(MIN_SALARY/12, 2) AS 'Média mínima mensal'
 FROM hr.jobs
 ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE ASC;

39308bd1bb4ed533c72d8ef9b7e5ab585c42ee30
Raphael Rêgo <raphael.mlrego@gmail.com>
2021-06-05 18:16:20 -0300

    feito desafio 13

---
+++ desafio13.sql
@@ -0,0 +1,8 @@
+SELECT
+p.ProductName AS 'Produto',
+p.Price AS 'Preço'
+FROM w3schools.order_details AS d
+INNER JOIN w3schools.products AS p
+  ON d.ProductID = p.ProductID
+WHERE d.Quantity > 80
+ORDER BY `Produto`;

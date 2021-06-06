1af0ac582d4f5abd7156ea5b77167ea519729654
Lucas Silva <martssilvapro@gmail.com>
2021-06-06 18:39:01 -0300

    corrige desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -1,14 +1,14 @@
 SELECT DISTINCT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
-jobs.JOB_TITLE AS Cargo, jobHistory.START_DATE AS `Data de início do cargo`,
-departments.DEPARTMENT_NAME AS Departamento
--- FAZER COM JOB_HISTORY
-FROM hr.job_history AS employees
+JOB_TITLE AS Cargo, jobHistory.START_DATE AS `Data de início do cargo`, DEPARTMENT_NAME AS Departamento
+FROM hr.job_history AS jobHistory
+INNER JOIN hr.employees AS employees
+ON jobHistory.JOB_ID = employees.JOB_ID
 INNER JOIN hr.jobs AS jobs
-ON employees.JOB_ID = jobs.JOB_ID
-INNER JOIN hr.job_history AS jobHistory
-ON employees.EMPLOYEE_ID = jobHistory.EMPLOYEE_ID
+ON jobHistory.JOB_ID = jobs.JOB_ID
 INNER JOIN hr.departments AS departments
-ON employees.DEPARTMENT_ID = departments.DEPARTMENT_ID
+ON jobHistory.DEPARTMENT_ID = departments.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, Cargo;
--- Dúvida porque tem resultados duplicados?
--- Dúvida como fazer a diferenciação de id quando os id são iguais?
+-- SELECT * FROM hr.employees;
+-- SELECT * FROM hr.job_history;
+-- SELECT * from hr.jobs;
+-- select * from hr.departments;

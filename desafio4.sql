ab0ada1ba1d3c88a780101c1aa632e6a642fad35
Alstent <rafaelasltent@gmail.com>
2021-06-06 23:44:32 +0200

    desafio 4, 7, 8 e 9

--- desafio4.sql
+++ desafio4.sql
@@ -4,7 +4,7 @@ SELECT j.job_title AS 'Cargo',
     WHEN AVG(e.salary) BETWEEN 2000 AND 5800 THEN 'Júnior'
     WHEN AVG(e.salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
     WHEN AVG(e.salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
-    WHEN AVG(e.salary) BETWEEN 7501 AND 10500 THEN 'CEO'
+    WHEN AVG(e.salary) > 10501  THEN 'CEO'
     END AS 'Senioridade'
 FROM hr.jobs AS j INNER JOIN hr.employees AS e ON j.job_id = e.job_id
 GROUP BY Cargo

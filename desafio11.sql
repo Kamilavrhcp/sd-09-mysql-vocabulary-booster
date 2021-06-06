f15fc3fcf030f16688f26332f15edbfe3c233d7e
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-06-06 20:07:50 -0300

    desafios 6-11 codeclimate maybe

--- desafio11.sql
+++ desafio11.sql
@@ -1,15 +1,10 @@
-SELECT * FROM w3schools.customers;
-
 SELECT
-	c1.ContactName AS `Nome`,
-    c1.Country AS `País`,
-    (SELECT
-		COUNT(*)
-	FROM
-		w3schools.customers AS c2
-	WHERE
-		c1.Country = c2.Country) AS `Número de compatriotas`
-FROM
-	w3schools.customers AS c1
-ORDER BY
-	`Nome` ASC;
+c1.ContactName AS `Nome`,
+c1.Country AS `País`,
+(SELECT COUNT(*)
+FROM w3schools.customers AS c2
+WHERE c1.Country = c2.Country AND c1.CustomerID <> c2.CustomerID) AS `Número de compatriotas`
+FROM w3schools.customers AS c1
+WHERE (SELECT COUNT(*) FROM w3schools.customers AS c2 
+WHERE c1.Country = c2.Country AND c1.CustomerID <> c2.CustomerID) > 0
+ORDER BY `Nome` ASC;
--- desafio7.sql
+++ desafio7.sql
@@ -1,12 +1,10 @@
 SELECT
-	UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome completo`,
-    jh.START_DATE AS `Data de início`,
-    e.SALARY AS 'Salário'
-FROM
-	hr.job_history AS jh
-    INNER JOIN hr.employees AS e
-    ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
-    WHERE MONTH(jh.START_DATE) BETWEEN 1 AND 3
-ORDER BY
-	`Nome completo` ASC, `Data de início` ASC;
+UCASE(CONCAT(e.FIRST_NAME, ' ', e.LAST_NAME)) AS `Nome completo`,
+jh.START_DATE AS `Data de início`,
+e.SALARY AS 'Salário'
+FROM hr.job_history AS jh
+INNER JOIN hr.employees AS e
+ON jh.EMPLOYEE_ID = e.EMPLOYEE_ID
+WHERE MONTH(jh.START_DATE) BETWEEN 1 AND 3
+ORDER BY `Nome completo` ASC, `Data de início` ASC;
     

97cddd3b7d3dcd5bd111d682007cb67f1a017a25
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-07 16:42:45 -0300

    Challenge three

--- desafio3.sql
+++ desafio3.sql
@@ -1,6 +1,12 @@
-SELECT 
-    JOB_TITLE AS Cargo,
-    (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
-FROM
-    hr.jobs
-ORDER BY (MAX_SALARY - MIN_SALARY), JOB_TITLE;
+SELECT
+    JOB_TITLE AS `Cargo`,
+    ROUND(AVG(SALARY), 2) AS `Média Salarial`,
+    CASE
+		WHEN AVG(SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
+        WHEN AVG(SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
+        WHEN AVG(SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
+        ELSE 'CEO'
+    END AS `Senioridade`
+FROM hr.employees INNER JOIN hr.jobs ON hr.jobs.JOB_ID = hr.employees.JOB_ID
+GROUP BY hr.employees.JOB_ID
+ORDER BY `Média Salarial` ASC;

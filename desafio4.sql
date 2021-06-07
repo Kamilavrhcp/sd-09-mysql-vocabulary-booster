e59adf56c1df7bba3b6b28cfaa0e8b9ef1d4467e
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-07 17:06:48 -0300

    Challenge four

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT
+	JOB_ID AS `Cargo`,
+    MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
+    MIN_SALARY / 12 AS `Média mínima mensal`,
+    MAX_SALARY / 12 AS `Média máxima mensal`
+FROM hr.jobs
+ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;

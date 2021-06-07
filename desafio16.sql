d2ca23785d3f13281316908e15ac71d1529381ec
Uibirá <usbarreto@gmail.com>
2021-06-07 16:02:58 -0300

    D.16: Fatality

---
+++ desafio16.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(100))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE CargoNumber INT;
+SELECT
+COUNT(H.EMPLOYEE_ID)
+FROM job_history AS H
+INNER JOIN employees AS E
+ON H.EMPLOYEE_ID = E.EMPLOYEE_ID
+WHERE E.EMAIL = email INTO CargoNumber;
+RETURN CargoNumber;
+END $$
+
+DELIMITER ;

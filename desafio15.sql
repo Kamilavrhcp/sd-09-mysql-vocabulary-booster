b94ef5137bf6c606bbbc73f36e5b3ff99f719ca4
Lucas Ribeiro <martins7ribeiro@gmail.com>
2021-06-07 15:52:27 -0300

    desafios 15-17 codeclimate hell yeah

--- desafio15.sql
+++ desafio15.sql
@@ -3,10 +3,10 @@ DELIMITER $$
 
 CREATE PROCEDURE buscar_media_por_cargo(IN job VARCHAR(50))
 BEGIN
-	SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
-	FROM employees AS e INNER JOIN jobs AS j
-	ON j.JOB_ID = e.JOB_ID
-	WHERE JOB_TITLE = job;
+SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+FROM employees AS e INNER JOIN jobs AS j
+ON j.JOB_ID = e.JOB_ID
+WHERE JOB_TITLE = job;
 END $$
 
 DELIMITER ;
--- desafio16.sql
+++ desafio16.sql
@@ -4,7 +4,7 @@ DELIMITER $$
 CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(email VARCHAR(80))
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE jobs INT;
+DECLARE jobs INT;
     SELECT COUNT(j.JOB_ID)
     FROM job_history AS j
     INNER JOIN employees AS e

2c7af4cffffe539918cab9fb99bc93ee6d5e1050
Uibirá <usbarreto@gmail.com>
2021-06-06 23:25:52 -0300

    D.06 e D.07 novamente outra vez

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,13 @@
 SELECT
 DISTINCT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
 jobs.JOB_TITLE AS Cargo,
-jobHist.START_DATE AS `Data de início do cargo`,
+jobHistory.START_DATE AS `Data de início do cargo`,
 departments.DEPARTMENT_NAME AS Departamento
-FROM hr.job_history AS jobHist
+FROM hr.job_history AS jobHistory
 INNER JOIN hr.employees AS employees
-ON jobHist.JOB_ID = employees.JOB_ID
+ON jobHistory.JOB_ID = employees.JOB_ID
 INNER JOIN hr.jobs AS jobs
-ON jobHist.JOB_ID = jobs.JOB_ID
+ON jobHistory.JOB_ID = jobs.JOB_ID
 INNER JOIN hr.departments AS departments
-ON jobHist.DEPARTMENT_ID = departments.DEPARTMENT_ID
+ON jobHistory.DEPARTMENT_ID = departments.DEPARTMENT_ID
 ORDER BY `Nome completo` DESC, Cargo;
--- desafio7.sql
+++ desafio7.sql
@@ -1,8 +1,9 @@
 SELECT
 UCASE(CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME)) AS 'Nome completo',
-jobHist.START_DATE AS 'Data de início',
+DATE(jobHist.START_DATE) AS 'Data de início',
 employees.SALARY AS 'Salário'
 FROM hr.employees AS employees
 INNER JOIN hr.job_history AS jobHist
 ON employees.EMPLOYEE_ID = jobHist.EMPLOYEE_ID
+WHERE MONTH(DATE(jobHist.START_DATE)) IN (01, 02, 03)
 ORDER BY `Nome completo` ASC, `Data de início` ASC;

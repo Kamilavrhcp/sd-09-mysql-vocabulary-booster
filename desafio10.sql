1609eeea7f91ebb7eb05c8710ba353ed4bbd4bd2
Uibirá <usbarreto@gmail.com>
2021-06-07 11:23:12 -0300

    D.10: Corrige seleção das médias, que precisam ser apenas acima de 20,00

--- desafio10.sql
+++ desafio10.sql
@@ -7,5 +7,6 @@ ROUND(AVG(D.Quantity), 2) AS Média
 FROM w3schools.products AS P
 INNER JOIN w3schools.order_details AS D
 ON P.ProductID = D.ProductID
-GROUP BY P.ProductID
+GROUP BY P.ProductName
+HAVING ROUND(AVG(D.Quantity), 2) > 20.00
 ORDER BY Média, Produto;

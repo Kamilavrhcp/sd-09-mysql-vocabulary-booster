7af9d4d93bcec31fccb7f5fc94145749c6ae48fc
Uibirá <usbarreto@gmail.com>
2021-06-07 07:27:51 -0300

    D.08

--- desafio6.sql
+++ desafio6.sql
@@ -1,13 +1,13 @@
 SELECT
 DISTINCT CONCAT(employees.FIRST_NAME, ' ', employees.LAST_NAME) AS `Nome completo`,
-jobs.JOB_TITLE AS Cargo,
-jobHistory.START_DATE AS `Data de início do cargo`,
-departments.DEPARTMENT_NAME AS Departamento
-FROM hr.job_history AS jobHistory
+jobs.JOB_TITLE AS `Cargo`,
+jobHist.START_DATE AS `Data de início do cargo`,
+departments.DEPARTMENT_NAME AS `Departamento`
+FROM hr.job_history AS jobHist
 INNER JOIN hr.employees AS employees
-ON jobHistory.JOB_ID = employees.JOB_ID
+ON jobHist.JOB_ID = employees.JOB_ID
 INNER JOIN hr.jobs AS jobs
-ON jobHistory.JOB_ID = jobs.JOB_ID
+ON jobHist.JOB_ID = jobs.JOB_ID
 INNER JOIN hr.departments AS departments
-ON jobHistory.DEPARTMENT_ID = departments.DEPARTMENT_ID
-ORDER BY `Nome completo` DESC, Cargo;
+ON jobHist.DEPARTMENT_ID = departments.DEPARTMENT_ID
+ORDER BY `Nome completo` DESC, Cargo ASC;

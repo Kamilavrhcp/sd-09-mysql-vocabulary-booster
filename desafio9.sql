c127045103188f4095492a198b0e223f3e18423c
Alstent <rafaelasltent@gmail.com>
2021-06-08 20:51:44 +0200

    requisito 9 e 10

--- desafio9.sql
+++ desafio9.sql
@@ -1,4 +1,4 @@
-SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome de contato',
+SELECT CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
   count(*) AS 'Total de pedidos'
 FROM w3schools.employees AS e
   INNER JOIN w3schools.orders AS o ON o.EmployeeID = e.EmployeeID

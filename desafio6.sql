d7c5ac21b9b3172be6567b757fe2d38d65cc3fa1
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-08 09:41:46 -0300

    START_DATE instead of HIRE_DATE

--- desafio6.sql
+++ desafio6.sql
@@ -1,9 +1,10 @@
 SELECT
     CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
     JOB_TITLE AS `Cargo`,
-    HIRE_DATE AS `Data de início do cargo`,
+    START_DATE AS `Data de início do cargo`,
     DEPARTMENT_NAME AS `Departamento`
 FROM hr.employees
     JOIN hr.jobs ON  hr.employees.JOB_ID = hr.jobs.JOB_ID
     JOIN hr.departments ON hr.employees.DEPARTMENT_ID = hr.departments.DEPARTMENT_ID
+    JOIN hr.job_history ON hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID
     ORDER BY `Nome completo`, `Cargo`;

664383480ef4da76f3873cc203dd7406a9669867
Antonio Schappo <antonioschappo@gmail.com>
2021-06-08 16:04:46 -0300

    third try challenge 16

--- desafio16.sql
+++ desafio16.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 CREATE PROCEDURE `buscar_quantidade_de_empregos_por_funcionario`(IN email VARCHAR(100))
 BEGIN
-SELECT COUNT(t1.EMPLOYEE_ID) FROM hr.job_history AS t1
+SELECT COUNT(t1.EMPLOYEE_ID) AS total_empregos FROM hr.job_history AS t1
 INNER JOIN hr.employees AS t2
 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
 WHERE t2.EMAIL = email;

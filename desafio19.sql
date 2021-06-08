4dfa9db07d47273de58448121c0fdb66b4041efc
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-06-08 03:27:43 -0300

    Lint FIX

--- desafio19.sql
+++ desafio19.sql
@@ -5,13 +5,13 @@ CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mes INT, ano
 RETURNS INT READS SQL DATA
 BEGIN
     DECLARE quant_contratacoes INT;
-	SELECT 
-		COUNT(*)
-	FROM
-		hr.employees
-	WHERE CONCAT(MONTH(HIRE_DATE),'-',YEAR(HIRE_DATE)) = CONCAT(mes,'-',ano)
-	INTO quant_contratacoes;
-	RETURN quant_contratacoes;
+    SELECT 
+        COUNT(*)
+    FROM
+        hr.employees
+    WHERE CONCAT(MONTH(HIRE_DATE),'-',YEAR(HIRE_DATE)) = CONCAT(mes,'-',ano)
+    INTO quant_contratacoes;
+    RETURN quant_contratacoes;
 END $$
 
 DELIMITER ;

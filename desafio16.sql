c89adfc8e563364bf152ee11f5b7ecf9f61cdf18
Antonio Schappo <antonioschappo@gmail.com>
2021-06-08 18:24:55 -0300

    first try challenge 17

--- desafio16.sql
+++ desafio16.sql
@@ -5,5 +5,5 @@ SELECT COUNT(t1.EMPLOYEE_ID) AS total_empregos FROM hr.job_history AS t1
 INNER JOIN hr.employees AS t2
 ON t1.EMPLOYEE_ID = t2.EMPLOYEE_ID
 WHERE t2.EMAIL = email;
-END 
+END $$
 DELIMITER ;

c683499f63972f3f6082acf0841c051f1978a7b7
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-08 09:14:42 -0300

    Excess table fields

--- desafio3.sql
+++ desafio3.sql
@@ -1,7 +1,5 @@
 SELECT
     JOB_TITLE AS `Cargo`,
-    MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínimo`,
-    ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`,
-    ROUND(MAX_SALARY / 12, 2) AS `Média máxima mensal`
+    MAX_SALARY - MIN_SALARY AS `Diferença entre salários máximo e mínimo`
 FROM hr.jobs
 ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;

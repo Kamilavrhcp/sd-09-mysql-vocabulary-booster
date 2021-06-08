1445995644695a6cf1f8710fa4c0d8e236f80534
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-08 09:06:11 -0300

    Swap three and four

--- desafio3.sql
+++ desafio3.sql
@@ -1,12 +1,7 @@
 SELECT
     JOB_TITLE AS `Cargo`,
-    ROUND(AVG(SALARY), 2) AS `Média salarial`,
-    CASE
-    WHEN AVG(SALARY) BETWEEN 2000 AND 5800 THEN 'Júnior'
-        WHEN AVG(SALARY) BETWEEN 5801 AND 7500 THEN 'Pleno'
-        WHEN AVG(SALARY) BETWEEN 7501 AND 10500 THEN 'Sênior'
-        ELSE 'CEO'
-    END AS `Senioridade`
-FROM hr.employees INNER JOIN hr.jobs ON hr.jobs.JOB_ID = hr.employees.JOB_ID
-GROUP BY hr.employees.JOB_ID
-ORDER BY `Média salarial` ASC, `Cargo` ASC;
+    MAX_SALARY - MIN_SALARY AS `Variação Salarial`,
+    ROUND(MIN_SALARY / 12, 2) AS `Média mínima mensal`,
+    ROUND(MAX_SALARY / 12, 2) AS `Média máxima mensal`
+FROM hr.jobs
+ORDER BY MAX_SALARY - MIN_SALARY, JOB_TITLE;

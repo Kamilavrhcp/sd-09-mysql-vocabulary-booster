c057ffe09110830e07830fd063cdb091b2e7780f
Gabriel Reis Barros Santana <gsantanaj18@gmail.com>
2021-06-08 16:23:10 -0300

    Refazendo requisito 3 e 6

--- desafio6.sql
+++ desafio6.sql
@@ -11,4 +11,4 @@ INNER JOIN
 hr.departments AS d ON jh.DEPARTMENT_ID=d.DEPARTMENT_ID
 INNER JOIN
 hr.jobs AS j ON j.JOB_ID=jh.JOB_ID
-ORDER BY 'Nome completo' DESC, 'Cargo';
+ORDER BY 'Nome Completo' DESC, 'Cargo';

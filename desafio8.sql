0c7d4ee0c168b2d8c4cadf8c1a106f7180068c62
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-10 17:17:19 -0300

    Challenge eight

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT
+    ContactName AS 'Nome de contato',
+    ShipperName AS 'Empresa que fez o pedido',
+    OrderDate AS 'Data do pedido'
+FROM w3schools.customers
+JOIN w3schools.orders ON w3schools.customers.CustomerID = w3schools.orders.CustomerID
+JOIN w3schools.shippers ON w3schools.orders.ShipperID = w3schools.shippers.ShipperID
+ORDER BY `Nome de contato`, `Empresa que fez o pedido`, `Data do pedido`;

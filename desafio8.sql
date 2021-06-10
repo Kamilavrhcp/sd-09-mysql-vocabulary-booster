8135a15a8f7883c2bec51c4126e74f37516a4b6c
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-10 17:20:09 -0300

    Restrict shippers

--- desafio8.sql
+++ desafio8.sql
@@ -5,4 +5,5 @@ SELECT
 FROM w3schools.customers
 JOIN w3schools.orders ON w3schools.customers.CustomerID = w3schools.orders.CustomerID
 JOIN w3schools.shippers ON w3schools.orders.ShipperID = w3schools.shippers.ShipperID
+ShipperName IN ('Speedy Express', 'United Package')
 ORDER BY `Nome de contato`, `Empresa que fez o pedido`, `Data do pedido`;

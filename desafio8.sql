e22e7985adcf5c1f934e7f05285b630387843516
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-10 17:27:39 -0300

    typo

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,9 @@
 SELECT
     ContactName AS 'Nome de contato',
-    ShipperName AS 'Empresa que fez o pedido',
+    ShipperName AS 'Empresa que fez o envio',
     OrderDate AS 'Data do pedido'
 FROM w3schools.customers
 JOIN w3schools.orders ON w3schools.customers.CustomerID = w3schools.orders.CustomerID
 JOIN w3schools.shippers ON w3schools.orders.ShipperID = w3schools.shippers.ShipperID
 WHERE ShipperName IN ('Speedy Express', 'United Package')
-ORDER BY `Nome de contato`, `Empresa que fez o pedido`, `Data do pedido`;
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;

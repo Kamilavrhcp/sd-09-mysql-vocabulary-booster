1be43e788cd8fae3b819b68990686f5bd3e92c40
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-10 15:50:53 -0300

    Upper case and filter month

--- desafio7.sql
+++ desafio7.sql
@@ -1,7 +1,8 @@
 SELECT
-    CONCAT(FIRST_NAME, ' ', LAST_NAME) AS `Nome completo`,
+    UPPER(CONCAT(FIRST_NAME, ' ', LAST_NAME)) AS `Nome completo`,
     START_DATE AS `Data de início`,
     SALARY AS `Salário`
 FROM hr.job_history
 JOIN hr.employees ON hr.job_history.EMPLOYEE_ID = hr.employees.EMPLOYEE_ID
+WHERE MONTH(START_DATE) IN (1, 2, 3)
 ORDER BY `Nome completo`, `Data de início`;

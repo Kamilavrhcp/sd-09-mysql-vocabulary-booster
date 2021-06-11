39bab1e0125ad838acf8c284fc2c03efadacca88
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-11 15:09:43 -0300

    Challenge ten

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT
+    ProductName AS `Produto`,
+    MIN(Quantity) AS `Mínima`,
+    MAX(Quantity) AS `Máxima`,
+    ROUND(AVG(Quantity), 2) AS `Média`
+FROM w3schools.orders
+JOIN w3schools.order_details ON w3schools.orders.OrderID = w3schools.order_details.OrderID
+JOIN w3schools.products ON w3schools.order_details.ProductID = w3schools.products.ProductID
+GROUP BY ProductName
+ORDER BY `Média`, `Produto`;

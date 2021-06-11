90d409132d0ef23b2f169855101712d7defe6ddc
Víctor Cançado <victor.cancado@gmail.com>
2021-06-11 18:44:55 -0300

    Adiciona Requisito 8

---
+++ desafio8.sql
@@ -0,0 +1,14 @@
+SELECT 
+    ContactName AS 'Nome de contato',
+    ShipperName AS 'Empresa que fez o envio',
+    OrderDate AS 'Data do pedido'
+FROM
+    w3schools.customers AS cus
+        INNER JOIN
+    w3schools.orders AS ord ON cus.CustomerId = ord.CustomerID
+        INNER JOIN
+    w3schools.shippers AS shp ON shp.ShipperID = ord.ShipperID
+WHERE
+    ShipperName = 'Speedy Express'
+        OR ShipperName = 'United Package'
+ORDER BY ContactName , ShipperName , OrderDate;

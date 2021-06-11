260da5cd249a549f3798f6e22314d257f9ad3b8b
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-11 15:14:45 -0300

    Filter with having

--- desafio10.sql
+++ desafio10.sql
@@ -7,4 +7,5 @@ FROM w3schools.orders
 JOIN w3schools.order_details ON w3schools.orders.OrderID = w3schools.order_details.OrderID
 JOIN w3schools.products ON w3schools.order_details.ProductID = w3schools.products.ProductID
 GROUP BY ProductName
+HAVING `Média` > 20
 ORDER BY `Média`, `Produto`;

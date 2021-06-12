5b17b2bb1e89ee43f01e6356fb20675047f28232
Víctor Cançado <victor.cancado@gmail.com>
2021-06-12 11:05:34 -0300

    Adiciona requisito 8

---
+++ desafio9.sql
@@ -0,0 +1,9 @@
+SELECT 
+    CONCAT(LastName, ' ', FirstName) AS 'Nome completo',
+    COUNT(ord.EmployeeID) AS 'Total de pedidos'
+FROM
+    w3schools.employees AS emp
+        INNER JOIN
+    w3schools.orders AS ord ON emp.EmployeeID = ord.EmployeeID
+GROUP BY `Nome completo`
+ORDER BY `Total de pedidos`;

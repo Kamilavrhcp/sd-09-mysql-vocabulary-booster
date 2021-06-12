6c89725f0c7e52afb7d988c1719505db6a295261
Alstent <rafaelasltent@gmail.com>
2021-06-12 17:56:49 +0200

    requisito 20

--- desafio20.sql
+++ desafio20.sql
@@ -8,6 +8,7 @@ CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN email VARCHAR(25))
       INNER JOIN hr.employees AS e ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
       INNER JOIN hr.departments AS d ON d.DEPARTMENT_ID = e.DEPARTMENT_ID
       INNER JOIN hr.jobs AS j ON j.JOB_ID = e.JOB_ID
-    WHERE e.EMAIL =  email;
+    WHERE e.EMAIL =  email
+    ORDER BY 2, 3;
   END
 !! DELIMITER ;

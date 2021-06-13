3271aafbe608fe33f7b1f6a7a4bbc56619a021ed
Kevin Fraga <kevinciolac@gmail.com>
2021-06-13 17:11:00 -0300

    desafio :four: fix

--- desafio4.sql
+++ desafio4.sql
@@ -32,7 +32,7 @@ SELECT
             hr.jobs
         WHERE
             JOB_ID = hr.employees.JOB_ID) AS Cargo,
-    ROUND(AVG(SALARY), 2) AS 'Média Salarial',
+    ROUND(AVG(SALARY), 2) AS 'Média salarial',
     CASE
         WHEN ROUND(AVG(SALARY), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
         WHEN ROUND(AVG(SALARY), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'

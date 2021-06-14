bcd043ae050580407c10a438a17322e58dc7fde3
Kevin Fraga <kevinciolac@gmail.com>
2021-06-14 04:43:37 -0300

    desafio :one::eight: fix

--- desafio18.sql
+++ desafio18.sql
@@ -20,11 +20,19 @@ SELECT
             hr.employees
         WHERE
             EMPLOYEE_ID = hr.job_history.EMPLOYEE_ID) AS 'Nome completo',
-	CONCAT(DAY(START_DATE), '/', MONTH(START_DATE), '/', YEAR(START_DATE)) AS 'Data de início',
-    CONCAT(DAY(END_DATE), '/', MONTH(END_DATE), '/', YEAR(END_DATE)) AS 'Data de rescisão',
+    CONCAT(DAY(START_DATE),
+            '/',
+            MONTH(START_DATE),
+            '/',
+            YEAR(START_DATE)) AS 'Data de início',
+    CONCAT(DAY(END_DATE),
+            '/',
+            MONTH(END_DATE),
+            '/',
+            YEAR(END_DATE)) AS 'Data de rescisão',
     ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS 'Anos trabalhados'
 FROM
-	hr.job_history
+    hr.job_history
 WHERE
-	START_DATE > 0
-ORDER BY `Nome completo`, `Anos trabalhados`;
+    START_DATE > 0
+ORDER BY `Nome completo` , `Anos trabalhados`;

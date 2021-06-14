357d4293cf530ad31a7eb8bb7942b44715e16575
Kevin Fraga <kevinciolac@gmail.com>
2021-06-14 01:47:52 -0300

    desafio :one::four: fix

--- desafio14.sql
+++ desafio14.sql
@@ -6,12 +6,12 @@ Use o banco w3schools como referência
 Monte uma query que exiba 01 coluna com o alias "País" contendo o nome do país. */
 
 SELECT 
-    Country
+    Country AS 'País'
 FROM
     w3schools.customers 
 UNION SELECT 
-    Country
+    Country AS 'País'
 FROM
     w3schools.suppliers
-ORDER BY Country
+ORDER BY `País`
 LIMIT 5;

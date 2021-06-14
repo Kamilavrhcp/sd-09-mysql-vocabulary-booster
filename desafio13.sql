c2217de138df71c0c29b7c7664569b3688fcf22f
Víctor Cançado <victor.cancado@gmail.com>
2021-06-14 12:00:49 -0300

    Altera identação requisito 13

--- desafio13.sql
+++ desafio13.sql
@@ -2,8 +2,8 @@ SELECT
     p.ProductName AS 'Produto', p.Price AS 'Preço'
 FROM
     w3schools.products AS p
-INNER JOIN
-	w3schools.order_details d
-ON p.ProductID = d.ProductID
-WHERE d.Quantity > 80
+        INNER JOIN
+    w3schools.order_details d ON p.ProductID = d.ProductID
+WHERE
+    d.Quantity > 80
 ORDER BY Produto;

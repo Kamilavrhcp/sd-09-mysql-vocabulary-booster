76e7f17d5c30b6108e55ddeb6bb0bae04abeb5dc
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-15 20:54:15 -0300

    Challenge 18

---
+++ desafio18.sql
@@ -0,0 +1,17 @@
+SELECT 
+    (SELECT 
+            CONCAT(FIRST_NAME, ' ', LAST_NAME)
+        FROM
+            hr.employees
+        WHERE
+            EMPLOYEE_ID = hr.job_history.EMPLOYEE_ID) AS 'Nome completo',
+    DATE_FORMAT(START_DATE, '%d/%m/%Y') AS 'Data de início',
+    DATE_FORMAT(END_DATE, '%d/%m/%Y') AS 'Data de rescisão',
+    ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS 'Anos trabalhados'
+FROM
+    hr.job_history
+WHERE
+    START_DATE > 0
+ORDER BY Nome completo , Anos trabalhados
+
+SELECT DATE_FORMAT('2017-06-15', '%d/%m%Y')

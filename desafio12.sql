99da84ccf43311097f189c84d9ad9e15d143eef8
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-06-15 20:07:18 -0300

    Challenge twelve

---
+++ desafio12.sql
@@ -0,0 +1,19 @@
+SELECT 
+    CONCAT(hr.employees.FIRST_NAME,
+            ' ',
+            hr.employees.LAST_NAME) AS 'Nome completo funcionário 1',
+    hr.employees.SALARY AS 'Salário funcionário 1',
+    hr.employees.PHONE_NUMBER AS 'Telefone funcionário 1',
+    CONCAT(employees2.FIRST_NAME,
+            ' ',
+            employees2.LAST_NAME) AS 'Nome completo funcionário 2',
+    employees2.SALARY AS 'Salário funcionário 2',
+    employees2.PHONE_NUMBER AS 'Telefone funcionário 2'
+FROM
+    hr.employees,
+    hr.employees AS employees2
+WHERE
+    hr.employees.JOB_ID = employees2.JOB_ID
+        AND hr.employees.FIRST_NAME <> employees2.FIRST_NAME
+        AND hr.employees.LAST_NAME <> employees2.LAST_NAME
+ORDER BY `Nome completo funcionário 1`, `Nome completo funcionário 2`;

7f14b06f3e36deaeb351aa3db1a2fe78eb88778c
Víctor Cançado <victor.cancado@gmail.com>
2021-06-17 17:45:08 -0300

    Adiciona requisito 19

---
+++ desafio19.sql
@@ -0,0 +1,20 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(mth INT, yr INT)
+RETURNS INT READS SQL DATA 
+BEGIN
+	DECLARE total_employees INT;
+SELECT 
+    COUNT(*)
+FROM
+    hr.employees
+WHERE
+    MONTH(HIRE_DATE) = mth
+        AND YEAR(HIRE_DATE) = yr INTO total_employees;
+    RETURN total_employees;
+END $$
+
+DELIMITER ;
+
+SELECT exibir_quantidade_pessoas_contratadas_por_mes_e_ano(6, 1987);

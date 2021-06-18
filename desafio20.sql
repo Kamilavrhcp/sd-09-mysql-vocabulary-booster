f14d385f35f4780e3fe84640623743c5e9da82b4
Víctor Cançado <victor.cancado@gmail.com>
2021-06-17 21:52:49 -0300

    Corrige codeclimate requisito 20

--- desafio20.sql
+++ desafio20.sql
@@ -3,21 +3,21 @@ DELIMITER $$
 
 CREATE PROCEDURE exibir_historico_completo_por_funcionario(IN emp_mail VARCHAR(50))
 BEGIN
-	SELECT 
-		CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
+SELECT 
+	CONCAT(emp.FIRST_NAME, ' ', emp.LAST_NAME) AS 'Nome completo',
         dep.DEPARTMENT_NAME AS 'Departamento',
         jb.JOB_TITLE AS 'Cargo'
-	FROM
-		hr.employees AS emp
+FROM
+	hr.employees AS emp
     INNER JOIN
-		hr.job_history AS jh
-	ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
+	hr.job_history AS jh
+ON emp.EMPLOYEE_ID = jh.EMPLOYEE_ID
     INNER JOIN
-		hr.departments AS dep 
-	ON jh.DEPARTMENT_ID = dep.DEPARTMENT_ID
+	hr.departments AS dep 
+ON jh.DEPARTMENT_ID = dep.DEPARTMENT_ID
     INNER JOIN
-		hr.jobs AS jb
-	ON jb.JOB_ID = jh.JOB_ID
+	hr.jobs AS jb
+ON jb.JOB_ID = jh.JOB_ID
     WHERE emp.EMAIL = emp_mail
     ORDER BY `Departamento`, `Cargo`;
 END $$

78c9405221dee83cc998ca13d9b7b872e82ec99d
Lucas Duque <lucas3rios2014@gmail.com>
2021-06-20 19:36:15 -0300

    desafio 18

---
+++ desafio18.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT_WS(" ", e.FIRST_NAME, e.LAST_NAME) AS "Nome completo",
+DATE_FORMAT(j.START_DATE, '%d/%m/%Y') AS "Data de início",
+DATE_FORMAT(j.END_DATE, '%d/%m/%Y') AS "Data de rescisão",
+ROUND(TIMESTAMPDIFF(DAY, j.START_DATE, j.END_DATE)/365, 2) AS "Anos trabalhados"
+FROM hr.job_history AS j
+INNER JOIN hr.employees AS e ON j.EMPLOYEE_ID = e.EMPLOYEE_ID
+ORDER BY CONCAT_WS(" ", e.FIRST_NAME, e.LAST_NAME),
+ROUND(TIMESTAMPDIFF(DAY, j.START_DATE, j.END_DATE)/365, 2);

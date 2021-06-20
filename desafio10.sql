2342d6294f14f08fa4d8f3feb7d16ee01814c885
Lucas Duque <lucas3rios2014@gmail.com>
2021-06-20 00:24:46 -0300

    esqueci de descomentar

--- desafio10.sql
+++ desafio10.sql
@@ -1,6 +1,6 @@
 SELECT products.ProductName AS Produto,
--- MIN(details.Quantity) AS Mínima,
--- MAX(details.Quantity) AS Máxima,
+MIN(details.Quantity) AS Mínima,
+MAX(details.Quantity) AS Máxima,
 ROUND(AVG(details.Quantity), 2) AS Média
 FROM w3schools.order_details AS details
 INNER JOIN w3schools.products AS products ON details.ProductID = products.ProductID

067e56c949f2cef91f28cdc0c7e62f803453a41b
Sabrina <sasa2605.alves@gmail.com>
2021-06-20 14:26:25 -0300

    Requirement 11-14 Code Climate corrections

--- desafio13.sql
+++ desafio13.sql
@@ -3,7 +3,7 @@ ProductName AS 'Produto',
 Price AS 'Preço'
 FROM w3schools.products
 WHERE ProductID IN(
-	SELECT ProductID
-	FROM w3schools.order_details
-	WHERE Quantity > 80)
+SELECT ProductID
+FROM w3schools.order_details
+WHERE Quantity > 80)
 ORDER BY ProductName ASC;

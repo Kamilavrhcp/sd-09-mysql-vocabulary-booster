1ee1a3bd15d4d7cecf00bde25ed8597ff30d4c92
Lucas Duque <lucas3rios2014@gmail.com>
2021-06-19 23:39:44 -0300

    desafio 9

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT CONCAT_WS(" ", employees.FirstName, employees.LastName) AS "Nome completo",
+COUNT(orders.OrderID) AS "Total de pedidos"
+FROM w3schools.orders AS orders
+INNER JOIN w3schools.employees AS employees ON orders.EmployeeID = employees.EmployeeID
+GROUP BY orders.EmployeeID
+ORDER BY COUNT(orders.OrderID);

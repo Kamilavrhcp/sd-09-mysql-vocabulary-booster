2b015aa21b0c15f05fe512e97ebdbcfb90adda9d
Lucas Duque <lucas3rios2014@gmail.com>
2021-06-20 16:38:31 -0300

    consertando desafio 15 parte 2

--- desafio15.sql
+++ desafio15.sql
@@ -1,11 +1,10 @@
 USE hr;
 DELIMITER $$
-CREATE PROCEDURE buscar_media_por_cargo(IN Cargo VARCHAR(35), OUT media DOUBLE)
+CREATE PROCEDURE buscar_media_por_cargo(IN Cargo VARCHAR(35))
 BEGIN
 SELECT ROUND(AVG(e.SALARY), 2) AS "Média salarial"
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-WHERE j.JOB_TITLE = Cargo
-INTO media;
+WHERE j.JOB_TITLE = Cargo;
 END $$
 DELIMITER ;

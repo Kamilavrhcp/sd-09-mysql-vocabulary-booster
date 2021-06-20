989e43966c4054ff21722f7981e2808684628563
Lucas Duque <lucas3rios2014@gmail.com>
2021-06-19 22:56:33 -0300

    consertando o join

--- desafio6.sql
+++ desafio6.sql
@@ -1,8 +1,9 @@
 SELECT CONCAT_WS(" ", e.FIRST_NAME, e.LAST_NAME) AS "Nome completo",
 j.JOB_TITLE as Cargo,
-DATE(e.HIRE_DATE) AS "Data de início do cargo",
+DATE(jh.START_DATE) AS "Data de início do cargo",
 d.DEPARTMENT_NAME AS "Departamento"
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
+INNER JOIN hr.job_history AS jh ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
 INNER JOIN hr.departments AS d ON e.DEPARTMENT_ID = d.DEPARTMENT_ID
 ORDER BY CONCAT_WS(" ", e.FIRST_NAME, e.LAST_NAME) DESC, Cargo;

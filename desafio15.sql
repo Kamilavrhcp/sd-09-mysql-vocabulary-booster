345cba2c3297bc7d54a565db3d38d76466997fa3
Lucas Duque <lucas3rios2014@gmail.com>
2021-06-20 16:31:25 -0300

    consertando desafio 15

--- desafio15.sql
+++ desafio15.sql
@@ -5,6 +5,7 @@ BEGIN
 SELECT ROUND(AVG(e.SALARY), 2) AS "Média salarial"
 FROM hr.employees AS e
 INNER JOIN hr.jobs AS j ON e.JOB_ID = j.JOB_ID
-WHERE j.JOB_TITLE = Cargo;
+WHERE j.JOB_TITLE = Cargo
+INTO media;
 END $$
 DELIMITER ;

107d9184a065e8f12f594d9536fb76ab321490e0
Sabrina <sasa2605.alves@gmail.com>
2021-06-21 11:55:17 -0300

    requirement 16 e 19

--- desafio19.sql
+++ desafio19.sql
@@ -4,12 +4,11 @@ DELIMITER $$
 CREATE FUNCTION exibir_quantidade_pessoas_contratadas_por_mes_e_ano(month INT, year INT)
 RETURNS INT READS SQL DATA
 BEGIN
-	DECLARE hired_amount INT;
-	SELECT COUNT(*)
-	FROM hr.employees AS h
-	WHERE MONTH(h.HIRE_DATE) = month AND YEAR(h.HIRE_DATE) = year
+DECLARE hired_amount INT;
+SELECT COUNT(*)
+FROM hr.employees AS h
+WHERE MONTH(h.HIRE_DATE) = month AND YEAR(h.HIRE_DATE) = year
     INTO hired_amount;
     RETURN hired_amount;
 END$$
-
 DELIMITER ;

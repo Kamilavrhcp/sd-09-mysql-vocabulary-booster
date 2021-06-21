02838cdda5a9d065fecdb702217b33feeb072292
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-06-21 01:16:57 -0300

    desafio sql 16 - fix

--- desafio16.sql
+++ desafio16.sql
@@ -1,5 +1,14 @@
-SELECT  jobs.JOB_TITLE AS 'Cargo',
-        jobs.MAX_SALARY - jobs.MIN_SALARY AS 'Diferença entre salários máximo e mínimo'
-FROM  hr.jobs
-ORDER BY  jobs.MAX_SALARY - jobs.MIN_SALARY,
-          jobs.JOB_TITLE;
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(usermail VARCHAR(50))
+RETURNS INT READS SQL DATA
+BEGIN
+DECLARE result INT;
+SELECT COUNT(*) FROM hr.job_history AS t1
+INNER JOIN hr.employees AS t2 ON t1.employee_id = t2.employee_id
+WHERE t2.email = usermail INTO result;
+RETURN result;
+END $$
+
+DELIMITER ;

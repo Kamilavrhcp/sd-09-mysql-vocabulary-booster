8d1f9cc610abacbfcffd0db837cca9407da3f778
Sabrina <sasa2605.alves@gmail.com>
2021-06-20 21:13:26 -0300

    Corrections

---
+++ desafio15.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(IN job varchar(300))
+BEGIN
+SELECT
+ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
+FROM employees AS e
+INNER JOIN jobs AS j
+ON j.job = job;
+END $$
+
+DELIMITER ;
+
+CALL buscar_media_por_cargo('Programmer');
+/*Consultei o código do colega Kevin Fraga para
+entender o uso da sintaxe do stored procedure no projeto*/

8cfb08584d51426eadd49c8e52129d4ef2ad83ef
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-06-20 23:05:42 -0300

    desafio sql 8 - fix style

--- desafio8.sql
+++ desafio8.sql
@@ -7,4 +7,3 @@ INNER JOIN w3schools.customers AS t2 ON t1.customerID = t2.customerID
 INNER JOIN w3schools.shippers AS t3 ON t1.shipperID = t3.shipperID
 WHERE t1.shipperID IN (1, 2)
 ORDER BY 1 ASC, 2 ASC, 3 ASC;
-

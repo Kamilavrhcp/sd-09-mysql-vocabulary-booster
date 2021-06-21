a6a98f59beca6a4641532ae176a1c207b0fe4358
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-06-21 00:13:42 -0300

    desafio sql 8  - fix

--- desafio8.sql
+++ desafio8.sql
@@ -1,9 +1,10 @@
-SELECT
-  t2.ContactName AS 'Nome de contato',
-  t3.ShipperName AS 'Empresa que fez o envio',
-  t1.OrderDate AS 'Data do pedido'
-FROM w3schools.customers AS t1
-INNER JOIN w3schools.customers AS t2 ON t1.customerID = t2.customerID
-INNER JOIN w3schools.shippers AS t3 ON t1.shipperID = t3.shipperID
-WHERE t1.shipperID IN (1, 2)
-ORDER BY 1 ASC, 2 ASC, 3 ASC;
+SELECT  customers.ContactName AS `Nome de contato`,
+        shippers.ShipperName AS `Empresa que fez o envio`,
+        orders.OrderDate AS `Data do pedido`
+FROM w3schools.orders
+JOIN w3schools.customers ON orders.CustomerID = customers.CustomerID
+JOIN w3schools.shippers ON orders.ShipperID = shippers.ShipperID
+WHERE shippers.ShipperName = 'Speedy Express' OR shippers.ShipperName = 'United Package'
+ORDER BY  `Nome de contato`,
+          `Empresa que fez o envio`,
+          `Data do pedido`;

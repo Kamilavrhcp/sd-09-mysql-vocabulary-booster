fe0421fc31b1b7229075fa060da053c17f440d69
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-06-21 01:22:02 -0300

    desafio sql 18 - fix

--- desafio18.sql
+++ desafio18.sql
@@ -1,7 +1,8 @@
-SELECT  jobs.JOB_TITLE AS `Cargo`,
-        jobs.MAX_SALARY - jobs.MIN_SALARY AS `Variação Salarial`,
-        ROUND(jobs.MIN_SALARY / 12, 2) AS `Média mínima mensal`,
-        ROUND(jobs.MAX_SALARY / 12, 2) AS `Média máxima mensal`
-FROM hr.jobs
-ORDER BY  `Variação Salarial`,
-          `Cargo`;
+SELECT
+    CONCAT(t1.first_name, ' ', t1.last_name) AS 'Nome completo',
+    DATE_FORMAT(t2.start_date, '%d/%m/%Y') AS 'Data de início',
+    DATE_FORMAT(t2.end_date, '%d/%m/%Y') AS 'Data de rescisão',
+    ROUND(DATEDIFF(t2.end_date, t2.start_date) / 365, 2) AS 'Anos trabalhados'
+FROM hr.employees AS t1
+INNER JOIN hr.job_history AS t2 ON t1.employee_id = t2.employee_id
+ORDER BY 1 ASC , 4 ASC;

4e0b3e0cd2a8d0d464222cce504b7c86aa1abd97
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-06-20 22:08:40 -0300

    desafio sql 1 - fix digitation error

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,1 @@
-SELECT contry_name AS País, IF (region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY contry_name ASC;
+SELECT country_name AS País, IF (region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão' FROM hr.countries ORDER BY country_name ASC;

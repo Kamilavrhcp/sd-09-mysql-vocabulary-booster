a66b238b240ee5afb40c59ac8d70311f6ffd964d
Sabrina <sasa2605.alves@gmail.com>
2021-06-21 11:42:24 -0300

    requirement 16 e 19

--- desafio15.sql
+++ desafio15.sql
@@ -1,18 +1,14 @@
 USE hr;
 DELIMITER $$
-CREATE PROCEDURE buscar_media_por_cargo(in cargo varchar(50))
+
+CREATE PROCEDURE buscar_media_por_cargo(IN job_name VARCHAR(100))
 BEGIN
-SELECT 
-ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
-FROM
-hr.employees AS e
-INNER JOIN
-hr.jobs AS j ON j.JOB_ID = e.JOB_ID
-WHERE
-j.JOB_TITLE = cargo;
+SELECT ROUND(AVG(e.SALARY), 2) AS `Média salarial`
+FROM employees AS e
+INNER JOIN jobs AS j
+ON j.JOB_ID = e.JOB_ID 
+WHERE JOB_TITLE = job_name;
 END $$
 DELIMITER ;
-
-CALL buscar_media_por_cargo('Programmer');
 /*Consultei o código do colega Kevin Fraga para
 entender o uso da sintaxe do stored procedure no projeto*/
